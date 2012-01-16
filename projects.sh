6c1865ed03a1cf3c8ac6eaf922a12bc4fd3ba010
spiralofhope <soh@spiralofhope.com>
2012-01-15 17:24:27 -0800

    from 2012-01-14

--- projects.sh
+++ projects.sh
@@ -1,64 +1,94 @@
-# Was working decently.  I re-did this to solve my odd multiple-geany-window issue.
+#!/usr/bin/env zsh
+# FIXME: This only works in zsh.  In regular shell (remove the top line) it's really truly fucked up!
 
-PROJECTS="/home/user/live/projects"
-# Doesn't fucking work when ~/live is a symlink which isn't pointing anywhere
-# if [ ! -d $PROJECTS ]; then
-\ls $PROJECTS/.
-if [ $? != "0" ]; then
-  \echo "ERROR:  \"$PROJECTS\" does not exist!"
+# TODO - I don't need to use `find`
+# FIXME:  Work in symbolic-link directories.
+
+# TODO:  Skip lost+found
+
+PROJECTS="/l/projects"
+NEW_PROJECT_MESSAGE="New project notes started `date`"
+
+if [ ! -f "${PROJECTS}/projects.txt" ]; then
+  \echo "ERROR:  \"${PROJECTS}/projects.txt\" does not exist!"
   return 1
 fi
 
-# Where {} is the name of the directory.
-NOTES='{}.txt'
-# NOTES="project-notes.txt"
-# The message put in the new project notes file, if one needs to be made.
-MESSAGE="New project notes started `date`"
+\cd "$PROJECTS"
 
-## If it isn't running, start it.
-#ps alx|grep medit
-#if [ $? -eq 1 ]; then
-  #medit&
-  ## Because calling medit so rapidly when it doesn't already exist will open many medit instances, I do this..
-  #sleep 2
-#fi
+OLDIFS=$IFS
+IFS=$'\n'
 
-# Nope, I can't even do this.. because I have to sleep to allow the program to start up.
-#"nohup" "medit" >> /dev/null & ; PID=$!
-#EXIT="1"
-#until [ $EXIT != "1" ]; do
-#  sleep 0.1
-#  "ps" -p $PID -o comm= ; EXIT=$?
-#  echo $EXIT
-#done
-# sleep 3
+# TODO:  Find a way to make this code re-usable?
 
-#TODO - sanity-check $PROJECTS to make sure it's a directory, and that I can CD into it.
-# Make sure the files are readible
-cd "$PROJECTS"
-
-# TODO - how can I get all of this launched in a nice and sorted manner?
-find . -maxdepth 1 -type d \
-  -exec sh -c "
-    if [ ! -f '{}'/'$NOTES' ]; then
-      \echo '$MESSAGE' > '{}'/'$NOTES'
+open_new_projects() {
+  # TODO:  I have no clue how to go line-by-line in a variable, so I'm using `find` directly.  (update: I can use an array)
+  # I'd bet that the IFS changing thing does the trick.
+  for dir in $( \find . -maxdepth 1 -type d | \sort -f ); do
+    # Remove the starting "." and then the starting "/"
+    # This "blanks out" the current directory, which is normally just a period.
+    dir=$( \echo "$dir" | \sed 's/^.//' | \sed 's/^\///' )
+    ## Skip the blanked-out current directory
+    if [ "x$dir" = "x" ]; then
+      continue
     fi
-  " \; \
-  -exec sh -c "\geany '{}'/'$NOTES' &" \; \
-  -exec sh -c "\ls '{}'" \; \
-  -exec sh -c "\sleep 0.1" \;
+    dir_file="${dir}/${dir}.txt"
+
+    # The actual work
+    if [ ! -f "$dir_file" ]; then
+      \echo "Found a new project:  $dir"
+      \echo "Inserting message:  $MESSAGE"
+      \echo "$NEW_PROJECT_MESSAGE" > "$dir_file"
+    fi
+  done
 }
-# The sleep has been added to ensure that one single geany window has all the files opened as tabs within it.  It's been opening multiple windows which is horribly messy!
-# .. but it doesn't work.
 
-# Another method which seems pretty interesting but ended up being annoying because find was giving a leading "./"
-# TODO: I ought to solve that.  It should be easy.  Then I can do so much more!
-#\find . -maxdepth 1 -type d -print |
-#while \read i ; do
-  #\echo "$i"/"$i".txt
-  #\echo ""
-  #\sleep 0.1
-#done
+cache_files() {
+  # TODO:  I have no clue how to go line-by-line in a variable, so I'm using `find` directly.
+  # I'd bet that the IFS changing thing does the trick.
+  for dir in $( \find . -maxdepth 1 -type d | \sort -f ); do
+    # Remove the starting "." and then the starting "/"
+    # This "blanks out" the current directory, which is normally just a period.
+    dir=$( \echo "$dir" | \sed 's/^.//' | \sed 's/^\///' )
+    ## Skip the blanked-out current directory
+    if [ "x$dir" = "x" ]; then
+      continue
+    fi
+    dir_file="${dir}/${dir}.txt"
 
-# This'll delete any stray project notes that get placed throughout my ~ and its subdirectories due to a misfire.
-# find ~/ -maxdepth 1 -type f -name 'project-notes.txt' -exec rm -v {} \;
+    # The actual work
+    \cat "$dir_file" >> /dev/null
+  done
+}
+
+open_files() {
+  # TODO:  I have no clue how to go line-by-line in a variable, so I'm using `find` directly.
+  # I'd bet that the IFS changing thing does the trick.
+  for dir in $( \find . -maxdepth 1 -type d | \sort -f ); do
+    # Remove the starting "." and then the starting "/"
+    # This "blanks out" the current directory, which is normally just a period.
+    dir=$( \echo "$dir" | \sed 's/^.//' | \sed 's/^\///' )
+    ## Skip the blanked-out current directory
+    if [ "x$dir" = "x" ]; then
+      continue
+    fi
+    dir_file="${dir}/${dir}.txt"
+
+    # The actual work
+    \geany "$dir_file" &
+    sleep 0.2
+  done
+}
+
+# Make sure it's the first tab.
+# I only need to "&" (background process) for the first summoning of Geany.
+\geany ./todo.txt &
+# But I need to wait a bit so it actually gets a process which other files can attach to.
+\sleep 1
+\geany ./projects.txt
+open_new_projects
+cache_files
+open_files
+\geany ./todo.txt
+
+IFS=$OLDIFS
