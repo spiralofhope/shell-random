7f8e1d3c4f585bc9dcf164629a39b5194303c982
spiralofhope <soh@spiralofhope.com>
2012-01-15 20:44:05 -0800

    `weechat` wrapper/helper thing.  Obsoleted since I no longer use weechat.

---
+++ archive/weechat.sh
@@ -0,0 +1,3 @@
+# Terminal --geometry 80x24+0+0 --command=/usr/local/bin/weechat-curses --hide-menubar --hide-borders
+Terminal --geometry 80x24+0+0 --command=/usr/local/bin/weechat-curses --hide-menubar &
+
