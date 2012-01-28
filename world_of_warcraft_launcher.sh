225317ff67994298ad04531117e3ac285e926006
spiralofhope <soh@spiralofhope.com>
2012-01-28 06:45:15 -0800

    From 2011-12-12 Unity Linux.

--- world_of_warcraft_launcher.sh
+++ world_of_warcraft_launcher.sh
@@ -1,5 +1,8 @@
-#!/usr/bin/env zsh
+#!/bin/env zsh
 
+#TODO:  This should refuse to run in other than root.
+
+# My previous Unity Linux needed this.  Now I can just `\rm -rf ~/.wine && \winecfg` and the default sound settings are correct.
 # padsp is from pulseaudio-utils (try task-pulseaudio)
 # \rm -rf ~/.wine
 # \padsp \winecfg
@@ -7,26 +10,9 @@
 
 # TODO:  have a config.wtf just for raiding, and summon it via this script with something like ./wow.sh raid
 
-wowdir="/mnt/ssd/WoW/World of Warcraft"
-# The default Openbox configuration file's location.
-rc=$HOME/.config/openbox/rc.xml
-# Unity Linux Alpha1
-#sudo=(\gksu -u root)
-#sudo_user=(\gksu -u user)
+# Read in the configuration
+\. $( \readlink -f $0 ).ini
 
-# Unity Linux Alpha1
-sudo=(\su -c)
-sudo_user=(\sh -c)
-
-#sudo=(\sh -c)
-#sudo_user=(\sh -c)
-
-# --
-
-rc_normal=${rc}-normal.xml
-rc_wine=${rc}-wine.xml
-wowconfig=$wowdir/WTF/Config.wtf
-wow_texts=/mnt/ssd/WoW/texts
 
 _wow_die(){
   \echo " * ^c detected, killing WoW."
@@ -37,18 +23,20 @@ trap _wow_die INT
 _wow_teardown(){
   \echo " * Cleaning up.."
 
-  # Not installed on Unity Linux
+  # Installed but not working on Unity Linux.  It says "no or unknown cpufreq driver is active on this CPU"
   #\echo " * Cooling down the CPU for \"on demand\" performance."
-  ## On Unity, there is `cpufreq-set` in cpufreqtools
-  ## I need to install the amd cpufreq stuff I think..
-  #\cpufreq-selector --governor ondemand
+  #\cpufreq-set --governor ondemand
 
   \echo " * Restoring the mouse."
   \xsetroot -cursor_name left_ptr
 
   \echo " * Re-enabling original Openbox hotkeys."
   \cp --force $rc_normal $rc
-  \openbox --reconfigure
+  if ! [ $(whoami) = root ]; then
+    \openbox --reconfigure
+  else
+    \su user -c "\openbox --reconfigure"
+  fi
   \echo " * Backing up any WoW configuration changes that I've made in-game."
   \echo "   Note that in case I screwed things up, there is a static copy kept"
   \echo "     as Config.wtf-WINE-backup"
@@ -90,13 +78,13 @@ test_cache_files() {
 _open_wow_texts() {
   cache_files $wow_texts
   cd $wow_texts
-  \geany _wow.txt &
+  \su user -c "\geany _wow.txt" &
   sleep 1
   for i in $wow_texts/*; do
-    \geany $i &
+    \su user -c "\geany $i" &
     sleep 0.1
   done
-  \geany _wow.txt &
+  \su user -c "\geany _wow.txt" &
   cd -
   sleep 3
 }
@@ -117,80 +105,68 @@ _hide_mouse() {
 
 _clear_combatlog() {
   # Blanking out the combat log because there's no other mechanism to trim it.  It can grow very large!
-  log="/mnt/ssd/WoW/World of Warcraft/Logs/WoWCombatLog.txt"
-  \rm --force $log
+  \rm --force $wow_log
   # Leaving it missing will cause problems.
-  \touch $log
+  \touch $wow_log
 }
 
 # --
 
-if [ -z $1 ]; then
-  \echo " * Launching Mangler (Ventrilo client) with auto-login."
-  # Zomg plaintext login information!
-  \padsp \mangler -s xx:xx -u xx -p xx &
-
-  _open_wow_texts
+if ! [ $(whoami) = root ]; then
+  \echo "You need to be root!"
+  _wow_teardown
+else
 
   \killall -9 unclutter
-  \killall -9 unclutter
-
-  _hide_mouse
+  #_hide_mouse
   _clear_combatlog
-fi
 
-\echo " * Disabling conflicting Openbox hotkeys."
-\cp --force $rc_wine $rc
-\openbox --reconfigure
+  if [ -z $1 ]; then
+    \echo " * Launching Mangler (Ventrilo client) with auto-login."
+    # Zomg plaintext login information!
+    su user -c "\padsp \mangler -s $mangler_ip:$mangler_port -u $mangler_user -p $mangler_pass" &
+    
+    _open_wow_texts
+  fi
 
-\echo " * Backing up existing WoW settings."
-\cp --force $wowconfig      ${wowconfig}-previous
-\echo " * Applying Wine-specific WoW settings."
-\cp --force ${wowconfig}-WINE $wowconfig
+  \echo " * Disabling conflicting Openbox hotkeys."
+  \cp --force $rc_wine $rc
+  \openbox --reconfigure
 
-\echo " * Escalating privileges..."
-\echo " * Killing any existing/hung Wow.exe process.."
-\echo "   ( note that this is not a kill -9 )"
-$sudo \
+  \echo " * Backing up existing WoW settings."
+  \cp --force $wowconfig      ${wowconfig}-previous
+  \echo " * Applying Wine-specific WoW settings."
+  \cp --force ${wowconfig}-WINE $wowconfig
+
+  \echo " * Escalating privileges..."
+  \echo " * Killing any existing/hung Wow.exe process.."
+  \echo "   ( note that this is not a kill -9 )"
   \killall Wow.exe &> /dev/null
 
-# Last used under Ubuntu, not available / not downloaded for Unity Linux.
-#\echo " * Heating up the CPU for maximum performance."
-## $sudo dpkg-reconfigure gnome-applets
-## cpufreq is probably a package that's pre-installed
-## I can drop the cpufreq applet into my toolbar and use it there, or:
-#\cpufreq-selector --governor performance
+  #\echo " * Heating up the CPU for maximum performance."
+  #\cpufreq-set --governor performance
 
-\echo " * Launching Wow.exe via wine."
-# I'm $sudoing into the user to request the password,
-# since I need $sudo later to do the renices.  It's
-# much more simple this way.
-# However, I do have $sudo earlier, so technically I
-# don't need it here anymore.  It does no harm.
-$sudo_user \
-  \padsp \wine $wowdir/Wow.exe &> /dev/null & \
-  wowpid=$!
+  \echo " * Launching Wow.exe via wine."
+  \su user -c "\padsp \wine \"$wowdir\"/Wow.exe &> /dev/null" & \
+    wowpid=$!
+  #\su user -c "\padsp \wine \"$wowdir\"/launcher.exe &> /dev/null" & \
+    #wowpid=$!
   #\echo "   WoW is at pid: " $wowpid
-
-# -----------
-#\wine $wowdir/Wow.exe &> /dev/null & \
-  #wowpid=$!
-
-$sudo \
   \renice -n -10 $wowpid &> /dev/null
-$sudo \
   \renice -n -10 `\pidof wineserver` &> /dev/null
 
-#\echo " ┌──────────────────────────┐ "
-#\echo " │ Do not close this window │ "
-#\echo " └──────────────────────────┘ "
-\echo " .--------------------------. "
-\echo " : Do not close this window : "
-\echo " \`--------------------------' "
-\echo ""
-\echo "To restore the mouse pointer, do:"
-\echo "\xsetroot -cursor_name left_ptr"
+  #\echo " ┌──────────────────────────┐ "
+  #\echo " │ Do not close this window │ "
+  #\echo " └──────────────────────────┘ "
+  \echo " .--------------------------. "
+  \echo " : Do not close this window : "
+  \echo " \`--------------------------' "
+  \echo ""
+  \echo "To restore the mouse pointer, do:"
+  \echo "\xsetroot -cursor_name left_ptr"
 
-\wait $wowpid &> /dev/null
-\echo " * WoW has ended."
-_wow_teardown
+  \wait $wowpid &> /dev/null
+  \echo " * WoW has ended."
+  _wow_teardown
+
+fi
