a74349f9c9ad227aa72216a9869bd7882577f9b6
spiralofhope <soh@spiralofhope.com>
2012-02-01 06:43:11 -0800

    GITFIXME - older revision from 2010-11-21 Lubuntu.

--- bitnami-start.sh
+++ bitnami-start.sh
@@ -1,29 +1,11 @@
-if ! [ $(whoami) = root ]; then
-  \echo "You need to be root!"
-else
+# Tested 2010-11-21 on Lubuntu 10.10, updated recently.
 
-\adduser --no-create-home mysql &> /dev/null
 if ! [ -d /opt/bitnami ]; then
-  ln -s /home/bitnami-install /opt/bitnami &> /dev/null
+  ln -s /home/bitnami-install /opt/bitnami
 fi
 
-\chown -R user:users /home/bitnami-install
-\chown -R user:users /home/bitnami-data
-
-\echo ''
-\echo ' * Starting MySQL.'
 # MySQL must be started as a regular user.
-\su user -c "/opt/bitnami/ctlscript.sh start mysql" &
+sudo -u user /opt/bitnami/ctlscript.sh start mysql &
 
-\echo ''
-\echo ' * Starting Apache.'
 # Apache must be started as root.
-/opt/bitnami/ctlscript.sh start apache & \
-  waitpid=$!
-
-# I did this nonsense just to get my prompt back when things end.
-# .. I don't like having to press enter.  =p
-\wait $waitpid
-\echo ''
-
-fi
+sudo /opt/bitnami/ctlscript.sh start apache &
