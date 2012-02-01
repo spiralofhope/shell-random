42ead34986cf48dd63c81a5a1b66bf120e5765a9
spiralofhope <soh@spiralofhope.com>
2012-02-01 06:44:29 -0800

    GITFIXME - latest revision

--- bitnami-stop.sh
+++ bitnami-stop.sh
@@ -1,2 +1,20 @@
-sudo -u user /opt/bitnami/ctlscript.sh stop &
-sudo /opt/bitnami/ctlscript.sh stop &
+if ! [ $(whoami) = root ]; then
+  \echo "You need to be root!"
+else
+
+\su user -c "/opt/bitnami/ctlscript.sh stop"
+/opt/bitnami/ctlscript.sh stop
+
+# Leftover stuff from failed starts.
+\killall \
+  ctl.sh \
+  ctlscript.sh \
+  &> /dev/null
+
+# Kill log files before they bloat things up:
+\cd ./bitnami-install/apache2/logs
+\rm -f \
+  ./access_log \
+  ./error_log
+
+fi
