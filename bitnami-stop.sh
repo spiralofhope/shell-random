d42928037746fa99d452c75bedb8b90a265ee787
spiralofhope <soh@spiralofhope.com>
2012-02-01 06:44:09 -0800

    GITFIXME - older revision from 2010-11-21 Lubuntu.

--- bitnami-stop.sh
+++ bitnami-stop.sh
@@ -1,20 +1,2 @@
-if ! [ $(whoami) = root ]; then
-  \echo "You need to be root!"
-else
-
-\su user -c "/opt/bitnami/ctlscript.sh stop"
-/opt/bitnami/ctlscript.sh stop
-
-# Leftover stuff from failed starts.
-\killall \
-  ctl.sh \
-  ctlscript.sh \
-  &> /dev/null
-
-# Kill log files before they bloat things up:
-\cd ./bitnami-install/apache2/logs
-\rm -f \
-  ./access_log \
-  ./error_log
-
-fi
+sudo -u user /opt/bitnami/ctlscript.sh stop &
+sudo /opt/bitnami/ctlscript.sh stop &
