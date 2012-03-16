24a8859c472480a673f23fe27ca112cce9f2fe17
spiralofhope <soh@spiralofhope.com>
2012-03-16 15:48:42 -0700

    ..

--- bitnami-start.sh
+++ bitnami-start.sh
@@ -3,29 +3,33 @@ bitnami_dir=/z/mediawiki
 if ! [ $(whoami) = root ]; then
   \echo "You need to be root!"
 else
+  \adduser --no-create-home mysql &> /dev/null
 
-\adduser --no-create-home mysql &> /dev/null
-if ! [ -d /opt/bitnami ]; then
-  ln --force --symbolic $bitnami_dir/bitnami-install /opt/bitnami &> /dev/null
-fi
+  # Of course, there might be an actual file or directory at /opt/bitnami, but I ought to notice that..
+  if [ -s /opt/bitnami ]; then
+    \rm --force /opt/bitnami
+  fi
+  \ln --symbolic \
+    $bitnami_dir/bitnami \
+    /opt/bitnami
 
-\chown -R user:users $bitnami_dir/bitnami-install
-\chown -R user:users $bitnami_dir/bitnami-data
+  # This might be a bad idea!
+  \chown -R user:users $bitnami_dir/bitnami-install
+  \chown -R user:users $bitnami_dir/bitnami-data
 
-\echo ''
-\echo ' * Starting MySQL.'
-# MySQL must be started as a regular user.
-\su user -c "/opt/bitnami/ctlscript.sh start mysql" &
+  \echo ''
+  \echo ' * Starting MySQL.'
+  # MySQL must be started as a regular user.
+  \su user -c "/opt/bitnami/ctlscript.sh start mysql" &
 
-\echo ''
-\echo ' * Starting Apache.'
-# Apache must be started as root.
-/opt/bitnami/ctlscript.sh start apache & \
-  waitpid=$!
+  \echo ''
+  \echo ' * Starting Apache.'
+  # Apache must be started as root.
+  /opt/bitnami/ctlscript.sh start apache & \
+    waitpid=$!
 
-# I did this nonsense just to get my prompt back when things end.
-# .. I don't like having to press enter.  =p
-\wait $waitpid
-\echo ''
-
+  # I did this nonsense just to get my prompt back when things end.
+  # .. I don't like having to press enter.  =p
+  \wait $waitpid
+  \echo ''
 fi
--- bitnami-stop.sh
+++ bitnami-stop.sh
@@ -1,20 +1,33 @@
 if ! [ $(whoami) = root ]; then
   \echo "You need to be root!"
 else
+  \su user -c "/opt/bitnami/ctlscript.sh stop"
+  /opt/bitnami/ctlscript.sh stop \
+    waitpid=$!
 
-\su user -c "/opt/bitnami/ctlscript.sh stop"
-/opt/bitnami/ctlscript.sh stop
+  # Leftover stuff from failed starts.
+  \killall \
+    ctl.sh \
+    ctlscript.sh \
+    &> /dev/null
 
-# Leftover stuff from failed starts.
-\killall \
-  ctl.sh \
-  ctlscript.sh \
-  &> /dev/null
+  # Kill log files before they bloat things up:
+  \cd ./bitnami-install/apache2/logs
+  \rm --force \
+    ./access_log \
+    ./error_log
 
-# Kill log files before they bloat things up:
-\cd ./bitnami-install/apache2/logs
-\rm -f \
-  ./access_log \
-  ./error_log
+  # I did this nonsense just to get my prompt back when things end.
+  # .. I don't like having to press enter.  =p
+  \wait $waitpid
+  \echo ''
 
+  # If shit really hits the fan, nuke everything.
+  \killall \
+    httpd \
+    mysqld.bin \
+    &> /dev/null
+
+  \wait $waitpid
+  \echo ''
 fi
