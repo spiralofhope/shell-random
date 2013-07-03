cb28fc488d5f6495b2411924bfce6f9ec37494cc
spiralofhope <soh@spiralofhope.com>
2013-07-02 18:22:36 -0700

    self-explanatory.

---
+++ unfinished/gpg-decrypt-from-clipboard.sh
@@ -0,0 +1,10 @@
+
+# A helper to decrypt a PGP block copied into the clipboard.
+
+# I don't know if the  ;\  should just be a  \  or not, another version of this file was just  \
+
+nohup \
+\xterm  -fn 9x15  -bg black  -fg gray  -sl 10000  -geometry 113x46+0+0  -title 'decrypting'  -e \
+$SHELL -c "
+\xclip  -o  -sel  clip | \gpg  -d | \kwrite  --stdin ;\
+" >> /dev/null&
