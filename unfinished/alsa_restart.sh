4b0536ac3e932830ff5310c7de64b0cd77536953
spiralofhope <soh@spiralofhope.com>
2013-07-02 18:13:41 -0700

    Restarting ALSA, never even started because I switched to PulseAudio.

---
+++ unfinished/alsa_restart.sh
@@ -0,0 +1,11 @@
+# try restarting ALSA
+# service alsa restart
+## must be root
+
+# Doing alsactl to store mixer settings...                        [  OK  ]
+# no. (sound is being used by pid   7700))                        [FAILED]
+# ALSA driver is already running.Doing alsactl to restore mixer se[  OK  ]
+
+# It doesn't give a decent exit code.  Argh.
+# So I'd have to get and process the text to get the pid.
+
