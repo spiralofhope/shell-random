41b07a37297de5dc706688f093c7c4009cf474ed
spiralofhope <soh@spiralofhope.com>
2013-07-02 18:41:51 -0700

    From back in my RAID testing days.

---
+++ dead/raid-add.sh
@@ -0,0 +1,2 @@
+# After swapping HDDs, this will rebuild the new HDD (HDC)
+raidhotadd /dev/md0 /dev/hdc4
---
+++ dead/raid-check.sh
@@ -0,0 +1,6 @@
+# check on the activity of the RAID
+# note that the RAID devices are listed backwards (1 then 0)
+clear
+more /proc/mdstat
+echo
+
