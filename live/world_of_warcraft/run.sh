a9741d79d4546c96ce48a5780036313659bc1f3c
spiralofhope <soh@spiralofhope.com>
2013-07-21 23:43:55 -0700

    Made opengl and an alternate display optional.

--- live/world_of_warcraft/run.sh
+++ live/world_of_warcraft/run.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 
-# may be a fix for occasional issues..
+# TODO - an easy way to swap Config.wtf files around, for raiding.
+
+
+# --
+# -- TROUBLESHOOTING
+# --
+
+# May be a fix for occasional issues
 #  \wine  ./Agent.exe --nohttpauth
 
 
-  # TODO: an easy way to swap Config.wtf files around, for raiding.
+# --
+# -- CONFIGURATION
+# --
+
+# running with  -opengl  will disable most of the advanced eye-candy, but will greatly improve performance.
+# I have some old -opengl notes, but I can no longer reproduce any issues (on 64bit):
+  # -opengl solves black/blank screen issues (was happening at odd angles) on 32bit (on 64bit was fine).  Might also be a driver issue, but whatever..
+  # however, the consequence is when using a viewport the game world it will have odd colours in the blanked-out area(s?).  Oh well.  Don't resize+zoom out a bit.
+  # Also, it's fucking horribly slow in LFR..
+  # Seems to only be fixable with a lowered view distance..
+  # need to update the driver?
+  # this doesn't seem to work:   wine wow.exe -d3d
+  # [nothing helpful]  https://help.ubuntu.com/community/WorldofWarcraft/Troubleshooting
+  # Turned out to be an issue with Config.wtf, textures perhaps..
+opengl=-opengl
+# The alternate display you'd like to run things on.
+# FIXME - Occasional issues with the screen going black when using an alternate x display.  Cannot properly reproduce.
+# FIXME - having audio issues when using an alternate x display and switching back to :0  Cannot properly reproduce.
+_display=:3
+
 
 
 _wow_die(){
@@ -15,35 +41,54 @@ _wow_die(){
 trap _wow_die INT
 
 
+
 _wow_setup(){
   \echo  " * Setup.."
   \gksudo  -u root  "\echo"
-#  ~/.config/openbox/wine.sh
-  \echo  " - disabling the screen saver"
-  # Never kill the task.  Always ask it to exit.
-  \xscreensaver-command -exit
+  if [[ x$_display == x ]]; then
+    ~/.config/openbox/wine.sh
+    \echo  " - disabling the screen saver"
+    \xscreensaver-command  -exit
+  fi
   \echo  " - Deleting creature cache (for SilverDragon)"
-  \rm  --force --verbose /l/wow/_game/Cache/WDB/enUS/creaturecache.wdb
+  \rm  --force  --verbose  /l/wow/_game/Cache/WDB/enUS/creaturecache.wdb
   # This is necessary if not using -opengl and using high settings.
   /l/bin/processor_heat.sh  --yes ; heatup=$?
 }
 
 
+
 _wow_run(){
   if [[ ! x$1 == "xnomumble" ]]; then
-DISPLAY=:3    /l/bin/mumble.sh  $@ &
+    if [[ x$_display == x ]]; then
+                 /l/bin/mumble.sh  $@ &
+    else
+      DISPLAY=$_display  /l/bin/mumble.sh  $@ &
+    fi
   fi
   \echo " * Launching WoW"
 #  \sudo  \swapoff  --all
 
-# -opengl solves black/blank screen issues (was happening at odd angles) on 32bit (on 64bit was fine).  Might also be a driver issue, but whatever..
-# however, the consequence is when using a viewport the game world it will have odd colours in the blanked-out area(s?).  Oh well.  Don't resize+zoom out a bit.
-# Also, it's fucking horribly slow in LFR..
-# Seems to only be fixable with a lowered view distance..
-# need to update the driver?
-# this doesn't seem to work:   wine wow.exe -d3d
-# [nothing helpful]  https://help.ubuntu.com/community/WorldofWarcraft/Troubleshooting
-# Turned out to be an issue with Config.wtf, textures perhaps..
+  if [[ x$1 == "xupdate" ]]; then
+    __GL_THREADED_OPTIMIZATIONS=1  WINEDEBUG=-all  WINEPREFIX=/l/wow/_wineprefix/  /usr/bin/wine \
+      "/l/wow/_game/World of Warcraft Launcher.exe"
+    \echo  "Press enter when the update has concluded.."
+    \read  __
+  else
+    # Launch on a new X session on display 3
+    # Control-Alt-F7/F8 will swap between the two displays.
+
+    if [[ x$_display != x ]]; then
+      \sudo  \X  $_display  -ac  -terminate &
+      \sleep 2
+      #  `ck-launch-session`  resolves audio issues.
+      # I seem to be forced to use xterm.
+      # I don't know how to have xterm close.
+      DISPLAY=$_display  \xterm  -e  /usr/bin/ck-launch-session &
+      # Optional, but useful.
+      # I don't understand why it ignores ~/.config/openbox/* and there are no options to start with alternate configuration, to force its use.
+      DISPLAY=$_display  /usr/bin/openbox  --startup &
+    fi
 
 :<<NOTES
   # NVidia threaded optimizations:
@@ -56,46 +101,15 @@ DISPLAY=:3    /l/bin/mumble.sh  $@ &
   /usr/bin/wine  /l/wow/_game/Wow.exe  >> /dev/null 2>&1 &
 NOTES
 
-  if [[ x$1 == "xupdate" ]]; then
-    __GL_THREADED_OPTIMIZATIONS=1  WINEDEBUG=-all  WINEPREFIX=/l/wow/_wineprefix/  /usr/bin/wine \
-      "/l/wow/_game/World of Warcraft Launcher.exe"
-    \echo  "Press enter when the update has concluded.."
-    \read  ANSWER
-  else
-    # running with  -opengl  will disable most of the advanced eye-candy, but will greatly improve performance.
-#    opengl=-opengl
-
-    # Launch on a new X session on display 3
-    # Control-Alt-F7/F8 will swap between the two displays.
-    \sudo  \X  :3  -ac  -terminate &
-    \sleep 2
-    #  `ck-launch-session`  resolves audio issues.
-    # I seem to be forced to use xterm.
-    # I don't know how to have xterm close.
-    DISPLAY=:3  \xterm  -e /usr/bin/ck-launch-session &
-    # Optional, but useful.
-    # I don't understand why it ignores ~/.config/openbox/* and there are no options to start with alternate configuration, to force its use.
-    DISPLAY=:3  /usr/bin/openbox  --startup &
-
-DISPLAY=:3 \
-     __GL_THREADED_OPTIMIZATIONS=1  WINEDEBUG=-all  WINEPREFIX=/l/wow/_wineprefix/  /usr/bin/wine \
-      /l/wow/_game/Wow-64.exe  $opengl  >> /dev/null 2>&1 &
+    # TODO - niceness.  However, wine isn't so simple to nice.. this has to be researched carefully.
+    __GL_THREADED_OPTIMIZATIONS=1  WINEDEBUG=-all  WINEPREFIX=/l/wow/_wineprefix/  DISPLAY=$_display \
+    /usr/bin/wine \
+    /l/wow/_game/Wow-64.exe  $opengl  >> /dev/null 2>&1 &
+    wowpid=$!
   fi
-  wowpid=$!
 
 
-#  \nice -n -10 /usr/bin/wine       /l/wow/_game/Wow.exe  >> /dev/null 2>&1 & wowpid=$!
-#  \nice -n -10 \padsp \wine       /l/wow/_game/Wow.exe  >> /dev/null 2>&1 & wowpid=$!
-#  \nice -n -10  \wine  /l/wow/_game/Wow-64.exe  >> /dev/null 2>&1 & wowpid=$!
-  # I never did get mumble-overlay to work.
-#  \nice -n -10  \mumble-overlay  \wine  /l/wow/_game/Wow-64.exe  >> /dev/null 2>&1 & wowpid=$!
-#  \nice -n -10 /usr/bin/wine64       /l/wow/_game/Wow-64.exe  >> /dev/null 2>&1 & wowpid=$!
-#      /usr/bin/wine  /l/wow/_game/Wow.exe  >> /dev/null 2>&1 & wowpid=$!
-#      /usr/bin/wine  /l/wow/_game/Wow.exe  -opengl  >> /dev/null 2>&1 & wowpid=$!
-
-
-#  \nice -n -10 /usr/local/bin/wine /l/wow/_game/Wow.exe  >> /dev/null 2>&1 & wowpid=$!
-#  \renice -n -10 $wowpid &> /dev/null
+  # TODO - Additional niceness.  However, wine isn't so simple to nice.. this has to be researched carefully.
   \sudo  \renice  -n -10  $( \pidof wineserver )  &>  /dev/null
   \echo " ┌──────────────────────────┐ "
   \echo " │ Do not close this window │ "
@@ -103,12 +117,15 @@ DISPLAY=:3 \
 }
 
 
+
 _wow_teardown(){
   \echo  " * Teardown.."
   \gksudo  -u root  "\echo"
-#  ~/.config/openbox/unwine.sh && unwinepid=$!
-  \echo " - enabling the screen saver"
-  \xscreensaver  -nosplash &
+  if [[ x$_display == x ]]; then
+    ~/.config/openbox/unwine.sh
+    \echo " - enabling the screen saver"
+    \xscreensaver  -nosplash &
+  fi
   \echo " - Killing any lingering processes."
   /usr/bin/wineboot --shutdown
   /usr/bin/wineserver --kill
@@ -126,6 +143,7 @@ _wow_teardown(){
     /l/bin/processor_cool.sh
   fi
 }
+
 
 
 # --
