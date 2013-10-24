db5cfad715f0984f14b9ed173e943c2732a7ea90
spiralofhope <soh@spiralofhope.com>
2013-10-23 20:51:46 -0700

    Run with no parameters to process all .flac files.

--- live/convert-flac-to-mp3.sh
+++ live/convert-flac-to-mp3.sh
@@ -3,19 +3,16 @@
 
 
 
-_convert_setup() {
-  output_filename="${@/%.flac/ _.mp3}"
-}
-
-
-
-# Note that  \avconv  is a drop-in replacement for the depreciated  \ffmpeg
 _convert_flac_to_mp3() {
   \echo  "Note:  Adding _ to identify this as a transcoded item."
+  # Note that  \avconv  is a drop-in replacement for the depreciated  \ffmpeg
   \avconv \
     -i "$1" \
     -qscale:a 0 \
     "$2"
+  if [[ $? -ne 0 ]]; then
+    read  __
+  fi
 }
 
 
@@ -32,6 +29,19 @@ _convert_vbrfix() {
 
 
 
-_convert_setup        "$1"
-_convert_flac_to_mp3  "$1"  "$output_filename"
-_convert_vbrfix       "$output_filename"
+if [[ $1 == "" ]]; then
+  for i in *.flac; do
+    # Seems like a straightforward way to bail out.
+    if [[ $i == "*.flac" ]]; then
+      \echo  "ERROR:  No .flac files found."
+      continue
+    fi
+    output_filename="${i/%.flac/ _.mp3}"
+    _convert_flac_to_mp3  "$i"  "$output_filename"
+    _convert_vbrfix              "$output_filename"
+  done
+else
+    output_filename="${@/%.flac/ _.mp3}"
+    _convert_flac_to_mp3  "$1"  "$output_filename"
+    _convert_vbrfix             "$output_filename"
+fi
