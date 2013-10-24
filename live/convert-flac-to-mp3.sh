fc271cd4335a385c9f0d757df7ac23fc4097482a
spiralofhope <soh@spiralofhope.com>
2013-10-23 20:24:16 -0700

    .

--- live/convert-flac-to-mp3.sh
+++ live/convert-flac-to-mp3.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+# TODO - figure out how to convert to ogg.  My passing attempt failed and I gave up too easily.
+
 
 
 _convert_setup() {
   output_filename="${@/%.flac/ _.mp3}"
 }
+
+
+
+# Note that  \avconv  is a drop-in replacement for the depreciated  \ffmpeg
+_convert_flac_to_mp3() {
+  \echo  "Note:  Adding _ to identify this as a transcoded item."
+  \avconv \
+    -i "$1" \
+    -qscale:a 0 \
+    "$2"
+}
+
 
 
 _convert_vbrfix() {
@@ -18,17 +32,6 @@ _convert_vbrfix() {
 
 
 
-# Note that  \avconv  is a drop-in replacement for the depreciated  \ffmpeg
-_convert_flac_to_ogg() {
-  \echo  "Note:  Adding _ to identify this as a transcoded item."
-  \avconv \
-    -i "$1" \
-    -qscale:a 0 \
-    "$2"
-}
-
-
-
 _convert_setup        "$1"
-_convert_flac_to_ogg  "$1"  "$output_filename"
+_convert_flac_to_mp3  "$1"  "$output_filename"
 _convert_vbrfix       "$output_filename"
