c52a59eecd95d49b04f41b8dbdcd3d5bfdc77393
spiralofhope <soh@spiralofhope.com>
2013-12-10 00:30:40 -0800

    .

--- live/terminal.sh
+++ live/terminal.sh
@@ -2,39 +2,42 @@
 
 # Run through various terminals to find one that's installed.. '
 
-# Not tabbed?  Use `screen`.  Not much point in a windowed environment unless you're REALLY tight on memory.
-#   screen screws up the scrollback.
+# Not tabbed?  Use `screen`.  There's not much point for using screen locally, in a windowed environment, unless you're REALLY tight on memory.
+#   `screen` screws up the scrollback.
 #   one optimal usage might be:  screen -a -D -R -q -T term -h 1000 -x .. but I can't get this to work for me.
 #   \xterm -fn vga -bg black -fg gray -cr darkgreen -sl 10000 -geometry 80x24+0+0 -e screen -q
 
-# TODO:  Implement a universal parameter thingy, so that this script can be passed parameters and it will translate them into the appropriate format for each terminal.  This is necessary for universality, and also since Openbox does not allow --parameter (two dashes) in it's rc.xml .. sad.
+# TODO:  Implement a universal parameter thingy, so that this script can be passed parameters and it will translate them into the appropriate format for each terminal.  This is necessary for universality.
+
+
 
 terminal_setup() {
   # This fixes a GNOME hotkey issue which was starting the terminal in /  I don't know if this breaks any other usage!
-  \cd ~
+  #\cd  ~
 
   # This was for Unity Linux, possibly for Ubuntu/Lubuntu, does not apply to Gentoo/Sabayon
   # Check for my favourite font, with a safe fallback.
-  DISPLAY=:0.0 \xlsfonts | grep vga
-  if [ $? -eq 1 ]; then
-    font=-*-fixed-medium-*-*-*-14-*-*-*-*-*-*-*
+  # Not sure why I originally used  DISPLAY=  here.
+  #DISPLAY=:0.0  \xlsfonts  |  \grep  vga
+  \xlsfonts  |  \grep  --line-regexp  --quiet  vga
+  if [[ $? -eq 1 ]]; then
+    font="-*-fixed-medium-*-*-*-14-*-*-*-*-*-*-*"
   else
-    font=vga
+    font="vga"
   fi
-
-  terminal_run=0
 }
+
+
 
 run_if_exists(){
-  if [ $terminal_run -eq 0 ]; then
-    if [[ $? -eq 127 || $? -eq 0 ]]; then
-      $@
-      if [ $? -eq 0 ]; then
-        terminal_run=1
-      fi
-    fi
-  fi;
+  which  $1  >  /dev/null
+  if [[ $? -eq 0 ]]; then
+    $@
+    exit  0
+  fi
 }
+
+
 
 terminal_determination() {
 
@@ -70,7 +73,9 @@ terminal_determination() {
   # Tabbed
   # when using bash as the default shell, the prompt doesn't immediately appear.  zsh works fine.
   run_if_exists \
-    \sakura --geometry 80x24+1+1 $@
+    \sakura \
+      --geometry 80x24+1+1 \
+      $@
 
   # http://invisible-island.net/xterm/
   run_if_exists \
@@ -100,42 +105,53 @@ terminal_determination() {
   # Bloated, but at least it can use the default system fixed width font so it looks right.
   # Tabbed
   run_if_exists \
-    \gnome-terminal --geometry 80x24+0+0 $@
+    \gnome-terminal \
+      --geometry 80x24+0+0 \
+      $@
 
   # TODO:  Website
   # Tabbed
   run_if_exists \
-    \Terminal --geometry 80x24+0+0 $@
+    \Terminal \
+      --geometry 80x24+0+0 \
+      $@
 
   # TODO:  Website
   # Tabbed
   run_if_exists \
-    \terminal --geometry 80x24+0+0 $@
+    \terminal \
+      --geometry 80x24+0+0 \
+      $@
 
   # http://lilyterm.luna.com.tw/
   # Tabbed
   run_if_exists \
-    \lilyterm $@
+    \lilyterm \
+      $@
 
   # http://materm.sourceforge.net/wiki/
   # (Formerly materm)
   # FIXME: my options from the Zaurus
   # Tabbed
   run_if_exists \
-    \mrxvt $@
+    \mrxvt \
+      $@
 
   # http://roxterm.sourceforge.net/
   # This term doesn't feel right
   # Tabbed
   run_if_exists \
-    \roxterm $@
+    \roxterm \
+      $@
 
   # http://software.jessies.org/terminator/
   # Tabbed
   # Holy shit, their geometry is pixel based, not character based like everyone else in the universe.
   #\terminator --geometry 80x24+0+0 $@
   run_if_exists \
-    \terminator --geometry +0+0 $@
+    \terminator \
+      --geometry +0+0 \
+      $@
 
   # http://www.eterm.org/
   # Has dependencies.. I won't want to use it.
@@ -143,73 +159,24 @@ terminal_determination() {
   #  \Eterm
 }
 
+
+
 # --
 # -- Begin
 # --
 
 terminal_setup
 # TODO:  This can be redone.  The knowledge of "with_lines-ness" can be moved up into the individual programs.
-# 1) Each program can set a flag.
-# 2) When referring to the programs, pass an option to require+execute with or without with_lines-ness.
-# seems straightforward.. good luck, self!
+#   1) Each program can set a flag.
+#   2) When referring to the programs, pass an option to require+execute with or without with_lines-ness.
+#   It seems straightforward.. good luck, self!
 if [ "x$1" == "xwith_lines" ]; then
   # Nuke $1
   shift
-
-  run_if_exists \
-    \sakura $@
-
-  run_if_exists \
-    \lxterminal "$@"
-
-  run_if_exists \
-    \Terminal "$@"
-
-  # --
-  # Fallbacks
-  # --
-  # This includes some sane defaults.  Not sure if I ought/needed to do this..
-
-  run_if_exists \
-    \aterm \
-      ` # Output to the window should not have it scroll to the bottom.` \
-      -si \
-      ` # No visual bell. ` \
-      +vb \
-      ` # No scrollbar. ` \
-      +sb \
-      ` # The default font can do fancy designs. ` \
-      -font default \
-      -bg black \
-      -fg gray \
-      -cr darkgreen \
-      -sl 10000 \
-      -geometry 80x24+0+0 \
-      $@
-
-  run_if_exists \
-    \xterm \
-        ` # Output to the window should not have it scroll to the bottom.` \
-      -si \
-        ` # No visual bell. ` \
-      +vb \
-        ` # No scrollbar. ` \
-      +sb \
-        ` # Jump scrolling.  Normally, text is scrolled one line at a time; this option allows xterm to move multiple lines at a time so that it does not fall as far behind. Its use is strongly recommended since it makes xterm much faster when scanning through large amounts of text. ` \
-      -j \
-        ` # Indicates that xterm may scroll asynchronously, meaning that the screen does not have to be kept completely up to date while scrolling. This allows xterm to run faster when network latencies are very high and is typically useful when running across a very large internet or many gateways. ` \
-      -s \
-        ` # My font addition ` \
-      -fn $font \
-        ` # xterm should assume that the normal and bold fonts have VT100 line-drawing characters.  It sets the forceBoxChars resource to "true". ` \
-      +fbx \
-      -bg black \
-      -fg gray \
-      -cr darkgreen \
-      -sl 10000 \
-      -geometry 80x24+0+0 \
-      $@
-
-else
-  terminal_determination
+  run_if_exists  \sakura     "$@"
+  run_if_exists  \lxterminal "$@"
+  run_if_exists  \Terminal   "$@"
 fi
+
+# If just running things, or if none of the with_lines items exist, then attempt the whole list:
+terminal_determination
