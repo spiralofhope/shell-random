4e0cbdefb8bf1a00bf16fd5d71a7267433b3149f
spiralofhope <soh@spiralofhope.com>
2013-12-15 10:24:53 -0800

    .

--- live/projects.sh
+++ live/projects.sh
@@ -4,22 +4,22 @@ PROJECTS="/l"
 NEW_PROJECT_MESSAGE="New project notes started `date`"
 
 if [ ! -d $PROJECTS ]; then
-  \echo "ERROR:  \"${PROJECTS}\" does not exist!"
-  return 1
+  \echo  "ERROR:  \"${PROJECTS}\" does not exist!"
+  return  1
 fi
 if [ ! -f "${PROJECTS}/projects.txt" ]; then
-  \echo "ERROR:  \"${PROJECTS}/projects.txt\" does not exist!"
-  return 1
+  \echo  "ERROR:  \"${PROJECTS}/projects.txt\" does not exist!"
+  return  1
 fi
 
 \cd "$PROJECTS"
 
 # Make sure it's the first tab.
 # I only need to "&" (background process) for the first summoning of Geany.
-\geany ./todo.txt &
+\geany  ./todo.txt &
 # But I need to wait a bit so it actually gets a process which other files can attach to.
-\sleep 1
-\geany ./projects.txt
+\sleep  1
+\geany  ./projects.txt
 
 # Open all the other project files.
 for i in *; do
@@ -30,15 +30,15 @@ for i in *; do
   if [[ $i == 'lost+found' ]]; then
     continue
   fi
-  echo " * Processing $i"
+  \echo  " * Processing $i"
   if [ ! -f $i/$i.txt ]; then
-    \echo "   New project, inserting message:\n   $NEW_PROJECT_MESSAGE"
-    \echo "$NEW_PROJECT_MESSAGE" > $i/$i.txt
+    \echo  "   New project, inserting message:\n   $NEW_PROJECT_MESSAGE"
+    \echo  "$NEW_PROJECT_MESSAGE" > $i/$i.txt
   fi
-  \cat $i/$i.txt >> /dev/null
-  \geany $i/$i.txt
-  sleep 0.2
+  \cat  $i/$i.txt >> /dev/null
+  \geany  $i/$i.txt
+  \sleep  0.2
 done
 
 # Switch back to that first tab
-\geany ./todo.txt &
+\geany  ./todo.txt &
