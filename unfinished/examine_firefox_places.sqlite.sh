185564202d9bc80303d4a7729ca6c6b1becbf1f2
spiralofhope <soh@spiralofhope.com>
2014-02-23 14:15:04 -0800

    Firefox cannot bookmark a given link at the commandline.  I'd like to solve that issue.

---
+++ unfinished/examine_firefox_places.sqlite.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env  zsh
+
+# Tested 2014-02-23 on Lubuntu 13.10, updated recently.
+
+#\sudo  \apt-get  install  sqlite3
+
+# List all folders:
+#\sqlite3  places.sqlite \
+#  "select id,title from moz_bookmarks where type=2"
+
+# Just grab one of them:
+last_folder=$( \
+\sqlite3  places.sqlite \
+  "select id  from moz_bookmarks  where type=2" |\
+  \tail  --lines=1
+)
+#echo $f
+
+some_ids=$( \sqlite3  places.sqlite  "select fk  from moz_bookmarks  where parent="$last_folder )
+#echo $some_ids
+
+## Who the fuck knows why I have to do this echo trick..
+#for i in $( \echo  $some_ids ) ; do
+#  \sqlite3  places.sqlite \
+#  "select url  from moz_places  where id="$i
+#done
+
+
+
+# Give complete info:
+for i in $( \echo  $some_ids ) ; do
+  \sqlite3  places.sqlite \
+  "select *  from moz_places  where id="$i
+done
+
+# TODO - How do I learn the names of all the tables?
+# TODO - Learn how to append a new bookmark.
+# TODO - Will I be able to do this on a live-opened database?
