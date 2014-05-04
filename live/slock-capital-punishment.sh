a7ab5c3b1d5c44e7225f7a62724fba404ccddd20
spiralofhope <soh@spiralofhope.com>
2014-05-03 19:44:06 -0700

    A simple way to have a screen saver running or not.  Made necessary by xautolock not noticing that flash is playing a video.

---
+++ live/slock-capital-punishment.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env  bash
+
+
+
+# TODO - This can't deal with the process being run by another user and being denied permission to kill their process.
+# Yes, I could also use `pidof`, except it won't work if there are multiple instances or if there is, say, an xautolock which isn't running slock.
+local  slock_pid=$( \
+  \ps  ax |\
+  \grep  xautolock |\
+  \grep  slock |\
+  \cut \
+    -d' ' \
+    -f1 \
+)
+\echo  'The slock pid is' $slock_pid
+\zenity \
+  --question \
+  --title='' \
+  --text='Do you want slock to be running?'
+if [ $? -eq 1 ]; then
+  # no, stop slock
+  if [[ x$slock_pid == 'x' ]]; then
+    \echo  "slock isn't running."
+  else
+    \echo  'killing slock..'
+    \kill  $slock_pid
+  fi
+else
+  # yes, run slock
+  if [[ x$slock_pid == 'x' ]]; then
+    \echo  'starting slock..'
+    \xautolock  -time 5  -locker slock &
+  else
+    \echo  'slock is already running'
+  fi
+fi
