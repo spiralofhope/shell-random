0eb5d1c577143229c0e4cbfd21ec0c08ee2f3816
spiralofhope <soh@spiralofhope.com>
2014-05-20 01:40:02 -0700

    reworked to execute under vanilla sh

--- live/slock-capital-punishment.sh
+++ live/slock-capital-punishment.sh
@@ -1,20 +1,30 @@
-#!/usr/bin/env  bash
+#!/usr/bin/env  sh
 
 
 
-# TODO - This can't deal with the process being run by another user and being denied permission to kill their process.
-# Yes, I could also use `pidof`, except it won't work if there are multiple instances or if there is, say, an xautolock which isn't running slock.
-slock_pid=$( \
-  \ps  ax |\
-  \grep  xautolock |\
-  \grep  slock |\
-  \xargs              ` # Trim whitespace ` |\
-  \cut \
-    -d' ' \
-    -f1 \
-)
+get_pid_of_1_2() {
+  # TODO - This can't deal with the process being run by another user and being denied permission to kill their process.
+  # Yes, I could also use `pidof`, except it won't work if there are multiple instances or if there is, say, an xautolock which isn't running slock.
+  \echo  $( \
+    \ps  ax |\
+    \grep  $1 |\
+    \grep  $2 |\
+    \xargs              ` # Trim whitespace ` |\
+    \cut \
+      -d' ' \
+      -f1 \
+  )
+}
 
-\echo  'The slock pid is' $slock_pid
+
+
+slock_pid=$( get_pid_of_1_2  xautolock  slock )
+
+
+if ! [ -z $slock_pid ]; then
+  \echo  'The slock pid is' $slock_pid
+fi
+
 
 \zenity \
   --question \
@@ -22,18 +32,24 @@ slock_pid=$( \
   --text='Do you want slock to be running?'
 if [ $? -eq 1 ]; then
   # no, stop slock
-  if [[ x$slock_pid == 'x' ]]; then
-    \echo  "slock isn't running."
-  else
-    \echo  'killing slock..'
-    \kill  $slock_pid
-  fi
+  case "$slock_pid" in
+    '')
+      \echo  "slock wasn't running.  Doing nothing."
+    ;;
+    *)
+      \echo  'slock was running.  Killing slock..'
+      \kill  $slock_pid
+    ;;
+  esac
 else
   # yes, run slock
-  if [[ x$slock_pid == 'x' ]]; then
-    \echo  'starting slock..'
-    \xautolock  -time 5  -locker slock &
-  else
-    \echo  'slock is already running'
-  fi
+  case $slock_pid in
+    '')
+      \echo  "slock wasn't running.  Starting slock.."
+      \xautolock  -time 5  -locker slock &
+    ;;
+  *)
+    \echo  'slock is already running.  Doing nothing.'
+  ;;
+  esac
 fi
