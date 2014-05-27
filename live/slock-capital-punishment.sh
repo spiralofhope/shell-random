c872d118a0bc5a748f416c9b1f9c5c6e89173042
spiralofhope <soh@spiralofhope.com>
2014-05-27 00:30:21 -0700

    dpms (monitor power saving) support via xset

--- live/slock-capital-punishment.sh
+++ live/slock-capital-punishment.sh
@@ -18,35 +18,43 @@ get_pid_of_1_2() {
 
 
 
-slock_pid=$( get_pid_of_1_2  xautolock  slock )
+xautolock_pid=$( get_pid_of_1_2  xautolock  slock )
 
 
-if ! [ -z $slock_pid ]; then
-  \echo  'The slock pid is' $slock_pid
+if ! [ -z $xautolock_pid ]; then
+  \echo  'The slock pid is' $xautolock_pid
 fi
 
 
 \zenity \
   --question \
   --title='' \
-  --text='Do you want slock to be running?'
+  --text='Do you want slock and monitor power saving to be running?'
 if [ $? -eq 1 ]; then
-  # no, stop slock
-  case "$slock_pid" in
+  # no, stop slock via xautolock
+  case "$xautolock_pid" in
     '')
       \echo  "slock wasn't running.  Doing nothing."
     ;;
     *)
-      \echo  'slock was running.  Killing slock..'
-      \kill  $slock_pid
+      \echo  'slock was running.  Killing slock and disabling power saving..'
+      # Kill xautolock, preventing slock from being triggered.
+      \kill  $xautolock_pid
+      # Disable monitor power saving.
+      \xset  -dpms
     ;;
   esac
 else
-  # yes, run slock
-  case $slock_pid in
+  # yes, run slock via xautolock
+  case $xautolock_pid in
     '')
-      \echo  "slock wasn't running.  Starting slock.."
-      \xautolock  -time 5  -locker slock &
+      \echo  "slock wasn't running.  Starting slock and enabling power saving.."
+      # Enable a 5 minute timer to trigger slock.
+      \xautolock  -time 5  -locker \slock &
+      # Enable monitor power saving.
+      \xset  +dpms
+      # 180 seconds = 5 minutes
+      \xset  dpms 0 0 180 \
     ;;
   *)
     \echo  'slock is already running.  Doing nothing.'
