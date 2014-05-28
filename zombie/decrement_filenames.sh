51a330bc19d5912e185dd2c7f3cf4b2975ffadfc
spiralofhope <soh@spiralofhope.com>
2014-05-28 00:33:05 -0700

    props

--- zombie/decrement_filenames.sh
+++ zombie/decrement_filenames.sh
@@ -6,6 +6,8 @@
 # Where the files are numbered.
 # And the numbering begins at 2 instead of 1.
 # Rename all the files, lowering each by one.
+# Props to
+#   http://www.linuxmisc.com/12-unix-shell/64e578a3b5840b56.htm
 
 
 
