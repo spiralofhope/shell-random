714f68adfec1d1991d0d1b0d00ea6301de227c7f
spiralofhope <soh@spiralofhope.com>
2014-07-12 12:56:04 -0700

    Rewrite.

--- unfinished/mp3-listing/build-flat-directory.sh
+++ unfinished/mp3-listing/build-flat-directory.sh
@@ -1,17 +1,48 @@
-# This won't work because the collection is too large!
-\echo  '* Building flat/'
-\echo  '* Building flat/ -- NOTE: Any file that is mentioned here is a duplicate filename.'
+#!/usr/bin/env  sh
 
-\cd  ../
-# ~DO:  if exist
-# This will fail <----------------------
-# A ruby solution would work!
-\rm  --force  --recursive  ./flat/
-\mkdir  ./flat/
 
-\echo  '* Building flat/ -- tested'
-\find  ../music  -name \*.mp3  -exec \
-  \ln  --symbolic ../{} --target-directory=./flat/ \;
 
-# echo "* Building flat/ -- untested"
-# find ../untested -name \*.mp3 -exec ln -s ../{} --target-directory=./flat/ \;
+#verbose=--verbose
+
+
+
+if [ -z $2 ]; then
+\echo  "
+Turn a series of files/directories into one directory of symbolic links.
+
+$0  <directory>  <targets>
+
+<targets> may be any number of files or directories.
+
+Examples:
+
+$0  scripts  **/*.sh
+"
+  \exit  1
+fi
+
+
+
+target_directory=$1
+shift
+\mkdir  --parents  $target_directory
+if [ $? -ne 0 ]; then
+  \echo  'error'
+  \exit  1
+fi
+
+
+
+_ln() {
+  \ln  --backup  --relative  --symbolic  $verbose  $1  --target-directory=$2
+}
+
+
+
+for i in $@; do
+  if [ -d $i ]; then
+    \find  $i  -L  -type d  -exec  _ln  {}  $target_directory \;
+  else
+    _ln  $i  $target_directory
+  fi
+done
