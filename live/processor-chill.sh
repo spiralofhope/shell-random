4988992cb9b94ff151518274f201e394d1ba66bd
spiralofhope <soh@spiralofhope.com>
2014-07-19 16:01:22 -0700

    Flash likes to hang my computer now that my hardware is damaged.  It cooks everything.  Let's see if this works.

---
+++ live/processor-chill.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env  sh
+
+
+
+# Maybe there's a better way to do this, but I don't know it.
+get_the_number_of_processors() {
+  \echo  $( \
+    \cat  /proc/cpuinfo |\
+      \grep processor |\
+      \tail  --lines=1 |\
+      \cut  --bytes=13- \
+    )
+}
+
+
+
+# --
+
+
+
+\echo -n ' - cpufreq: Cooling down the processor'
+case $( get_the_number_of_processors ) in
+  0)
+    \echo ''
+  ;;
+  *)
+    \echo 's'
+esac
+
+
+# `hardinfo` will prove that the CPU settings will change.
+for i in $( \seq 0 $( get_the_number_of_processors ) ); do
+  \sudo  \echo -n ''
+  \echo  '   chilling processor' $i
+  \sudo  \cpufreq-set  --cpu $i  --governor powersave
+done
