6fe08befbb8ff695b6688341323bdbb290eaa3df
spiralofhope <soh@spiralofhope.com>
2014-11-13 17:22:04 -0800

    It can now play a simple collection of notes.  I could implement a text file inputted chiptune format, but I'm not that crazy.

--- live/alarm.sh
+++ live/alarm.sh
@@ -1,12 +1,37 @@
-#!/bin/sh
-\echo "Alarm started."
+#!/usr/bin/env  sh
+
+
+
+\echo  'Alarm started.'
+
 # TODO:  Is there a way to occasionaly display the remaining time?
 \sleep $*
-# Visual
-\echo "alarm" | \leafpad &
+
+
+# Visual notification.
+# TODO - I could just open a terminal, use Xdialog, zenity or some other such thing.
+\echo  'alarm' | \leafpad &
+
+
 # Audio
-#\speaker-test -t sine -l 1 -p 1
-# TODO - something nicer..
-\speaker-test \
-  --test wav \
-  --nloops 1
+# FIXME - There is popping between notes.
+# TODO? - A mono chiptune.
+#   As I'm not a musician and simple searches have not come up with the "source" frequency/duration for each note for such music, I cannot produce anything easily.
+# TODO? - A stereo chiptune.
+#   It feels possible to essentially create two "threads", and control left and right speakers separately.
+#   Hell, more speakers are probably also possible.
+# TODO? - Experiment with multiple mono threads.  Can multiple instances of speaker-test exist and play overlapping notes simultaneously, simulating multiple channels?
+_alarm() {
+  ( \speaker-test  --frequency $1  --test sine )&
+  pid=$!
+  \sleep  0.${2}s
+  \kill  -9 $pid
+}
+
+
+
+_alarm  400  200
+_alarm  450  300
+_alarm  400  200
+_alarm  450  400
+_alarm  400  400
