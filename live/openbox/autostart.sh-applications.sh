34fc5572f19abc0dc9f9a56991d8c591683420f8
spiralofhope <soh@spiralofhope.com>
2015-03-26 03:59:52 -0700

    A hackish way of opening other todo files.  This'll completely break if I ever switch to another text editor.

--- live/openbox/autostart.sh-applications.sh
+++ live/openbox/autostart.sh-applications.sh
@@ -65,7 +65,8 @@ connected() {
   # TODO: wmctrl and minimize it.  Heck, toss it on another desktop.
   /l/shell-random/git/live/projects.sh
   # This is so bad, but it'll be a decent workaround.
-  \geany  /mnt/1/windows-data/l/live/_outbox--0/_outbox--0.txt
+  \geany  /mnt/1/windows-data/l/live/__/__.txt
+  \geany  /mnt/1/windows-data/l/live/_outbox--1/_outbox--1.txt
 }
 
 
