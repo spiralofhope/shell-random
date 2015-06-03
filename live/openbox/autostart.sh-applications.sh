370e499c51fb51727d95fab137eaf9db2a64723b
spiralofhope <soh@spiralofhope.com>
2015-06-03 13:48:29 -0700

    Shelving the spacefm panel experiment.

--- live/openbox/autostart.sh-applications.sh
+++ live/openbox/autostart.sh-applications.sh
@@ -91,15 +91,19 @@ connected() {
   &
   # Make sure that everything is loaded up before these socket commands are sent.
   \sleep  2
+
+  \spacefm &
+
+# While a really cool idea, it's confusing as all hell for me, and all spacefm instances will have the same panel setup, which is stupid.
   # I don't see how multiple socket commands can be sent in one line.  Bah.
   # I don't know how to set the sizes of each panel.
-  \spacefm  --socket-cmd  set window_maximized true
-  \spacefm  --socket-cmd  set focused_panel 1
-  \spacefm  --socket-cmd  set focused_pane filelist
-  \spacefm  --socket-cmd  set panel1_visible true
-  \spacefm  --socket-cmd  set panel2_visible true
-  \spacefm  --socket-cmd  set panel3_visible true
-  \spacefm  --socket-cmd  set panel4_visible true
+#  \spacefm  --socket-cmd  set window_maximized true
+#  \spacefm  --socket-cmd  set focused_panel 1
+#  \spacefm  --socket-cmd  set focused_pane filelist
+#  \spacefm  --socket-cmd  set panel1_visible true
+#  \spacefm  --socket-cmd  set panel2_visible true
+#  \spacefm  --socket-cmd  set panel3_visible true
+#  \spacefm  --socket-cmd  set panel4_visible true
 }
 
 
