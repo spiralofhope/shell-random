84b14267f960e45e48d61ce861f9be763f51c777
spiralofhope <soh@spiralofhope.com>
2015-06-05 14:23:04 -0700

    Moving the spacefm stuff out, and correcting the live stuff.

--- live/openbox/autostart.sh-applications.sh
+++ live/openbox/autostart.sh-applications.sh
@@ -68,42 +68,13 @@ connected() {
   \geany  /mnt/1/windows-data/l/live/__/__.txt
   \geany  /mnt/1/windows-data/l/live/_outbox--1/_outbox--1.txt
 
-
   \spacefm \
+    --panel=1 \
     --new-window \
-    --panel=1  --no-saved-tabs \
-      /mnt/1/windows-data/l/live/_outbox--1/ \
-  &
-  # Make sure that multiple spacefm windows don't spawn.
-  \sleep  1
-  \spacefm \
-    --panel=2  --no-saved-tabs  --reuse-tab \
-      /1/__/ \
-  &
-  \spacefm \
-    --panel=3  --reuse-tab \
-      /1/ \
-  &
-  \spacefm \
-    --panel=4  --reuse-tab \
-      /mnt/1/windows-data/l/live/__/ \
-      /l/_outbox--0/ \
-  &
-  # Make sure that everything is loaded up before these socket commands are sent.
-  \sleep  2
+    --no-saved-tabs \
+    --reuse-tab \
+    /l/
 
-  \spacefm &
-
-# While a really cool idea, it's confusing as all hell for me, and all spacefm instances will have the same panel setup, which is stupid.
-  # I don't see how multiple socket commands can be sent in one line.  Bah.
-  # I don't know how to set the sizes of each panel.
-#  \spacefm  --socket-cmd  set window_maximized true
-#  \spacefm  --socket-cmd  set focused_panel 1
-#  \spacefm  --socket-cmd  set focused_pane filelist
-#  \spacefm  --socket-cmd  set panel1_visible true
-#  \spacefm  --socket-cmd  set panel2_visible true
-#  \spacefm  --socket-cmd  set panel3_visible true
-#  \spacefm  --socket-cmd  set panel4_visible true
 }
 
 
