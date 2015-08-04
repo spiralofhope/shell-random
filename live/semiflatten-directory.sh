4e3bd725b479ed297db095b5d4a9087968fa771f
spiralofhope <soh@spiralofhope.com>
2015-08-04 03:27:17 -0700

    Now with more double-quotes.

--- live/semiflatten-directory.sh
+++ live/semiflatten-directory.sh
@@ -12,14 +12,14 @@ _go() {
   if [ $(\ls "$1" | wc -l) -ne 1 ]; then
     continue
   fi
-  target=./$1/
-  source=./$1/$( ls  --directory  ./$1/ )
+  target=./"$1"/
+  source=./"$1"/$( ls  --directory  ./"$1"/ )
   # TODO? - If the one subdirectory is named the same as its parent
   #         I don't think I'll do this.
   #\echo  " * Processing $1"
   # Move that one subdirectory's contents into its parent ($1)
   # FIXME? - I don't know why I can't double-quote $source
-  \mv  $source/*  "$target"
+  \mv  "$source"/*  "$target"
   # Remove the now-empty directory
   \rmdir  "$source"
 }
