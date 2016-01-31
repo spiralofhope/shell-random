e7831b7dab582ed59ac4b219e782294d05e603aa
spiralofhope <spiralofhope@gmail.com>
2016-01-31 10:25:23 -0800

    Though I haven't used it in.. years probably, editing the lib and moving stuff.  It'll probably still work in zsh.  Probably.

--- live/zsh/lib.sh
+++ live/zsh/lib.sh
@@ -1,24 +1,6 @@
 # Used by other functions.
-# Loaded first, before everything.
+# Loaded before anything else "zsh"-specific, and after everything "bash and zsh".
 
-# TODO - Fucking hell, `du` has trailing spaces and the name of the directory.  It even puts '.' if nothing is specified.
-# TODO - remove a trailing period.
-# TODO - remove trailing spaces.
-comma() {
-  if [ -z $1 ]; then
-    # Incorrect usage.
-  elif [ -z $2 ]; then
-    \echo "$1" | \sed -e :a -e 's/\(.*[0-9]\)\([0-9]\{3\}\)/\1,\2/;ta'
-  else
-    # Incorrect usage.
-  fi
-}
-# Although at the commandline, this works:
-#   comma 1000
-# This is the required way to use it when scripting:
-#   comma '1000'
-# Or more complex:
-#   local count=$( comma $( \ls -1 . | \wc -l ) )
 
 
 # http://www.codecodex.com/wiki/Shuffle_an_array#Zsh
