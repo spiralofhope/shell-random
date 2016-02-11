fe306a50d4ac2515123481e261def31214c8bf8d
spiralofhope <spiralofhope@gmail.com>
2016-02-11 14:20:16 -0800

    Screw all this complexity.

--- live/bash-windows/_dotfiles/go.cmd
+++ live/bash-windows/_dotfiles/go.cmd
@@ -13,40 +13,11 @@ SET  "TARGET=C:\Users\user\"
 
 
 
-:: ----
-:: Elevate privileges
-:: ----
-::   http://blog.spiralofhope.com?p=13553
-::   https://stackoverflow.com/questions/7044985
-::     http://stackoverflow.com/a/12264592/1190568
-:checkPrivileges
-NET FILE 1>NUL 2>NUL
-if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
-
-:getPrivileges
-if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
-ECHO.
-ECHO **************************************
-ECHO Invoking UAC for Privilege Escalation
-ECHO **************************************
-
-setlocal DisableDelayedExpansion
-set "batchPath=%~0"
-setlocal EnableDelayedExpansion
-ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\OEgetPrivileges.vbs"
-ECHO args = "ELEV " >> "%temp%\OEgetPrivileges.vbs"
-ECHO For Each strArg in WScript.Arguments >> "%temp%\OEgetPrivileges.vbs"
-ECHO args = args ^& strArg ^& " "  >> "%temp%\OEgetPrivileges.vbs"
-ECHO Next >> "%temp%\OEgetPrivileges.vbs"
-ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs"
-"%SystemRoot%\System32\WScript.exe" "%temp%\OEgetPrivileges.vbs" %*
-exit /B
-
-:gotPrivileges
-if '%1'=='ELEV' shift /1
-setlocal & pushd .
-cd /d %~dp0
-:: ----
+NET FILE 1>NUL 2>NUL & IF ERRORLEVEL 1 (
+  ECHO Right-click and run this as administrator.
+  PAUSE
+  EXIT /B
+)
 
 
 
