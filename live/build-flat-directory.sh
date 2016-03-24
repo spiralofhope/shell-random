c5c69eb97497f6acce3c6aeb2c569ca42ae31a5f
spiralofhope <soh@spiralofhope.com>
2016-03-24 12:37:06 -0700

    Chippnig the cruft off.  A simple sh => zsh was the big fix.  Whoops.

--- live/build-flat-directory.sh
+++ live/build-flat-directory.sh
@@ -1,10 +1,29 @@
-#!/usr/bin/env  sh
+#!/usr/bin/env  zsh
+
+
+
+# 2016-03-24 - Tested and works under:
+#   zsh 5.0.2 (x86_64-pc-linux-gnu)
+
+
+# Problem:
+#
+# * Given a directory
+#   **  with many subdirectories
+#   **  where there are many files
+# * Build one single directory
+#   **  which has a flat list of links to those files
+#   **  by 'flat', I mean that directory has no subdirectories
+# * Allow the processing of specific filenames
+#   **  using fancy filename wildcards would be nice
+
+
+# TODO - If there are filename collisions, automatically rename them.
+#        ln has numbered backups
 
 
 
 #verbose=--verbose
-
-
 
 if [ -z $2 ]; then
 \echo  "
