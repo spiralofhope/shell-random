f834bcb16021668e1b5c3859f118a2fe3a85ea39
spiralofhope <spiralofhope@gmail.com>
2016-03-30 09:30:37 -0700

    I hear /tmp/ is a good place to put tmp files.

--- live/gui-yesno-dialog.sh
+++ live/gui-yesno-dialog.sh
@@ -12,16 +12,19 @@ width=0
 #height=5
 #width=19
 text="$1"
+# TODO - there's some thingy thing that GNU has to make one of these 
+# safely-named
+_tmpfile=/tmp/$$.tmp
 
 
 # I suspect I could have solved this with `read`, but I think I'd have 
 # to have a second script.
 
 \urxvt  -geometry 239x64  -e \
-  \dialog  --yesno  "$text"  $height  $width  --trace  $$.tmp 2>/dev/null
+  \dialog  --yesno  "$text"  $height  $width  --trace  $_tmpfile 2>/dev/null
 
-if [ -f $$.tmp ]; then
-  \rm  --force  $$.tmp
+if [ -f $_tmpfile ]; then
+  \rm  --force  $_tmpfile
   \echo  '0'
 else
   \echo  '1'
