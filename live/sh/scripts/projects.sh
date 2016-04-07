d3304072387ac08a82ada2970ac499e290c025c4
spiralofhope <spiralofhope@gmail.com>
2016-04-07 16:54:02 -0700

    A complete rewrite:  speed, directories and paths with spaces, portability, and mostly for a single-window of geany.

--- live/sh/scripts/projects.sh
+++ live/sh/scripts/projects.sh
@@ -2,113 +2,108 @@
 
 
 
-# sh   - This will work well at the commandline.
-# sh   - In Openbox, opened as a hotkey, this will open multiple instances, with the bulk being in the last one opened.
-# zsh  - This will be erratic as all fuck.
-# bash - This will open two instances of random shit.
+# Goal
+# Given a series of directories.
+# Open a text file located within each directory.
+# Prefer loading some files first, so they will appear as the first tabs.
+
+
+
+# FIXME - If run multiple times, it will attempt to open a new tab with a nonexistent file.
+#         This is likely because I have a leading or trailing printf in the final command.
 
 
 
 setup() {
-  PROJECTS="/l"
+  \echo  " * begin"
+  projects_directory="/l"
+  windows_projects_directory="/mnt/1/windows-data/l/live"
   NEW_PROJECT_MESSAGE="New project notes started `date`"
-}
-
-
-
-cache_file() {
-  \cat  "$1"  > /dev/null
-}
-
-
-
-# Run before any other project text file is opened.
-editor_setup() {
-  cache_file  "$PROJECTS"/todo.txt
-  cache_file  "$PROJECTS"/projects.txt
-  cache_file  /1/_outbox--0/_outbox--0.txt
-  cache_file  /mnt/1/windows-data/l/live/_outbox--0/_outbox--0.txt
-  cache_file  /mnt/1/windows-data/l/live/_outbox--1/_outbox--1.txt
-
-  # I need to make sure the geany process is running, otherwise another attempt to run geany may open a separate instance of it.
-  # FIXME - is there a more graceful way to do this?  I just want to wait that a pid exists.
-  \sleep  2
-  \geany &
-
-  # Ensuring the first two tabs are these:
-  \geany  "$PROJECTS"/todo.txt &
-  \geany  "$PROJECTS"/projects.txt &
-  \geany  /l/_outbox--0/_outbox--0.txt &
-  \geany  /mnt/1/windows-data/l/live/_outbox--0/_outbox--0.txt &
-  \geany  /mnt/1/windows-data/l/live/_outbox--1/_outbox--1.txt &
-}
-
-
-
-# Run after all projects have been processed.
-editor_finish() {
-  # Switch back to the first tab.
-  \geany  "$PROJECTS"/todo.txt &
-}
-
-
-
-editor_open_file() {
-  \geany  "$1" &
-}
-
-
-
-process_projects() {
-  if [ ! -d "$PROJECTS" ]; then
-    \echo  "ERROR:  \"${PROJECTS}\" does not exist!"
-    return  1
-  fi
-  if [ ! -f "${PROJECTS}/projects.txt" ]; then
-    \echo  "ERROR:  \"${PROJECTS}/projects.txt\" does not exist!"
-    return  1
-  fi
-
-  \cd  "$PROJECTS"
-
-  # Make 'for' case-insensitive
+  \cd  "$projects_directory"
+  # Case-insensitivity.
   LC_COLLATE=en_US ; export LC_COLLATE
+  # IFS (Internal Field Separator), change to a carriage return.
+  IFS_original=$IFS
+  IFS=`printf "\r"`
+}
+
+
+build_array_of_directories() {
   for i in *; do
-    if   [ ! -d "$i" ]; then
+    # Skip non-directories
+    # Note that this will not skip symlinks.
+    if ! [[ -d "$i" ]]; then
+      \echo  "skipping non-directory  $i"
       continue
-    elif [ ! -f "$i/$i".txt ]; then
-      cache_file  "$1"
     fi
+    #\echo  "processing - $i"
+    # I don't know why this won't work:
+    #array_of_directories="$array_of_directories$IFS$i"
+    # Technically I shouldn't be adding a \r to the beginning of the array, but it doesn't seem to matter.
+    array_of_directories="$array_of_directories`printf \"\r\"`$i"
   done
+  #\echo  "$array_of_directories"
 
-  for i in *; do
-    \echo  " * Processing $i"
+  #for i in $array_of_directories; do
+    #echo ++$i
+  #done
+  #return 0
+}
 
-    # This also works for symbolic links which point to a directory.
-    # FIXME - clarify the above statement.
-    if [ ! -d "$i" ]; then
-      \echo  "skipping non-directory $i"
+
+build_array_of_files() {
+  for i in $array_of_directories; do
+    if [[ $i == "" ]]; then continue; fi
+    i="$i/$i.txt"
+
+    # Create files as necessary
+    # TODO? - Skip files in an array of exclusions
+    # TODO - check if this will work properly with symlinks.
+    if ! [[ -f "$i" ]]; then
+      \echo .
+      \echo  "   New project $i, inserting message:"
+      \echo  "$NEW_PROJECT_MESSAGE"
+      \echo .
+      \touch  "$i"
+      \echo  "$NEW_PROJECT_MESSAGE" >> "$i"
+    fi
+    local  size_of_file=` \stat  --printf="%s"  "$i"  |  \cut -f 1 `
+    if [[ $size_of_file == 0 ]] ; then
+      \echo  "skipping 0-byte $i"
       continue
-    # TODO - design some other way to skip directories without needing a file in that directory.
-    elif [ "$i" = "e" ] || [ "$i" = "e_p" ]  ; then
-      continue
     fi
-
-    # If there is a file, and it is 0-byte, then don't open it.
-    if [ -e "$i/$i.txt" ]; then
-      local  size_of_file=$( \stat  --printf="%s"  "$i/$i.txt"  |  \cut -f 1 )
-      if [ $size_of_file -eq 0 ] ; then
-        \echo  "skipping 0-byte $i/$i.txt"
-        continue
-      fi
-    fi
-
-    if [ ! -f "$i/$i".txt ]; then
-      \echo  "   New project $i, inserting message:\n   $NEW_PROJECT_MESSAGE"
-      \echo  "$NEW_PROJECT_MESSAGE" > "$i/$i.txt"
-    fi
-    editor_open_file  "$i/$i.txt"
+    #\echo  "processing - $i"
+    array_of_files="$array_of_files`printf \"\r\"`$i"
   done
+  #\echo  $array_of_files
+
+  #for i in $array_of_files; do
+    #echo ++$i
+  #done
+  #return 0
+}
+
+
+process_array_of_files() {
+  # Ensure these are the first tabs:
+  # TODO? - Yes this could be made cleaner, but at this point I don't give a fuck.
+  array_of_files="todo.txt`printf \"\r\"`projects.txt`printf \"\r\"`_outbox--0/_outbox--0.txt`printf \"\r\"`$windows_projects_directory/_outbox--0/_outbox--0.txt`printf \"\r\"`$windows_projects_directory/_outbox--1/_outbox--1.txt`printf \"\r\"`$array_of_files"
+
+  # Switch to the first tab:
+  array_of_files="$array_of_files`printf \"\r\"`todo.txt"
+}
+
+
+open_array_of_files() {
+  #echo  \geany  $array_of_files
+  \geany  $array_of_files
+}
+
+
+
+teardown() {
+  IFS=$IFS_original
+  \echo  " * end"
 }
 
 
@@ -118,6 +113,8 @@ process_projects() {
 # --
 
 setup
-editor_setup
-process_projects
-editor_finish
+build_array_of_directories
+build_array_of_files
+process_array_of_files
+open_array_of_files
+teardown
