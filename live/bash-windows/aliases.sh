e5eed40c6e2fd631ce806b0bb199b5c0a5a12242
spiralofhope <spiralofhope@gmail.com>
2016-05-13 13:36:19 -0700

    Removing cruft

--- live/bash-windows/aliases.sh
+++ live/bash-windows/aliases.sh
@@ -3,8 +3,12 @@
 # GNU bash, version 4.3.42(5)-release (x86_64-pc-msys)
 
 
+alias  addons='\cd  /c/l/live/World_of_Warcraft/_dotfiles/Interface/AddOns'
+
+
+
 # --
-# -- Windows console Applications
+# --  Windows console applications
 # --
 
 
@@ -12,12 +16,16 @@
 # https://coderwall.com/p/ee-law/use-nano-from-git-bash-on-windows-d
 # http://vijayparsi.wordpress.com/2012/03/07/nano-with-mingw32-for-msysgit/
 #   \curl -L -O http://www.nano-editor.org/dist/v2.2/NT/nano-2.2.6.zip
-alias   nano="${GIT_DIRECTORY}/bin/nano/nano.exe  --autoindent  --nowrap  --tabsize=2  --tabstospaces"
-alias fdupes="${GIT_DIRECTORY}/bin/fdupes.exe"
+# It doesn't work on this present 64-bit setup
+#alias   nano="${GIT_DIRECTORY}/bin/nano/nano.exe  --autoindent  --nowrap  --tabsize=2  --tabstospaces"
+
+# Fdupes, duplicate file checker/deleter
+# TODO
+alias  fdupes="${GIT_DIRECTORY}/bin/fdupes.exe"
 
 # Tortoise, thingy TODO
 # http://tortoisesvn.net/downloads.html
-alias svn="${PF}/TortoiseSVN/bin/svn.exe"
+alias  svn="${PF}/TortoiseSVN/bin/svn.exe"
 
 # Mercurial, version control
 # http://mercurial.selenic.com/wiki/Download
@@ -28,17 +36,19 @@ alias  hg="${PF}/\TortoiseHg/hg.exe"
 # http://sourceforge.net/projects/mcwin32/
 alias  mc="${PFx}/Midnight\ Commander/mc.exe"
 
-alias  geany="${PFx}/Geany/bin/Geany.exe"
-
 # 7zip compression, filename.7z
 # http://www.7-zip.org/
 alias  7z="${PF}/7-Zip/7z.exe"
 
-alias rename='/c/l/live/shell-random/git/live/rename.pl'
+alias  rename='/c/l/live/shell-random/git/live/rename.pl'
 
-alias alarm='/c/l/live/shell-random/git/live/bash-windows/scripts/alarm.sh'
+alias  rsync='/c/l/live/Git_For_Windows/bin/cwRsync/rsync.exe'
 
-alias rsync='/c/l/live/Git_For_Windows/bin/cwRsync/rsync.exe'
 
-alias addons='\cd  /c/l/live/World_of_Warcraft/_dotfiles/Interface/AddOns'
 
+# --
+# --  Windows GUI applications
+# --
+
+
+alias  geany="${PFx}/Geany/bin/Geany.exe"
