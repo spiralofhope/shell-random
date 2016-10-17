16b51cfd0281d4b8fdc04e0e726279bd2f1bd3d2
spiralofhope <spiralofhope@gmail.com>
2016-10-17 01:48:39 -0700

    Removing more hardcoding.

--- live/sh/mine.sh
+++ live/sh/mine.sh
@@ -7,18 +7,18 @@
 
 findfile() {
   if [[ -d $1 ]]; then
-    /c/l/live/shell-random/git/live/sh/scripts/findhelper.sh  file      $*
+    ${shell_random}/live/sh/scripts/findhelper.sh  file      $*
   else
-    /c/l/live/shell-random/git/live/sh/scripts/findhelper.sh  file  ./  $*
+    ${shell_random}/live/sh/scripts/findhelper.sh  file  ./  $*
   fi
 }
 
 
 finddir() {
   if [[ -d $1 ]]; then
-    /c/l/live/shell-random/git/live/sh/scripts/findhelper.sh  directory      $*
+    ${shell_random}/live/sh/scripts/findhelper.sh  directory      $*
   else
-    /c/l/live/shell-random/git/live/sh/scripts/findhelper.sh  directory  ./  $*
+    ${shell_random}/live/sh/scripts/findhelper.sh  directory  ./  $*
   fi
 }
 
@@ -28,8 +28,8 @@ finddir() {
 
 findinall() {
   if [[ -d $1 ]]; then
-    /c/l/live/shell-random/git/live/sh/scripts/findhelper.sh  file_contents      $*
+    ${shell_random}/live/sh/scripts/findhelper.sh  file_contents      $*
   else
-    /c/l/live/shell-random/git/live/sh/scripts/findhelper.sh  file_contents  ./  $*
+    ${shell_random}/live/sh/scripts/findhelper.sh  file_contents  ./  $*
   fi
 }
