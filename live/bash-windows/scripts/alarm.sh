48cf52a632be33361a9a2ab2f6af51b1f0de6af1
spiralofhope <spiralofhope@gmail.com>
2016-10-17 01:17:33 -0700

    Less hardcoding.

--- live/bash-windows/scripts/alarm.sh
+++ live/bash-windows/scripts/alarm.sh
@@ -11,8 +11,8 @@
 mshta.exe  "javascript:var sh=new ActiveXObject( 'WScript.Shell' ); sh.Popup( 'alarm', 10, '', 64 );close()"
 
 
-if [[ -f '/c/Program Files/VideoLAN/VLC/vlc.exe' ]]; then
-    /c/Program\ Files/VideoLAN/VLC/vlc.exe  --play-and-exit  "$WINDIR"/media/Notify.wav  vlc://quit   &> /dev/null
+if [[ -f "${PF}/VideoLAN/VLC/vlc.exe" ]]; then
+    ${PF}/VideoLAN/VLC/vlc.exe  --play-and-exit  "$WINDIR"/media/Notify.wav  vlc://quit   &> /dev/null
 else
   echo 'No audio player was found.  Edit this script to add one.'
 fi
