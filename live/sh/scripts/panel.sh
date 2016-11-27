fcb21b7b6b51a3935bd621b1a690dd6145747881
spiralofhope <spiralofhope@gmail.com>
2016-11-27 12:32:32 -0800

    Devuan support

--- live/sh/scripts/panel.sh
+++ live/sh/scripts/panel.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env  sh
 
-# 2016-11-25 on Slackware 14.1
-
 # TODO - given an array of strings, run the first program found.
 # `setsid`  is to force it to run in its own session, so that there's no lingering zsh process.
-  \setsid  \lxpanel &
+echo "trying lxpanel"
+\setsid  \lxpanel &
+killall -0 lxpanel
+
 if [ $? -ne 0 ]; then
+  echo "trying fbpanel"
   \setsid  \fbpanel &
 fi
+killall -0 fbpanel
+
+
 if [ $? -ne 0 ]; then
+  echo "trying xfce4-panel"
   \setsid  \xfce4-panel &
 fi
+killall -0 xfce4-panel
+
 if [ $? -ne 0 ]; then
+  echo "trying tint2"
   \setsid  \tint2 &
 fi
+killall -0 tint2
