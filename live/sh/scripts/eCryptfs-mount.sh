0e0c0df3ba9ba9754a0157d7b50aee08705edace
spiralofhope <soh@spiralofhope.com>
2016-11-28 17:00:48 -0800

    removing reliance on sudo

--- live/sh/scripts/eCryptfs-mount.sh
+++ live/sh/scripts/eCryptfs-mount.sh
@@ -6,24 +6,24 @@
 # Because it's annoying to remember this shit.
 
 
-if ! [ -d "$1" ]; then
+if ! [ -d "$*" ]; then
   \echo  " * That directory doesn't exist, making it.."
-  \mkdir  --verbose  "$1"
+  \mkdir  --verbose  "$*"
 fi
 
-
-# $1 is the name of the directory
+# $* is the name of the directory
 
 # This works for
 #   - creating a new encrypted folder
 #   - mounting an existing encrypted folder
-\sudo \
+# \sudo \
+/bin/su  --command "\
   mount  -t ecryptfs \
-    "$1"  "$1" \
+    \"$*\"  \"$*\" \
     ` # Ask for the user to type in a password ` \
     -o key=passphrase \
     -o ecryptfs_cipher=aes \
     -o ecryptfs_enable_filename_crypto \
     -o ecryptfs_key_bytes=32 \
     -o ecryptfs_passthrough=n \
-` # `
+"` # `
