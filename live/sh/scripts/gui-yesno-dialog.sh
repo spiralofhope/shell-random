ecfc87e2454b47504542b5bf97bfdb150881fc1f
spiralofhope <spiralofhope@gmail.com>
2017-06-30 03:25:39 -0700

    Decruft.  Apparently this was unused since a switch to Devuan.  Proper sh compatibility.

--- live/sh/scripts/gui-yesno-dialog.sh
+++ live/sh/scripts/gui-yesno-dialog.sh
@@ -14,17 +14,19 @@ width=0
 text="$1"
 # TODO - there's some thingy thing that GNU has to make one of these 
 # safely-named
-_tmpfile=/tmp/$$.tmp
+_tmpfile="/tmp/$$.tmp"
 
 
 # I suspect I could have solved this with `read`, but I think I'd have 
 # to have a second script.
 
-\urxvt  -geometry 239x64  -e \
-  \dialog  --yesno  "$text"  $height  $width  --trace  $_tmpfile 2>/dev/null
 
-if [ -f $_tmpfile ]; then
-  \rm  --force  $_tmpfile
+# TODO - use alternate terminals..
+\urxvt  -geometry 239x64  -e \
+  \dialog  --yesno  "$text"  "$height"  "$width"  --trace  "$_tmpfile" 2>/dev/null
+
+if [ -f "$_tmpfile" ]; then
+  \rm  --force  "$_tmpfile"
   \echo  '0'
 else
   \echo  '1'
