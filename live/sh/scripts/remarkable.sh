6f751f1aaed953b50904a0b46fa025fc1126405e
spiralofhope <spiralofhope@gmail.com>
2017-07-03 17:57:17 -0700

    A better timestamp, and a good working-over.

--- live/sh/scripts/remarkable.sh
+++ live/sh/scripts/remarkable.sh
@@ -2,16 +2,33 @@
 
 
 
-# Problem:
-# Begin a timer, incrementing seconds.
-# Record keystrokes to a file, stamped with the timer
-
-
-
 #DRY_RUN=true
 
 
-# ---------------------------------------------------------------------
+
+:<<'NOTES'
+
+- Begin a timer
+- Record keystrokes to a file
+- Record a timer to that file
+
+
+Requirements:
+  touch
+  date
+  expr
+  dd
+
+
+Tested on:
+  1. Windows 10 git-bash:
+     Git-2.10.1-64-bit
+     GNU bash, version 4.3.46(2)-release (x86_64-pc-msys)
+  2. dash - (version not recorded)
+
+NOTES
+
+
 
 setup() {
   _start_time=$( \expr $( \date  +%s%N ) / 1000000000 )
@@ -20,51 +37,45 @@ setup() {
 
   \echo  " * Begin"
   \echo  "   -----"
-  \echo  "Press any key to log that key"
+  \echo  "Press any key to log it"
   \echo  "Press ESCAPE to exit"
   \echo  ''
   if [ -z "$DRY_RUN" ]; then
     \touch  "$_filename"
-    \echo  "$_date_time" >> "$_filename"
-    \echo  "Writing to file:  $_filename"
-    \echo  ''
+    # Formatted as comma-separated values (CSV)
+    # The header
+    \echo  "character pressed,hh:mm:ss,elapsed seconds,$_date_time" >> "$_filename"
+    \echo  "Writing to $_filename"
   else
     \echo  "In dry run mode, not writing to a file."
-    \echo  ''
   fi
+  \echo  ''
 }
 
 
 
 go() {
-  until [ "$character" = "" ]; do
+  # This is a literal escape character embedded into this file.  While there may be another way to do this to avoid complications with a text file that has a binary thing in it, I don't know of it offhand and it doesn't seem to matter.
+  until [ "$character_pressed" = "" ]; do
     read_char() {
-      \stty -icanon -echo
+      \stty  -icanon -echo
       \eval  "$1=\$( \dd  bs=1  count=1 2>/dev/null )"
-      \stty icanon echo
+      \stty  icanon echo
     }
-    read_char character
+    read_char  character_pressed
     _current_time=$( \expr $( \date  +%s%N ) / 1000000000 )
-    _elapsed_seconds=$( \expr $_current_time - $_start_time )
-    #_date_time=$( \date +%Y-%m-%d--%H-%M-%S )
-    if [ "$DRY_RUN" = '' ]; then
-      #\echo  "$character,$_elapsed_seconds,$_date_time" >> "$_filename"
-      \echo  "~~$character - $_elapsed_seconds" >> "$_filename"
+    _elapsed_total_seconds=$( \expr $_current_time - $_start_time )
+    _hh_mm_ss=$( \date -d@$_elapsed_total_seconds -u +%H:%M:%S )
+    # Formatted as comma-separated values (CSV)
+    _output_string="$character_pressed,$_hh_mm_ss,$_elapsed_total_seconds"
+    \echo  "$_output_string"
+    if [ -z "$DRY_RUN" ]; then
+      \echo  "~~$_output_string" >> "$_filename"
     fi
-    #\echo  "$character,$_elapsed_seconds,$_date_time"
-    \echo  "$character - $_elapsed_seconds"
   done
 }
 
 
+
 setup
 go
-
-
-#\echo 'Enter a project name (optional)'
-#\read  -p "> "  _project_name
-#if [ -z "$_project_name" ]; then
-  #\echo  "No project name specified, using default 'remarkable'"
-  #_project_name="remarkable"
-#fi
-#_filename="$_project_name"--"$_date_time".txt
