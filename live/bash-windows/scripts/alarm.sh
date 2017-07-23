0d43a86daebaeef3204da5b69bbe9c822449959b
spiralofhope <spiralofhope@gmail.com>
2017-07-23 05:12:50 -0700

    Structure.

--- live/bash-windows/scripts/alarm.sh
+++ live/bash-windows/scripts/alarm.sh
@@ -2,6 +2,11 @@
 
 
 
+if ! [[ -f "${PF}/VideoLAN/VLC/vlc.exe" ]]; then
+  \echo  'No audio player was found.  Edit this script to add one.'
+  \exit  1
+fi
+
 \echo  'Alarm started.'
 
 # TODO:  Is there a way to occasionaly display the remaining time?
@@ -11,8 +16,4 @@
 mshta.exe  "javascript:var sh=new ActiveXObject( 'WScript.Shell' ); sh.Popup( 'alarm', 10, '', 64 );close()"
 
 
-if [[ -f "${PF}/VideoLAN/VLC/vlc.exe" ]]; then
-    ${PF}/VideoLAN/VLC/vlc.exe  --play-and-exit  "$WINDIR"/media/Notify.wav  vlc://quit   &> /dev/null
-else
-  echo 'No audio player was found.  Edit this script to add one.'
-fi
+${PF}/VideoLAN/VLC/vlc.exe  --play-and-exit  "$WINDIR"/media/Notify.wav  vlc://quit   &> /dev/null
