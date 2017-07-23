dc4093a7a2352ce8bc46dbdb5879b3042a94d66f
spiralofhope <spiralofhope@gmail.com>
2017-07-23 06:26:49 -0700

    Implement a wrapper around 'sleep' so that it can occasionally display progress.

--- live/zsh/scripts/alarm.sh
+++ live/zsh/scripts/alarm.sh
@@ -18,26 +18,41 @@ _sleep() {
   startDateTime=$( \date  --date "now"      )
   if [ -z "$*" ]; then
     _echo  "sleeping for 1s (default)"
-    timeToWait=1s
+    sleep_duration=1s
   elif [ "$1" = '--date' ]; then
     shift
     _echo  "sleeping until $*"
     # https://stackoverflow.com/questions/645992
     endTime=$(   \date  --date "$*"  +%s )
-    timeToWait=$(( $endTime - $startTime ))
-    if [ "$timeToWait" -lt 0 ]; then
+    sleep_duration=$(( $endTime - $startTime ))
+    if [ "$sleep_duration" -lt 0 ]; then
       \echo  'ERROR:  Time is in the past'
       _echo  "sleeping for 1s (default)"
-      timeToWait=1
+      sleep_duration=1
     fi
-    timeToWait=${timeToWait}s
-    # TODO:  Is there a way to occasionaly display the remaining time?
-    _echo  '' $startTime 'seconds\n' $endTime 'seconds to wait\n' $timeToWait
+    sleep_duration=${sleep_duration}s
+    _echo  '' $startTime 'seconds\n' $endTime 'seconds to wait\n' $sleep_duration
   else
-    _echo  "sleeping for $*"
-    timeToWait="$*"
+    sleep_duration="$*"
+    _echo  "sleeping for $sleep_duration"
   fi
-  \sleep  $timeToWait
+
+  # Wrap the 'sleep' command, so I can occasionally display how long it's been sleeping for.
+  \sleep  $sleep_duration & _pid=$!
+  _elapsed_time_seconds=0
+  until [ $? = 1 ]; do
+    \sleep 1
+    _elapsed_time_seconds=$(( $_elapsed_time_seconds + 1 ))
+    _echo_every_x_seconds=10
+    if [ $(( $_elapsed_time_seconds % $_echo_every_x_seconds )) -eq 0 ]; then
+      # IDEA - Be fancy.  But that's likely way too annoying.  I'd have to collect all parameters to 'sleep' (like '\sleep 1h 3m 2s' )to convert them to seconds, then convert that back up to h:m:s
+      # It's possible to force the user into one consistent format that's more strict than what 'sleep' allows, but that seems wrong to me.
+      # If I manage fancyness, I can display the time remaining.
+      \echo  "slept for $_elapsed_time_seconds seconds out of $sleep_duration"
+    fi
+    \ps  --pid $_pid | \grep  sleep  > /dev/null
+  done
+
   endDateTime=$( \date )
 }
 
@@ -68,7 +83,11 @@ _sleep  $*
 _audio_alert
 # Just in case
 \killall  -9 speaker-test 2> /dev/null
-_visual_alert  "Alarm started at:  $startDateTime  \nAlarm waited for:  $timeToWait  \nAlarm rang at:     $endDateTime"
+_visual_alert  "Alarm started at:  $startDateTime  \nAlarm waited for:  $sleep_duration  \nAlarm rang at:     $endDateTime"
+
+
+
+
 
 
 # To learn the number of seconds remaining, probably:
