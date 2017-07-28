be643f4f4f30d07858bbed9a355e1b3994a47b2f
spiralofhope <spiralofhope@gmail.com>
2017-07-28 15:26:51 -0700

    cleanup

--- live/sh/scripts/examples/conditionals.sh
+++ live/sh/scripts/examples/conditionals.sh
@@ -3,16 +3,37 @@
 
 
 # dash 0.5.7-4+b1
-if [ $stringa = $stringb ]; then
-  echo equal
-elif [ ! $stringa = $stringb ]; then
-  echo not equal
-else
-  echo something else
+__='a'
+#__=
+if [ x"$__" = "x" ]; then echo 'variable IS NOT set'
+else                      echo 'variable IS set'
 fi
 
 
+
 # dash 0.5.7-4+b1
-if [ a = a ] && [ b = b ]; then echo ok; fi
+stringa='a'
+#stringb='a'
+stringb='b'
+if   [   $stringa = $stringb ]; then echo 'variable strings ARE equal'
+elif [ ! $stringa = $stringb ]; then echo 'variable strings ARE NOT equal'
+fi
 
 
+
+# dash 0.5.7-4+b1
+__=1
+if [ $__ -eq 1 ]; then echo 'EQUAL TO a number'; fi
+if [ $__ -lt 2 ]; then echo 'LESS THAN a number'; fi
+__=2
+if [ $__ -gt 1 ]; then echo 'GREATER THAN a number'; fi
+
+
+
+# dash 0.5.7-4+b1
+if [ a = a ] && [ b = b ]; then echo 'both'; fi
+
+
+
+# dash 0.5.7-4+b1
+if [ a = a ] || [ a = b ]; then echo 'either'; fi
