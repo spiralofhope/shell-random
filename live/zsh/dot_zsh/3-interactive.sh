4dd2980e7d65dc87ac6c9bb4dec160d93055c7cd
spiralofhope <spiralofhope@gmail.com>
2017-10-10 13:16:43 -0700

    .

--- live/zsh/dot_zsh/3-interactive.sh
+++ live/zsh/dot_zsh/3-interactive.sh
@@ -22,6 +22,10 @@ SAVEHIST=10000
 #   https://www.reddit.com/r/commandline/comments/4knoj4/
 # HISTCONTROL=ignoredups:ignorespace
 # HISTCONTROL=ignoreboth
+#To save every command before it is executed (this is different from bash's history -a solution):
+setopt inc_append_history
+#To retrieve the history file everytime history is called upon.
+setopt share_history
 
 # Change the highlight colour.  Underlining doesn't seem to work.
 zle_highlight=(region:bg=red special:underline)
