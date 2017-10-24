3b6977c8ed074f0ab053991a6ff0597c9d60f740
spiralofhope <spiralofhope@gmail.com>
2017-10-24 11:18:46 -0700

    Edited a bit.. I'm not sure what's going on with the first two, but I don't care right now..

--- live/bash/scripts/examples/searching through a string for a character.sh
+++ live/bash/scripts/examples/searching through a string for a character.sh
@@ -2,76 +2,90 @@
 
 
 
-# searchstring "." "some.thing"
+#test=true
+
 
 # ----------------------
 # Counting from the left
 # ----------------------
-:<<'NOTE'
-
-Returns the position of the item, or -1 if it's not found.
-
-leftmost = 0
-
-So the positions of abcde become:
- abcde
- =
- 01234
-NOTE
-
-
 searchstring() {
   unset searchstring_success
-  until [ "sky" = "falling" ]; do
-  # 2 parameters, no blanks, first parameter  must be one character.
-  if [ ! "$#" -eq 2 ] || [ "$1" = "" ] || [ "$2" = "" ] || [ `expr length $1` -gt 1 ]; then echo "Needs two parameters: a character, and a string"; break ; fi
-  character="$1"
-  string="$2"
-  # Iterate through the string.
-  for i in $(seq 0 $((${#string} - 1))); do
-    # Checking that location in the string, see if the character matches.
-    # I should convert this into an 'until' so it makes sense to me, and it halts on the first success.
-    if [ "${string:$i:1}" = "$character" ]; then searchstring_success=$i ; fi
-  done
-  if [ ! "$searchstring_success" = "" ]; then echo $searchstring_success ; else echo "-1" ; fi
-  break
+  until [ 'sky' = 'falling' ]; do
+    # 2 parameters, no blanks, first parameter  must be one character.
+    if [ ! "$#" -eq 2 ] || [ "$1" = '' ] || [ "$2" = '' ] || [ $( \expr  length  "$1" ) -gt 1 ]; then
+      \echo  'ERROR - Needs two parameters: a character, and a string'
+      break
+    fi
+    character="$1"
+    string="$2"
+    # Iterate through the string.
+    for i in $( \seq  0  $((${#string} - 1)) ); do
+      # Checking that location in the string, see if the character matches.
+      # I should convert this into an 'until' so it makes sense to me, and it halts on the first success.
+      if [ "${string:$i:1}" = "$character" ]; then
+        searchstring_success="$i"
+      fi
+    done
+    if [ ! "$searchstring_success" = '' ]; then
+      \echo  "$searchstring_success"
+    else
+      \echo  '-1'
+    fi
+    break
   done
 }
+searchstring_test() {
+  \echo
+  \echo  "given 'abcde', search for the position of 'b' from the left, starting with 0"
+  searchstring  'b'  'abcde'
+  # =>
+  # 1
+  \echo  "given 'abcde', search for the position of 'f' (not found) from the left, starting with 0"
+  searchstring  'f'  'abcde'
+  # =>
+  # -1
+}
+
 
 
 # -----------------------
 # Counting from the right
 # -----------------------
-# giving position from the left
-
-:<<NOTE
-leftmost = 0
-
-So the positions of abcde become:
- abcde
- =
- 01234
-NOTE
-
-
+# returns a character's position from the left of a string
 searchstring_right_l() {
   unset searchstring_success
-  until [ "sky" = "falling" ]; do
-  # 2 parameters, no blanks, first parameter  must be one character.
-  if [ ! "$#" -eq 2 ] || [ "$1" = "" ] || [ "$2" = "" ] || [ `expr length $1` -gt 1 ]; then echo "Needs two parameters: a character, and a string"; break ; fi
-  character="$1"
-  string="$2"
+  until [ 'sky' = 'falling' ]; do
+    # 2 parameters, no blanks, first parameter  must be one character.
+    if [ ! "$#" -eq 2 ] || [ "$1" = '' ] || [ "$2" = '' ] || [ $( \expr  length  "$1" ) -gt 1 ]; then
+      \echo  'ERROR - Needs two parameters: a character, and a string'
+      break
+    fi
+    character="$1"
+    string="$2"
 
-  position=0
-  length=${#string}
-  until [ $length = -1 ]; do
-    if [ "${string:$length:1}" = "$character" ]; then searchstring_success=$length ; fi
-    ((position++))
-    ((length--))
+    position=0
+    length=${#string}
+    until [ $length = -1 ]; do
+      if [ "${string:$length:1}" = "$character" ]; then
+        searchstring_success="$length"
+      fi
+      ((position++))
+      ((length--))
+    done
+    if [ ! "$searchstring_success" = '' ]; then
+      \echo  "$searchstring_success"
+    else
+      \echo '-1'
+    fi
+    break
   done
-  if [ ! "$searchstring_success" = "" ]; then echo $searchstring_success ; else echo "-1" ; fi
-  break
-  done
+}
+searchstring_right_l_test() {
+  \echo
+  \echo  "given 'abcde', search for the position of 'b' from the left, starting with 0"
+  searchstring_right_l  'b'  'abcde'
+  # =>
+  # 1
 }
 
 
@@ -79,33 +93,49 @@ searchstring_right_l() {
 # -----------------------
 # Counting from the right
 # -----------------------
-# giving position from the right
-
-:<<NOTE
-rightmost = 1
-
-So the positions of abcde become:
- abcde
- =
- 54321
-NOTE
-
-
+# returns a character's position from the right of a string
 searchstring_right_r() {
   unset searchstring_success
-  until [ "sky" = "falling" ]; do
-  # 2 parameters, no blanks, first parameter  must be one character.
-  if [ ! "$#" -eq 2 ] || [ "$1" = "" ] || [ "$2" = "" ] || [ `expr length $1` -gt 1 ]; then echo "Needs two parameters: a character, and a string"; break ; fi
-  character="$1"
-  string="$2"
-  position=0
-  length=${#string}
-  until [ $length = -1 ]; do
-    if [ "${string:$length:1}" = "$character" ]; then searchstring_success=$position ; fi
-    ((position++))
-    ((length--))
-  done
-  if [ ! "$searchstring_success" = "" ]; then echo $searchstring_success ; else echo "-1" ; fi
-  break
+  until [ 'sky' = 'falling' ]; do
+    # 2 parameters, no blanks, first parameter  must be one character.
+    if [ ! "$#" -eq 2 ] || [ "$1" = '' ] || [ "$2" = '' ] || [ $( \expr  length  "$1" ) -gt 1 ]; then
+      \echo  'ERROR - Needs two parameters: a character, and a string'
+      break
+    fi
+    character="$1"
+    string="$2"
+    position=0
+    length="${#string}"
+    until [ "$length" = -1 ]; do
+      if [ "${string:$length:1}" = "$character" ]; then
+        searchstring_success="$position"
+      fi
+      ((position++))
+      ((length--))
+    done
+    if [ ! "$searchstring_success" = '' ]; then
+      \echo  "$searchstring_success"
+    else
+      \echo  '-1'
+    fi
+    break
   done
 }
+searchstring_right_r_test() {
+  \echo
+  \echo  "given 'abcde', search for the position of 'b' from the right"
+  searchstring_right_r  'b'  'abcde'
+  # =>
+  # 4
+}
+
+
+
+
+# -----------------------------
+if [ ! $test ]; then exit 0; fi
+
+
+searchstring_test
+searchstring_right_l_test
+searchstring_right_r_test
