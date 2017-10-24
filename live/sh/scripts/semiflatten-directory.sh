5de418c06e12d40a23342da4215f7e04a0d9eede
spiralofhope <spiralofhope@gmail.com>
2017-10-23 23:01:19 -0700

    - Style changes  - Fixed a corner-case where a directory with one file (which has spaces in it) wasn't being skipped.

--- live/sh/scripts/semiflatten-directory.sh
+++ live/sh/scripts/semiflatten-directory.sh
@@ -2,23 +2,51 @@
 
 
 
+#debug=true
+
+
+
+debug() {
+  if [ $debug ]; then
+    \echo  $*
+  fi
+}
+
+
 
 _go() {
-  # Given a directory
-  if [ -f "$1" ]; then
-    continue
+  if [ $debug ]; then
+    \echo  " * Processing:  $1"
   fi
-  # If it has only one subdirectory
-  if [ $(\ls "$1" | wc -l) -ne 1 ]; then
-    continue
-  fi
+
+  {  # Skip if not a directory
+    if [ ! -d "$1" ]; then
+      debug  "   Skipping (not a directory)"
+      continue
+    fi
+  }
+
+
+  {  # Skip if more than one item.
+    number_of_files_in_directory=$( \ls  --almost-all  -1 "$1"/ | \wc  --lines )
+    debug  "   $number_of_files_in_directory items within."
+    if [ $number_of_files_in_directory -ne 1 ]; then
+      continue
+    fi
+  }
+
+
+  {  # Skip is that one item is not a subdirectory.
+    if [ ! -f "$( \ls  --almost-all  -1 "$1"/ )" ]; then
+      debug  "   Skipping (not a subdirectory)"
+      continue
+    fi
+  }
+
+
   target=./"$1"/
-  source=./"$1"/$( ls  --directory  ./"$1"/ )
-  # TODO? - If the one subdirectory is named the same as its parent
-  #         I don't think I'll do this.
-  #\echo  " * Processing $1"
+  source=./"$1"/$( \ls  --directory  ./"$1"/ )
   # Move that one subdirectory's contents into its parent ($1)
-  # FIXME? - I don't know why I can't double-quote $source
   \mv  "$source"/*  "$target"
   # Remove the now-empty directory
   \rmdir  "$source"
