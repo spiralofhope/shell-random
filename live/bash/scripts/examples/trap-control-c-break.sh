b8d31e7a825e12ac7204981a24865a32862eead3
spiralofhope <spiralofhope@gmail.com>
2017-10-24 08:53:41 -0700

    Decrufting.

--- live/bash/scripts/examples/trap-control-c-break.sh
+++ live/bash/scripts/examples/trap-control-c-break.sh
@@ -1,22 +1,21 @@
-#!/usr/bin/env  bash
+#!/usr/bin/env  sh
 
 
 
-# http://www.linuxconfig.org/Bash_scripting_Tutorial#7-bash-trap-command
+# INT is ^C
+trap control_c INT
 
-# bash trap command
-trap bashtrap INT
-# bash clear screen command
-clear;
-# bash trap function is executed when CTRL-C is pressed:
-# bash prints message => Executing bash trap subrutine !
-bashtrap()
+control_c()
 {
-  echo "CTRL+C Detected !...executing bash trap !"
+  \echo  'control-c detected.'
+  # You can also exit this entire script with:
+  #exit  0
 }
+
 # for loop from 1/10 to 10/10
-for a in `seq 1 10`; do
-  echo "$a/10 to Exit."
-  sleep 1;
+for a in $( \seq 1 10 ); do
+  \echo  "$a/10 to Exit."
+  \sleep  1
 done
-echo "Exit Bash Trap Example!!!" 
+
+\echo  "Exiting trap example." 
