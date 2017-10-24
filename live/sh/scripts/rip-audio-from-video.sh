5def521991e5a38c81b68d51d144fe6b98cebcf4
spiralofhope <spiralofhope@gmail.com>
2017-10-23 21:18:24 -0700

    A rewrite of this the rip/transcode concept, to allow me to pull raw audio out without transcoding.

--- live/sh/scripts/rip-audio-from-video.sh
+++ live/sh/scripts/rip-audio-from-video.sh
@@ -2,13 +2,9 @@
 
 
 
-# Transcode an audio file into mp3.
-# While this could be used directly on a video, it will be terribly slow.  Instead:  transcode the file resulting from `rip-audio-from-video.sh`
-
-
 # 2017-10-23 - Tested on Devuan-1.0.0-jessie-i386-DVD with libav 6:11.9-1~deb8u1
 #debug=true
-#verbose='--verbose'
+
 
 
 # Requirements:
@@ -29,11 +25,25 @@
 }
 
 
+
 {  # target expanded path
   directory_without_file="$( \dirname "$source_file" )"
   filename="$( \basename "$source_file" )"
   filename_without_path_or_extension="${filename%.*}"
-  append=" _.mp3"
+
+  {  # Learn the codec being used:
+    audio_codec="$( \
+      \avconv  -i "$source_file"  2>&1 |\
+      \sed  --quiet  's/.* Audio: \([^,]*\).*/\1/p' \
+    )"
+
+    if [ $debug ]; then
+      \echo  'audio_codec is:'
+      \echo  "$audio_codec"
+    fi
+  }
+  append=".${audio_codec}"
+
   target_file="${directory_without_file}/${filename_without_path_or_extension}${append}"
 
   if [ $debug ]; then
@@ -43,33 +53,11 @@
 }
 
 
-{  # transcode
-  \echo  ' * Transcoding..'
-  # If avconv isn't found:
-  # On Devuan-1.0.0-jessie-i386-DVD:
-  #   apt-get install libav-tools
+
+{  # rip
   \avconv \
     -i "$source_file" \
-    -aq 3 \
+    -acodec copy \
     "$target_file" \
   ` # `
-  \echo  ''
-}
-
-
-{  # vbr fix
-  \echo  ' * Fixing the mp3 length..'
-  # TODO - Use a proper temporary file.
-  append=.$$
-  temp_filename="${target_file}${append}"
-  if [ $debug ]; then
-    \echo  'temp file is:'
-    \echo  "$temp_filename"
-  fi
-  \vbrfix  -makevbr  "$target_file"  "$temp_filename"
-  if [ $? -eq 0 ]; then
-    \mv  --force  $verbose  "$temp_filename"  "$target_file"
-    \rm  --force  $verbose  vbrfix.log  vbrfix.tmp
-  fi
-  \echo  ''
 }
