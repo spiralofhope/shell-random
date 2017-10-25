afdbc8c59eb725e87949b3420f5ce3e7040e903e
spiralofhope <spiralofhope@gmail.com>
2017-10-24 17:44:46 -0700

    Two ways to deal with duplicate teardown calls.  Also it deals with spawning a terminal, running this script, then killing that terminal.  So I've entirely overcome their limitations.
    A final thanks to https://unix.stackexchange.com/a/240736 for the inspiration.

--- live/sh/scripts/examples/trapping-control-c.sh
+++ live/sh/scripts/examples/trapping-control-c.sh
@@ -2,32 +2,37 @@
 
 
 
-# https://unix.stackexchange.com/a/240736
-
 # Working with a temporary directory like this allows one to fiddle around behind the scenes.  For example, run this in an xterm, kill the xterm and see if the temporary directory is removed or not.
 # FIXME - Use the proper tool to make this temp directory.
 temporary_directory="/tmp/trap-control-c-break.$$"
 # Working with a temporary file like this allows one to know if the teardown is already in progress, so as to not duplicate that effort.
-# TODO - How could I instead ignore signals during the process?
 temporary_teardown_file="$temporary_directory/already_tearing_down"
 
 
-# NOTE - If more signals arrive while executing _teardown, it may be run again.
-#        You may want to make sure your cleanup works correctly if invoked several times
-#        (and/or ignore signals during its execution)
-# NOTE - This does not catch signals passed to its parent.
-#        So if you launch a terminal (e.g. xterm), run this script, then close xterm, _teardown will not execute.
 
 for signal in INT QUIT HUP TERM USR1; do
   trap "
-    \touch  $temporary_teardown_file
     _teardown
-    \trap  -  $signal  EXIT
-    \kill  -s $signal  "'"$$"'  "$signal"
+    \touch  "$temporary_teardown_file"
+    \trap  -  "$signal"  EXIT
+    \kill  --signal  "$signal"  "'"$$"'  "$signal"
 done
+
+
 
 trap  _teardown  INT
 _teardown() {
+  # Instead of using a temporary file, and detecting if the teardown is already in progress, I could just un-set the traps:
+  unset_traps() {
+    __() {
+      echo
+    }
+    for signal in INT QUIT HUP TERM USR1 EXIT; do
+      trap __ "$signal"
+    done
+  }
+  #unset_traps
+
   if [ -f "$temporary_teardown_file" ]; then
     \echo  'Already exiting!'
   else
@@ -37,6 +42,7 @@ _teardown() {
       \echo  "Exiting in $i.."
       \sleep  1
     done
+    \echo
     \rm  --force  --recursive  --verbose  "$temporary_directory"
     exit  0
   fi
