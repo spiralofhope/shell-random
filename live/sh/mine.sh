b8ed35d21daf6e1af091e733d498d35ca01d20cc
spiralofhope <spiralofhope@gmail.com>
2017-10-26 09:40:32 -0700

    Shifted everything over.

--- live/sh/mine.sh
+++ live/sh/mine.sh
@@ -23,9 +23,7 @@ finddir() {
 }
 
 
-# technically I could make a `findin` that applies to only one file, but I won't bother.
-
-
+# TODO - Technically I could make a `findin` that applies to only one file, but I won't bother.
 findinall() {
   if [[ -d $1 ]]; then
     ${shell_random}/live/sh/scripts/findhelper.sh  file_contents      $*
@@ -33,3 +31,28 @@ findinall() {
     ${shell_random}/live/sh/scripts/findhelper.sh  file_contents  ./  $*
   fi
 }
+
+
+# Make and change into a directory:
+mcd() {
+  \mkdir  "$1" &&\
+  \cd  "$1"
+}
+
+
+# This used to have  --exclude-type supermount
+#alias  df='\df  --human-readable'
+_df_sorted(){
+  _df() {
+    \df  --human-readable  --exclude-type tmpfs  --exclude-type devtmpfs
+  }
+  # The text at the top
+  _df |\
+    \head --lines=1
+  # The actual list of stuff
+  _df |\
+    \tail --lines=+2  |\
+    \sort --key=${1}
+}
+alias  df='_df_sorted 5'    # sorted by mountpoint
+alias  df='_df_sorted 1'    # sorted by filesystem
