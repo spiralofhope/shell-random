e5f44593227b61f3a4e46f247fc0f6343e775a6d
spiralofhope <spiralofhope@gmail.com>
2017-10-26 09:37:10 -0700

    Moving things over.

--- live/bash and zsh/lib.sh
+++ live/bash and zsh/lib.sh
@@ -3,23 +3,4 @@
 
 
 
-# TODO - Fucking hell, `du` has trailing spaces and the name of the directory.  It even puts '.' if nothing is specified.
-# TODO - remove a trailing period.
-# TODO - remove trailing spaces.
-comma() {
-  if [ -z $1 ]; then
-    # Incorrect usage.
-    \echo  -n  ''
-  elif [ -z $2 ]; then
-    \echo "$1" | \sed -e :a -e 's/\(.*[0-9]\)\([0-9]\{3\}\)/\1,\2/;ta'
-  else
-    # Incorrect usage.
-    \echo  -n  ''
-  fi
-}
-# Although at the commandline, this works:
-#   comma 1000
-# This is the required way to use it when scripting:
-#   comma '1000'
-# Or more complex:
-#   count=$( comma $( \ls -1 . | \wc -l ) )
+# (empty)
---
+++ live/sh/lib.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env  sh
+# Used by other functions.
+# Loaded before anything else "sh"-specific.
+# Loaded before any zsh/bash content.
+
+
+
+# --follow-name would allow the file to be edited and less will automatically display changes.
+LESS=' --force  --ignore-case  --long-prompt  --no-init  --silent  --status-column  --tilde  --window=-2'
+export LESS
+
+
+
+# TODO - Fucking hell, `du` has trailing spaces and the name of the directory.  It even puts '.' if nothing is specified.
+# TODO - remove a trailing period.
+# TODO - remove trailing spaces.
+comma() {
+  if [ -z $1 ]; then
+    # Incorrect usage.
+    \echo  -n  ''
+  elif [ -z $2 ]; then
+    \echo "$1" | \sed -e :a -e 's/\(.*[0-9]\)\([0-9]\{3\}\)/\1,\2/;ta'
+  else
+    # Incorrect usage.
+    \echo  -n  ''
+  fi
+}
+# Although at the commandline, this works:
+#   comma 1000
+# This is the required way to use it when scripting:
+#   comma '1000'
+# Or more complex:
+#   count=$( comma $( \ls -1 . | \wc -l ) )
