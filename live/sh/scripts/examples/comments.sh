91576e09e2a9d83f2b082e300488be13d47996e4
spiralofhope <spiralofhope@gmail.com>
2017-10-26 23:53:21 -0700

    cleanup

--- live/sh/scripts/examples/comments.sh
+++ live/sh/scripts/examples/comments.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env  sh
-
-
-
+# GNU bash, version 4.3.33(1)-release (i686-pc-cygwin)
 # dash 0.5.7-4+b1
+
+
+
 # Lines like this one, with a preceeding number sign are commented-out.
 
 
-# dash 0.5.7-4+b1
+
 # HEREDOC aka Here document
 #   inline comments, or aborting a script from this point on.
-: <<HERE_DOCUMENT
+: <<'HERE_DOCUMENT'
   this text is ignored
   even code is ignored
   echo ignored
@@ -17,15 +18,15 @@
 HERE_DOCUMENT
 # The HERE_DOCUMENT text must be at the very beginning of the line.
 # Omitting the above HERE_DOCUMENT will have the below code ignored as well.
-echo not ignored
+\echo  'not ignored'
 
 
+
+\echo
 # HEREDOC in a variable
-variable=$(cat <<SETVAR
+variable=$(cat <<'SETVAR'
 This variable
 runs over multiple lines.
 SETVAR
 )
-echo $variable
-
-
+\echo  "$variable"
