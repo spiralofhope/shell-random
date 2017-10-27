54ec95e3f4a4b587a8d1b793034114da2ad413b2
spiralofhope <spiralofhope@gmail.com>
2017-10-26 20:57:23 -0700

    updated from bash-windows

--- live/babun/aliases.sh
+++ live/babun/aliases.sh
@@ -2,8 +2,16 @@
 
 
 
-\alias  current='cd /l/OS'
-\alias  home="\cd  "$windows_home_as_linux""
+# -X is to sort by extension.
+# alias  ls='\ls  -1  --all  --classify  --color=auto  --show-control-chars  -X'
+\alias  current='\cd  /l/OS/'
+\alias  home="\cd  \"$windows_home_as_linux\""
+
+# World of Warcraft
+alias  addons='\cd  /l/World_of_Warcraft/_dotfiles/Interface/AddOns/'
+alias     wow='\cd  /l/World_of_Warcraft/'
+
+
 
 # Not used/valid under Babun:
 \unalias  previous
