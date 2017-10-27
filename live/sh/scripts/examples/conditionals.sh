edcad82809a852723753704c2ade1e84abc1944b
spiralofhope <spiralofhope@gmail.com>
2017-10-26 22:12:45 -0700

    My example conditionals are all vanilla.

--- live/sh/scripts/examples/conditionals.sh
+++ live/sh/scripts/examples/conditionals.sh
@@ -37,3 +37,20 @@ if [ a = a ] && [ b = b ]; then echo 'both'; fi
 
 # dash 0.5.7-4+b1
 if [ a = a ] || [ a = b ]; then echo 'either'; fi
+
+
+
+# sh executed via
+# GNU bash, version 4.3.33(1)-release (i686-pc-cygwin)
+var=0
+case "$var" in
+  0)
+    echo $var is zero
+  ;;
+  1)
+    echo $var is one
+  ;;
+  *)
+    echo $var is neither zero nor one
+  ;;
+esac
