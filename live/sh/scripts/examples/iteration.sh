25a97c13f6a6ccdacdea7a7564200b5584e83981
spiralofhope <spiralofhope@gmail.com>
2017-10-28 23:09:15 -0700

    What would normally be basic things is terribly complex in a vanilla/Bourne shell.

--- live/sh/scripts/examples/iteration.sh
+++ live/sh/scripts/examples/iteration.sh
@@ -2,6 +2,8 @@
 
 
 
+
+#:<<'}'
 {  #  Basics
   # GNU bash, version 4.3.33(1)-release (i686-pc-cygwin)
   # GNU bash, version 4.3.30(1)-release (i586-pc-linux-gnu)
@@ -21,18 +23,60 @@
 
 
 
+#:<<'}'
+{  #  Walking through each line in a variable.
+
+  # NOTE - Notice how this removes beginning spaces:
+  variable="
+  testing
+  multiple
+  lines
+  "
+
+  # Or from a command:
+  #variable=$( \find . -maxdepth 1 )
+
+  # $variable cannot be quoted as "$variable"
+  for line in $variable; do
+    echo "_ $line"
+  done
+}
+{  #  Another method:
+  variable="
+  testing
+  multiple
+  lines
+  "
+  echo "$variable" |\
+  while read line;
+  do
+    echo $line
+  done
+}
+
+
+
+#:<<'}'
+# This may not work for everyone:
 {  #  Iterate through the listing made by 'find'
   # GNU bash, version 4.3.33(1)-release (i686-pc-cygwin)
   OLDIFS=$IFS
   IFS=$'\n'
-  for line in $( \find . -maxdepth 1 ); do
-    echo "$line"
-    echo "---"
+  for line in $( find . -maxdepth 1 ); do
+    \echo  "_ $line"
   done
   IFS=$OLDIFS
-
-  # This doesn't work as expected
-  #for line in $variable; do
-  #  echo $line
-  #done
 }
+
+
+
+#  Iterating through each character in a variable
+{  # Using grep
+  variable="testing"
+  echo "$variable" |\
+  grep -o . |\
+  while read character;  do
+    echo "_ $character"
+  done 
+}
+# I don't think it's possible to do it without external programs, when restricted to the Bourne shell (sh, dash, etc)
