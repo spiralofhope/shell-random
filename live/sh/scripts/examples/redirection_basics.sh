4943a6ed19fd4ce34fb49e3fee31defd9178c09f
spiralofhope <spiralofhope@gmail.com>
2017-10-29 11:36:19 -0700

    Because I keep forgetting.

---
+++ live/sh/scripts/examples/redirection_basics.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env  sh
+# Examples of basic redirection.
+# Sometimes you want to have a command "say nothing".
+# Sometimes you only want a command to omit an error message.
+
+
+
+# If it outputs something, let it.
+\expr 10 + 10
+\echo  $?
+# =>
+# 20
+# 0
+
+
+# Suppress regular output
+\expr 10 + 10  > /dev/null
+\echo  $?
+# =>
+# 0
+
+
+# If it outputs something, let it.
+\expr 10 + a
+\echo  $?
+# =>
+# expr: non-integer argument
+# 2
+
+
+# If it outputs an error, stop it.
+\expr 10 + a  2> /dev/null
+\echo  $?
+# =>
+# 2
+
+
+# If it outputs an error, or regular output, stop it.
+\expr 10 + a  > /dev/null 2> /dev/null
+\echo  $?
+\expr 10 + 10 > /dev/null 2> /dev/null
+\echo  $?
+# =>
+# 2
+# 0
