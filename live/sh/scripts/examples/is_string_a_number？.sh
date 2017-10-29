351cd3c2798bb3fd8239bb2189efa2a7d4e4b291
spiralofhope <spiralofhope@gmail.com>
2017-10-28 20:06:53 -0700

    .

--- live/sh/scripts/examples/is_string_a_number？.sh
+++ live/sh/scripts/examples/is_string_a_number？.sh
@@ -12,6 +12,9 @@ isnumber() {
     '')
       __="blank\t\tfalse"
     ;;
+    [-+][0-9]*)
+      __="signed number\ttrue"
+    ;;
     [0-9])
       __="single digit\ttrue"
     ;;
@@ -21,17 +24,15 @@ isnumber() {
     [-+][0-9]*[.][0-9]*)
       __="signed float\ttrue"
     ;;
-    [0-9]*[.][0-9]*)
+    [0-9]*[.][0-9]+)
       __="float\t\ttrue"
-    ;;
-    [-+][0-9])
-      __="signed number\ttrue"
-    ;;
-    [0-9]*[^^0-9])
-      __="multiple digit\ttrue"
     ;;
     *)
       __="not a number\tfalse"
+    ;;
+    #[0-9]*[^^0-9])
+    [0-9]*)
+      __="multiple digit\ttrue"
     ;;
   esac
   \echo "$variable\t$__"
@@ -59,8 +60,22 @@ isnumber  +1.0
 isnumber  1.0.1
 isnumber  -1.0.1
 isnumber  +1.0.1
+isnumber  1.
 isnumber  1-
 isnumber  a
 isnumber  a1
 isnumber  a1.0
 isnumber
+isnumber  100
+isnumber  1000
+isnumber  12:34
+isnumber  1.
+isnumber  1.02abc
+
+test_isnumber(){
+  __=$( isnumber $1 )
+  echo $2
+  echo $__
+}
+
+test_isnumber -2 'signed number'
