115239ede465eddecfd1af7ba1431ec151cc5112
spiralofhope <spiralofhope@gmail.com>
2017-11-19 04:46:13 -0800

    Slight update, but this needs to be tested before I could trust it.

--- live/sh/scripts/compare-cd-and-iso.sh
+++ live/sh/scripts/compare-cd-and-iso.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env  sh
 # Compare a CD to an ISO, to ensure it was burned properly.
+# 2017-11-19 - This wasn't tested any time recently, and this may have bashisms.
 
 
 
@@ -21,32 +22,34 @@ IDEAS
 #   MD5_FILENAME=$2
 #   DEVICE=$3
 
-ISO_FILENAME=pclinuxos-2007.iso
-MD5_FILENAME=pclinuxos-2007.md5sum
-DEVICE=/dev/dvd
+ISO_FILENAME='pclinuxos-2007.iso'
+MD5_FILENAME='pclinuxos-2007.md5sum'
+DEVICE='/dev/dvd'
 
 # If you don't have a .md5sum file, get the ISO's md5 with:
 #   SOURCE_MD5=`md5sum $ISO_FILENAME`
-read SOURCE_MD5 < $MD5_FILENAME
+read SOURCE_MD5 < "$MD5_FILENAME"
 # Remove the trailing stuff and keep the md5sum
-SOURCE_MD5=${SOURCE_MD5%% *}
+# FIXME? - Bashism?
+SOURCE_MD5="${SOURCE_MD5%% *}"
 
-FILESIZE=$(stat -c%s "$ISO_FILENAME")
+FILESIZE=$( \stat -c%s "$ISO_FILENAME" )
 
 # Get the disk's md5:
 # This tests ok and should work for most computers:
-DISK_MD5=$( md5sum "$DEVICE" )
+DISK_MD5=$( \md5sum "$DEVICE" )
 # But some computers may instead need to use this command (tested and works):
 #   DISK_MD5=`dd if=$DEVICE | head -c $FILESIZE | md5sum`
 # There is also this (untested).  Where <device> is the scsi "scsibus,target,lun" and <filename> is the iso file.  Try "man readcd" for more info:
 #   readcd dev=<device> f=- | diff -s - <filename>
 
 # Remove the trailing stuff and keep the md5sum
-DISK_MD5=${SOURCE_MD5%% *}
+# FIXME? - Bashism?
+DISK_MD5="${SOURCE_MD5%% *}"
 
 # Compare the two..
 printf "\n\n\n"
-if [ $SOURCE_MD5 = $DISK_MD5 ]; then
+if [ "$SOURCE_MD5" = "$DISK_MD5" ]; then
    echo "$ISO_FILENAME: OK"
 else
    echo "Sorry, your disk is not ok"
