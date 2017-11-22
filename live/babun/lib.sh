7989040999e5054080e452790c596ba03c93d395
spiralofhope <spiralofhope@gmail.com>
2017-11-22 05:41:00 -0800

    ./lib.sh wouldn't find cygpath/dirpath.  Specifying the complete path to those commands fixes that.

--- live/babun/lib.sh
+++ live/babun/lib.sh
@@ -35,17 +35,17 @@
   # Windows can work with forward slashes just fine, so don't worry.
 
   # CSIDL_PROGRAM_FILES 0x0026  --  This still reports (x86)
-  PF="$( \cygpath  --folder 0x0026 )"
+  PF="$( /usr/bin/cygpath  --folder 0x0026 )"
   # FIXME - Semi-manually doing it:
-  PF="$( \dirname  "$PF" )/Program Files"
+  PF="$( /usr/bin/dirname  "$PF" )/Program Files"
   # CSIDL_PROGRAM_FILESX86 0x002a
-  PFx="$( \cygpath  --folder 0x002a )"
+  PFx="$( /usr/bin/cygpath  --folder 0x002a )"
   # The Windows-style versions:
-  wPF="$(  \cygpath  --mixed  "$PF"  )"
-  wPFx="$( \cygpath  --mixed  "$PFx" )"
+  wPF="$(  /usr/bin/cygpath  --mixed  "$PF"  )"
+  wPFx="$( /usr/bin/cygpath  --mixed  "$PFx" )"
   # CSIDL_PROFILE 0x0028
-  windows_home_as_linux="$( \cygpath  --folder 0x0028 )"
-  windows_home_as_windows="$( \cygpath  --mixed  "$windows_home_as_linux" )"
+  windows_home_as_linux="$( /usr/bin/cygpath  --folder 0x0028 )"
+  windows_home_as_windows="$( /usr/bin/cygpath  --mixed  "$windows_home_as_linux" )"
 
   if [ $debug ]; then
     \echo  "PF   = $PF"
@@ -66,7 +66,7 @@ geany() {  #  The GUI editor
     string=${string}' '\"$( \cygpath  --dos  "$file" )\"
   done
 #  \cygstart  "$PFx/Geany/bin/geany.exe"  "$string" &
-  \cygstart  "/d/Program Files (x86)/Geany/bin/geany.exe"  "$string" &
+echo  \cygstart  "/d/Program Files (x86)/Geany/bin/geany.exe"  "$string" &
   \unset  string
 }
 
