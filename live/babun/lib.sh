c74b799223ceae1d0298c0f75221e50884e53bda
spiralofhope <spiralofhope@gmail.com>
2017-11-22 06:00:10 -0800

    Apparently I can't ./lib.sh because it won't re-load geany() for testing.  I don't understand, but whatever.

--- live/babun/lib.sh
+++ live/babun/lib.sh
@@ -35,17 +35,17 @@
   # Windows can work with forward slashes just fine, so don't worry.
 
   # CSIDL_PROGRAM_FILES 0x0026  --  This still reports (x86)
-  PF="$( /usr/bin/cygpath  --folder 0x0026 )"
+  PF="$( \cygpath  --folder 0x0026 )"
   # FIXME - Semi-manually doing it:
-  PF="$( /usr/bin/dirname  "$PF" )/Program Files"
+  PF="$( \dirname  "$PF" )/Program Files"
   # CSIDL_PROGRAM_FILESX86 0x002a
-  PFx="$( /usr/bin/cygpath  --folder 0x002a )"
+  PFx="$( \cygpath  --folder 0x002a )"
   # The Windows-style versions:
-  wPF="$(  /usr/bin/cygpath  --mixed  "$PF"  )"
-  wPFx="$( /usr/bin/cygpath  --mixed  "$PFx" )"
+  wPF="$(  \cygpath  --mixed  "$PF"  )"
+  wPFx="$( \cygpath  --mixed  "$PFx" )"
   # CSIDL_PROFILE 0x0028
-  windows_home_as_linux="$( /usr/bin/cygpath  --folder 0x0028 )"
-  windows_home_as_windows="$( /usr/bin/cygpath  --mixed  "$windows_home_as_linux" )"
+  windows_home_as_linux="$( \cygpath  --folder 0x0028 )"
+  windows_home_as_windows="$( \cygpath  --mixed  "$windows_home_as_linux" )"
 
   if [ $debug ]; then
     \echo  "PF   = $PF"
