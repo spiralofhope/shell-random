f2409d7182d14b90ccd3ec95e5497ae51f278aff
spiralofhope <spiralofhope@gmail.com>
2018-04-12 03:24:40 -0700

    Force X to start on that specific TTY/vt/whatever it's called, because it's fucking annoying to switch between them using some hotkey that randomly changes every reboot.

--- live/zsh/dot_zsh/4-login.sh
+++ live/zsh/dot_zsh/4-login.sh
@@ -8,14 +8,24 @@
 
 # if [ -z "$DISPLAY" ] && [ $( \tty ) == /dev/tty1 ]; then
 
-_tty=$( \tty )
-if    [ $_tty = '/dev/tty1' ] ||\
-      [ $_tty = '/dev/tty2' ]; then
+if    [ "$TTY" = '/dev/tty1' ] ||\
+      [ "$TTY" = '/dev/tty2' ]; then
 
 # This is a nice idea, but I think I need to chain zsh in the middle of things, unless I want to fuck around with the default shell..  perhaps `dtach` would work, but I don't know..
 #  echo $$ !> /tmp/zsh-launching-startx.ppid
 #  dtach -n /tmp/dtach.socket  \startx &
   \clear
-  \setsid  \startx
+
+  # Find the TTY number
+  #  e.g.  /dev/tty2  =>  2
+  string="$TTY"
+  pattern='/dev/tty'
+  local  tty_to_use=${string##${pattern}}
+  tty_to_use=${string##*${pattern}}
+
+  #\setsid  \startx
+  # Start X on that specific TTY
+  \startx  --  vt"$tty_to_use"
+
   \logout
 fi
