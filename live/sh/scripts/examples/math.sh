cfa067b0e99dd9b0fd275496aa80962455c40e89
spiralofhope <spiralofhope@gmail.com>
2018-04-20 07:53:08 -0700

    more math

--- live/sh/scripts/examples/math.sh
+++ live/sh/scripts/examples/math.sh
@@ -4,4 +4,11 @@
 
 # You need to use an external program like:
 \expr  "1 + 1"
+
 \echo  "1 + 1" | \bc
+
+# For more complex things
+\echo  "10 / 3" | \bc  --mathlib
+
+
+# More is possible with awk
