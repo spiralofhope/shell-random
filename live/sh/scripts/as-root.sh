19903f41f4ef00264dccd03bf603971e02e4e724
spiralofhope <spiralofhope@gmail.com>
2018-05-05 23:00:51 -0700

    An example of running a block of code as root, without using `sudo`.

---
+++ live/sh/scripts/as-root.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env  sh
+
+
+
+# As root:
+if ! [ "$USER" = 'root' ]; then
+  \echo  "enter root password"
+  /bin/su  --command  "$0"
+else
+
+# ---
+  \echo 'this is run as root'
+# ---
+
+fi   # The above is run as root
