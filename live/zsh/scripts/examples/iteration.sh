1ea5ebc7bccf97d19bd16c441a4306fbbfb0046c
spiralofhope <spiralofhope@gmail.com>
2018-05-17 16:39:58 -0700

    cleanup, pushed straggling examples from elsewhere

--- live/zsh/scripts/examples/iteration.sh
+++ live/zsh/scripts/examples/iteration.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env  zsh
-
-
-
-# GNU bash, version 4.3.30(1)-release (i586-pc-linux-gnu)
+# 
 # zsh 5.0.7 (i586-pc-linux-gnu)
-numa=0 ; numb=3
-until [ $numa -eq $numb ]; do
-  ((numa++))
-  echo iterating...
-done
+
+
+
+:<<'}'
+{
+  numa=0 ; numb=3
+  until [ $numa -eq $numb ]; do
+    ((numa++))
+    echo iterating...
+  done
+}
+
+
+:<<'}'  # Increment 1 to 10.
+{
+  for i in {1..10}; do echo $i; done
+}
+
+
+:<<'}'  # Increment 1 to 10, with no line breaks.
+{
+  for i in {1..10}; do printf $i; done
+}
