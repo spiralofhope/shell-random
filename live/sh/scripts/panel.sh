93302d47d437f2d04420a0f1506e44e788523aa1
spiralofhope <spiralofhope@gmail.com>
2018-07-08 03:23:51 -0700

    decruft

--- live/sh/scripts/panel.sh
+++ live/sh/scripts/panel.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env  sh
 # Launch any available panel (the bottom bar)
-# 2018-04-15 on Devuan-1.0.0-jessie-i386-DVD
-
+# 2018-07-07
+#   Devuan-1.0.0-jessie-i386-DVD
+#   dash 0.5.7-4+b1
 
 
 # TODO - given an array of strings, run the first program found.
@@ -15,17 +16,17 @@ trytry() {
   #
   \echo  "$1 attempt"
   \killall  $1   >  /dev/null 2> /dev/null
-  \setsid   $1  2>  /dev/null &
+  \setsid   $*  2>  /dev/null &
   \which    $1   >  /dev/null 2> /dev/null
   if [ $? = 0 ]; then
-    \echo  "$1 success"
+    \echo  "$* success"
     exit  0
   fi
-  \echo  "$1 failed"
+  \echo  "$* failed"
 }
 
 
-trytry fbpanel
-trytry lxpanel
-trytry xfce4-panel
-trytry tint2
+trytry  fbpanel
+trytry  lxpanel
+trytry  xfce4-panel
+trytry  tint2
