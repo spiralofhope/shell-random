b99f0e43960ff77090a63684d6517c1fa3de5dd3
spiralofhope <spiralofhope@gmail.com>
2018-09-28 19:00:52 -0700

    findqueue() and findplay() rewrite:
    - Simplification
    - Fix issue with deadbeef "resetting" while queueing.  It can't handle non-music files.

--- live/sh/mine.sh
+++ live/sh/mine.sh
@@ -115,8 +115,22 @@ findqueue() {
       \ps  alx | \grep  'deadbeef'
       \sleep  0.1
     done
-    find  .  -type f -iname "*${*}*" -print0 |\
+
+    # TODO - As deadbeef empties a playlist once it hits an invalid file, I'm going over its more common file types manually.
+    # TODO - fix this hackishness.
+
+    find  .  -type f -iname "*${@}*mp3" -print0 |\
       \xargs -0 -r -I file '/l/OS/bin/deadbeef-0.7.2/deadbeef'  --queue  file  > /dev/null 2> /dev/null  &
+
+    find  .  -type f -iname "*${@}*ogg" -print0 |\
+      \xargs -0 -r -I file '/l/OS/bin/deadbeef-0.7.2/deadbeef'  --queue  file  > /dev/null 2> /dev/null  &
+
+    find  .  -type f -iname "*${@}*flac" -print0 |\
+      \xargs -0 -r -I file '/l/OS/bin/deadbeef-0.7.2/deadbeef'  --queue  file  > /dev/null 2> /dev/null  &
+
+    find  .  -type f -iname "*${@}*m4a" -print0 |\
+      \xargs -0 -r -I file '/l/OS/bin/deadbeef-0.7.2/deadbeef'  --queue  file  > /dev/null 2> /dev/null  &
+
   }
   else                                                                    {   #  Audacious
     # Make sure it's running first.
@@ -141,81 +155,54 @@ findqueue() {
 
 
 findplay() {
+  # Note that findqueue() also have to have this variable set appropriately
   local  deadbeef=1
 
-  # TODO - Create the empty playlists in /l if they don't exist.
-  # TODO - perform the search once, then take the first result and play with that.  Then take the rest and queue it up.
-  # TODO - With with a separate 'findqueue' playlist.
-          # Remove any 'findqueue' playlist
-          # Create a new 'findqueue' playlist with these results.
-          # Play that playlist.
+  if [[ x$deadbeef == x1 ]]; then                                       {   #  Deadbeef
 
-  if [[ x$deadbeef == x1 ]]; then                                         {   #  Deadbeef
-    # Launch with an empty playlist
-    \setsid  '/l/OS/bin/deadbeef-0.7.2/deadbeef'  '/l/deadbeef_empty_playlist.dbpl'   > /dev/null 2> /dev/null  &
-    # Wait for it to launch
-    until pids=$( \pidof  '/l/OS/bin/deadbeef-0.7.2/deadbeef' ) ; do
-      \ps  alx | \grep  'deadbeef'
-      \sleep  0.1
-    done
-    # Queue
-    find  .  -type f -iname "*${*}*" -print0 |\
-      \xargs -0 -r -I file '/l/OS/bin/deadbeef-0.7.2/deadbeef'  --queue  file  > /dev/null 2> /dev/null  &
-    # Play
-    # FIXME - This doesn't always trigger
-            # .. unless I pause for a bit.
-            # .. but if I do pause, then sometimes it will play, and then restart playing.
-    #\sleep  1
-    # If findplay() is run without deadbeef already running, if it begins playing at all, then it begins playing at the second item!
-    '/l/OS/bin/deadbeef-0.7.2/deadbeef'  --play
-  }
-  else                                                                    {   #  Audacious
-
-    # Empty any current playlist:
-    \setsid  \audacious  --enqueue-to-temp  /l/empty_playlist.xspf   > /dev/null 2> /dev/null  &
-:<<'}'   #  Why in the everloving fuck doesn't this work?
-    until pids=$( \pidof  'audacious' ); do   
-      echo  'waiting for audacious to launch..'
-      \ps  alx | \grep  'audacious'
-      \sleep  0.1
-    done
-}
-    \sleep  1
-    # Queue
-    # Pulled from findqueue()
-    find  .  -type f -iname "*${*}*" -print0 |\
-      \xargs -0 -r -I file audacious  --enqueue  file
-    # Play
-    \audacious  --play
-  }
-  fi
-
-
-
-# Deadbeef has no functionality to just empty out its existing play list, but I can load an empty one.
-
-:<<'DEADBEEF_EMPTY_PLAYLIST'
-
-(
-\xxd  -r  <<  'DBPL'  #  xxd
+    {  #  Make a Deadbeef empty playlist
+  # Deadbeef has no functionality to just empty out its existing play list, but I can load an empty one.
+  local  tempfile=$( \mktemp  --suffix="--deadbeef-empty-playlist.dbpl" )
+(  #  It was a binary, so I encoded it with xxd
+\xxd  -r  <<  'DBPL'
 0000000: 4442 504c 0102 0000 0000 0000            DBPL........
 DBPL
-) > /l/deadbeef_empty_playlist.dbpl
+) > "$tempfile"
+    }
 
-DEADBEEF_EMPTY_PLAYLIST
+    {  # Launch with the empty playlist
+      \setsid  '/l/OS/bin/deadbeef-0.7.2/deadbeef'  "$tempfile"   > /dev/null 2> /dev/null  &
+      # Wait for it to launch
+      until pids=$( \pidof  '/l/OS/bin/deadbeef-0.7.2/deadbeef' ) ; do
+        \ps  alx | \grep  'deadbeef'
+        \sleep  0.1
+      done
+    }
+    
+    # Queue
+    findqueue $*
+    
+    {  #  Play
+      '/l/OS/bin/deadbeef-0.7.2/deadbeef'  --play
+      # If findplay() is run without deadbeef already running, if it begins playing at all, then it begins playing at the second item!
+      # .. so force it.
+      # .. which doesn't really work..
+      '/l/OS/bin/deadbeef-0.7.2/deadbeef'  --prev
+    }
+  }
+  else                                                                  {   #  Audacious
 
-
-# Audacious has no functionality to just empty out its existing play list, but I can load an empty one.  Here are three examples:
-
+    {  #  Make an Audacious empty playlist
+    # Audacious has no functionality to just empty out its existing play list, but I can load an empty one.  Here are three examples:
 :<<'AUPL'
 title=Now%20Playing
 AUPL
-
+#
 :<<'PLS'
 [playlist]
 NumberOfEntries=0
 PLS
-
+#
 :<<'XSPF'
 <?xml version="1.0" encoding="UTF-8"?>
 <playlist version="1" xmlns="http://xspf.org/ns/0/">
@@ -224,4 +211,34 @@ PLS
 </playlist>
 XSPF
 
+    local  tempfile=$( \mktemp  --suffix="--audacious-empty-playlist.pls" )
+(
+\cat << 'PLS'
+[playlist]
+NumberOfEntries=0
+PLS
+) >> "$tempfile"
+    }
+
+    {  # Launch with the empty playlist
+      \setsid  \audacious  --enqueue-to-temp  "$tempfile"   > /dev/null 2> /dev/null  &
+      ##  Why in the everloving fuck doesn't this work?
+      #until pids=$( \pidof  'audacious' ); do   
+        #echo  'waiting for audacious to launch..'
+        #\ps  alx | \grep  'audacious'
+        #\sleep  0.1
+      #done
+      \sleep  1
+    }
+
+    # Queue
+    findqueue $*
+
+    # Play
+    \audacious  --play
+  }
+  fi
+
+  # Teardown
+  #\rm   --force  --verbose  "$tempfile"
 }
