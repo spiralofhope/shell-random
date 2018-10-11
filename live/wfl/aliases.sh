775d935a5ad8dce3e57196a44114d5f8681c365c
spiralofhope <spiralofhope@gmail.com>
2018-10-11 14:37:53 -0700

    .

--- live/wfl/aliases.sh
+++ live/wfl/aliases.sh
@@ -10,6 +10,8 @@ alias D:='nocorrect  \cd  /mnt/d'
 
 local  l='/mnt/d/live'
 
+alias  l="nocorrect  \cd  $l"
+
 # -X is to sort by extension.
 # alias  ls='\ls  -1  --all  --classify  --color=auto  --show-control-chars  -X'
 \alias  current="\cd  /$l/OS/"
@@ -17,7 +19,6 @@ local  l='/mnt/d/live'
 # World of Warcraft
 alias  addons="nocorrect  \cd  /$l/World_of_Warcraft/_dotfiles/Interface/AddOns/"
 alias     wow="nocorrect  \cd  /$l/World_of_Warcraft/"
-
 
 
 # Not used/valid under wsl:
