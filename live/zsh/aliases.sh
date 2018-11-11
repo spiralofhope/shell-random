34841d5f6bbcd7fce856ec57d27f7c3d5028cf10
spiralofhope <spiralofhope@gmail.com>
2018-11-10 18:05:33 -0800

    Shifting aliases zsh -> sh

--- live/zsh/aliases.sh
+++ live/zsh/aliases.sh
@@ -25,28 +25,4 @@ alias  ping='nocorrect  \ping'
 # -- Linux console Applications
 # --
 
-# 2016-03-26 - Lubuntu (version not recorded)
-# 2016-03-28 - Slackware 14.1
-#alias  su="\sudo  $( \basename  $( \readlink  /proc/$$/exe ) )"
-#alias  sul='\sudo  \su  --login'
-
-# Go fuck yourself, drive.
-alias  eject='\eject  -i 0 ; \eject'
-# bash-windows does not support --color
-alias  grep='\grep --color'
-
-# TODO - What was I trying to achieve with this?
-# TODO - Improve
-#alias  killjobs='\kill  -9  $( \jobs -p )'
-
-# TODO? - What does this do?
-# alias  screen='TERM=screen screen'  # http://ubuntuforums.org/showthread.php?t=90910
-
-alias  pm-suspend='\sudo  /usr/sbin/pm-suspend'
-alias  reboot='    \sudo  /sbin/reboot'
-alias  poweroff='  \sudo  /sbin/poweroff'
-
-alias current=' \cd  /l/OS/current/'
-alias previous='\cd  /l/OS/previous/'
-
 alias git='nocorrect  \git'
