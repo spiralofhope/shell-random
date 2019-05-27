20c2ff9a8674fa17f3394ae3d5a4862c3b4b5beb
spiralofhope <spiralofhope@spiralofhope.com>
2019-05-27 01:15:48 -0700

    Including youtube-dl, and bunches of code needed to do things the right way.

--- live/zsh/mine.sh
+++ live/zsh/mine.sh
@@ -241,3 +241,66 @@ rm() {
     nocorrect  /bin/rm  --interactive  "$*"
   fi
 }
+
+
+
+ytdl() {
+  # To test the filename:
+#    --get-filename  \
+
+  \youtube-dl  \
+    --write-description  \
+    --write-info-json  \
+    --write-annotations  \
+    --write-all-thumbnails  \
+    --all-subs  \
+    --embed-subs \
+    --no-call-home  \
+    ` # Note that a base directory  ./  does not work (for subtitles) `  \
+    --output '%(uploader)s/%(upload_date)s - %(title)s/%(title)s'  \
+    "$@"
+
+  # Trailing periods are invalid on Windows; remove them:
+  #
+  # Testing:
+  # \rmdir **/* ; \rmdir * ; \mkdir  --parents  a/a.  b/b.  c/c  a/testing.one.  a/testing.two  a/testing.a.three.  a/testing.a.four
+  for directory in *.; do
+    if [ -d "$directory" ]; then
+      \mv  "$directory" "$(basename "$directory" .)"
+    fi
+  done
+
+  for directory in *; do
+    if [ -d "$directory" ]; then
+      \cd "$directory"
+      for subdirectory in *.; do
+        if [ -d "$subdirectory" ]; then
+          \mv  "$subdirectory" "$(basename "$subdirectory" .)"
+        fi
+      done
+      \cd  -
+    fi
+  done
+
+  # I have no idea how to use youtube-dl's --output to fix the date format, so do it manually:
+  # Testing:
+  # \mkdir  --parents  a  '12345678 - one'  '1234-5678 - two'  '1234-56-78 - three'  a/a  'a/12345678 - one'  'a/1234-5678 - two'  'a/1234-56-78 - three'
+  for directory in *; do
+    if [ -d "$directory" ]; then
+      \cd "$directory"
+
+      # Testing:
+      # \rmdir **/* ; \rmdir * ; \mkdir  --parents  a  '12345678 - one'  '1234-5678 - two'  '1234-56-78 - three'
+      for subdirectory in *; do
+        if [ -d "$subdirectory" ]; then
+          target=$( \echo  "$subdirectory" | \sed 's/\(^[0-9]\{4\}\)\([0-9]\{2\}\)/\1-\2-/' )
+          if ! [ "$subdirectory" == "$target" ]; then
+            \mv  "$subdirectory"  "$target"
+          fi
+        fi
+      done
+
+      \cd  -
+    fi
+  done
+}
