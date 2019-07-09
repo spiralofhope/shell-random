80601a174ac44e32d67f6a405c955f97e72a652c
spiralofhope <spiralofhope@spiralofhope.com>
2019-07-09 16:45:31 -0700

    - Hardcoded path changes. - Fixed and expanded the  a:  type aliases.

--- live/wfl/aliases.sh
+++ live/wfl/aliases.sh
@@ -2,9 +2,6 @@
 
 
 
-alias  c:='nocorrect  \cd  /mnt/c'
-alias  C:=c:
-#alias  d:='nocorrect  \cd  /mnt/d'
 wfl_mount_drive() {
   if  ! [ -d /mnt/$1 ]; then
     \sudo  \mkdir  --parents  /mnt/$1
@@ -15,27 +12,38 @@ wfl_mount_drive() {
     fi
   \cd  /mnt/$1
 }
-alias  a:='wfl_mount_drive  d'
-alias  A:a:
-alias  b:='wfl_mount_drive  d'
-alias  B:b:
-alias  e:='wfl_mount_drive  e'
-alias  E:e:
-alias  f:='wfl_mount_drive  f'
-alias  F:f:
+alias  a:='nocorrect  wfl_mount_drive  a  '
+alias  A:='nocorrect                   a: '
+alias  b:='nocorrect  wfl_mount_drive  b  '
+alias  B:='nocorrect                   b: '
+alias  c:='nocorrect  wfl_mount_drive  c  '
+alias  C:='nocorrect                   c: '
+alias  d:='nocorrect  wfl_mount_drive  d  '
+alias  D:='nocorrect                   d: '
+alias  e:='nocorrect  wfl_mount_drive  e  '
+alias  E:='nocorrect                   e: '
+alias  f:='nocorrect  wfl_mount_drive  f  '
+alias  F:='nocorrect                   f: '
+#
+# These are generally not used, but are here just in case:
+alias  w:='nocorrect  wfl_mount_drive  w  '
+alias  w:='nocorrect                   w: '
+alias  x:='nocorrect  wfl_mount_drive  x  '
+alias  x:='nocorrect                   x: '
+alias  y:='nocorrect  wfl_mount_drive  y  '
+alias  y:='nocorrect                   y: '
+alias  z:='nocorrect  wfl_mount_drive  z  '
+alias  z:='nocorrect                   z: '
 
 
-local  l='/mnt/a/live'
-
-alias  l="nocorrect  \cd  $l"
 
 # -X is to sort by extension.
 # alias  ls='\ls  -1  --all  --classify  --color=auto  --show-control-chars  -X'
 \alias  current="\cd  /$l/OS/"
 
 # World of Warcraft
-alias  addons="nocorrect  \cd  /$l/games/World_of_Warcraft/_dotfiles/Interface/AddOns/"
-alias     wow="nocorrect  \cd  /$l/games/World_of_Warcraft/"
+alias  addons="nocorrect  \cd  /mnt/c/live/games/World_of_Warcraft/_dotfiles/Interface/AddOns/"
+alias     wow="nocorrect  \cd  /mnt/c/live/games/World_of_Warcraft/"
 
 
 screen() {
