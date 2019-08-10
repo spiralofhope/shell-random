7d9c9f3abc7fb5a2ea1389206bfb5ee17f747561
spiralofhope <spiralofhope@spiralofhope.com>
2019-08-09 20:19:14 -0700

    .

--- dead/download-debian-dependencies.sh
+++ dead/download-debian-dependencies.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+# Download all of the listed dependencies of `apt-cache show`.
+#
 # https://blog.spiralofhope.com/?p=2590
+#
+# Tested 2013-06-08 on:
+# Lubuntu 13.04
+# bash 4.2.45(1)-release (x86_64-pc-linux-gnu)
+# apt 0.9.7.7ubuntu4 for amd64 compiled on Apr 12 2013 23:49:01
 
 
 
