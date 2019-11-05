acf4b12487638448691179670aba4cfd28e32d94
spiralofhope <soh@spiralofhope.com>
2019-11-04 18:38:26 -0800

    working toward a great solution

--- live/zsh/dos-style-dir.sh
+++ live/zsh/dos-style-dir.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env  zsh
+# 
+# zsh 5.7.1-1
 
 
 
@@ -89,10 +91,65 @@ ddir() {
   \export  LC_COLLATE="$OLD_LC_COLLATE"
   OLD_LC_COLLATE=
 }
+
 lsd() {
-  \ls  --almost-all  -l  --color=always | \grep ^d | \awk '{print $9}'
+  #originally:
+  # Stopped working; was only showing the first word of a directory name
+  #\ls  --almost-all  -l  --color=always | \grep ^d | \awk '{print $9}'
+
+  :<<'  }'
+    {
+     # This just outputs everything as blue..
+     # Works in Dash 0.5.10.2-5
+    \tput  setaf  12  ;\
+    \ls  -1  --directory  */  |\
+      \sed  --expression='s/\/\/$//'  ;\
+    \tput  sgr0
+  }
+
+  #:<<'  }'
+  {
+  # This outputs things in color, but doesn't have pagination
+  # For pagination, one would have to manually do  `lsd | less`
+  # Works in Dash 0.5.10.2-5
+  # TODO - only symlinks leading to a directory
+  for i in .* *; do
+    if [ -d "$i" ] ||
+       [ -L "$i" ]; then
+      if    [ -d "$i" ]; then  \tput  setaf  12  ;  \echo  "$i"   #  blue
+      elif  [ -L "$i" ]; then  \tput  setaf  14  ;  \echo  "$i"   #  cyan
+      fi
+    fi
+  done
+  # reset to normal
+  \tput  sgr0
+  }
+
+
+  # An experiment to paginate
+  :<<'  }'
+  {
+  # The array is a zshism
+  # There's probably a bashism but I don't care enough to explore using Bash.
+  array=
+  for i in .* *; do
+    if [ -d "$i" ] ||
+       [ -s "$i" ]; then
+      if    [ -d "$i" ]; then  \tput  setaf  12  ;  if [ -z "$array" ]; then  array="$i"  ;  else  array="$array$IFS$i"  ;  fi   #  blue
+      elif  [ -s "$i" ]; then  \tput  setaf  14  ;  if [ -z "$array" ]; then  array="$i"  ;  else  array="$array$IFS$i"  ;  fi   #  cyan
+      fi
+    fi
+  done
+  \echo  $array | less
+  # reset to normal
+  \tput  sgr0
+  unset  array
+  }
+
+
+
 }
+alias ddir=lsd
 lsda() {
   \ls  --almost-all  -l  --color=always | \grep ^d
 }
-alias ddir=lsd
