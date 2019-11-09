10ed790ebd0aef63e442aaabd8e63c354b54521b
spiralofhope <soh@spiralofhope.com>
2019-11-09 14:39:53 -0800

    fiddling, but it's not really working so I'll redo everything

--- live/dotfiles/home-user--files/go.sh
+++ live/dotfiles/home-user--files/go.sh
@@ -1,4 +1,60 @@
 #!/bin/sh
 
 
-../home-user--dotfiles/go.sh  '.'
+
+debug=true
+
+# FIXME - must not be run as root
+# TODO - It's stupid of me to house files and directories without their leading dot.  Untangle this.
+# FIXME - zsh_histfile cannot be converted into a symlink while in a zsh shell!
+#         workaround:  \sh, then \ln, then \kill zsh (which also kills the terminal)
+
+date="$( \date +%Y-%m-%d--%H-%M-%S )"
+
+if [ -z $1 ]; then
+  dot='.'
+  \ln  --force  --no-target-directory  --symbolic  "$PWD"/               ~/00--home-user--dotfiles
+  \ln  --force  --no-target-directory  --symbolic  "$PWD"/descript.ion   ~/descript.ion
+else
+  \ln  --force  --no-target-directory  --symbolic  "$PWD"/               ~/00--home-user--files
+  \ln  --force  --no-target-directory  --symbolic  "$PWD"/.descript.ion  ~/.descript.ion
+fi
+
+
+\echo  ''
+\echo  ' * Preparing home/user/ dot files..'
+for i in *; do
+  if [ ! -f "$i" ]; then
+    continue
+  fi
+  if [ "$i" = 'go.sh' ]; then
+    continue
+  fi
+  if [ $debug ]; then
+    \echo  "$i"
+  fi
+  # Back up any existing files.
+  # If a file and not a symlink.
+  if [ -f ~/"$dot""$i" -a ! -L ~/"$dot""$i" ]; then
+    \mv  ~/"$dot""$i"  ~/"$dot""${i}"--"$date"
+  fi
+  \ln  --force  --no-target-directory  --symbolic  "$PWD"/"$i"  ~/"$dot""$i"
+done
+
+
+\echo  ''
+\echo  ' * Preparing home/user/ dot directories..'
+for i in *; do
+  if [ ! -d "$i" ]; then
+    continue
+  fi
+  # Back up any existing directories.
+  # If a directory and not a symlink.
+  if [ -d ~/"$dot""$i" -a ! -L ~/"$dot""$i" ]; then
+    \mv  ~/"$dot""$i"  ~/"$dot""${i}"--"$date"
+  fi
+  \ln  --force  --no-target-directory  --symbolic  "$PWD"/"$i"  ~/"$dot""$i"
+done
+
+
+\echo  ' .. done. '
