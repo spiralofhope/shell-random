dea41b54604371550eef20b309bb505e7819ae33
spiralofhope <soh@spiralofhope.com>
2019-11-18 08:11:41 -0800

    sorting, decrufting

--- live/sh/aliases.sh
+++ live/sh/aliases.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env  sh
 
+# FIXME - sudo may not always exist
 
 
-alias  cp='\cp  --interactive'
+alias  ls='\ls  -1  --all  --classify  --color=always  --group-directories-first  --show-control-chars'
+alias  cp='\cp  --interactive  --preserve=all'
+#alias  df='_df_sorted 5'                                               # sorted by mountpoint
+alias  df='_df_sorted 1'                                                # sorted by filesystem
 alias  mv='\mv  --interactive'
-alias  rm='\rm  --interactive'
+alias  rm='\rm  --interactive  --one-file-system'
 alias  md='\mkdir'
 alias  du='\du  --human-readable'
 alias  cls='\clear'
-alias  more='less  --quit-at-eof  --quit-if-one-screen'
+alias  more='\less  --quit-at-eof  --quit-if-one-screen'
+#alias  less='\less  --force  --RAW-CONTROL-CHARS  --quit-if-one-screen  $@'
+alias  less='\less  --RAW-CONTROL-CHARS'
+alias  nano='\nano  --mouse'
+alias  reboot="/bin/su  -c  '/sbin/shutdown  -r  -t now  now  rebooting'"
+alias  pm-suspend='\sudo  /usr/sbin/pm-suspend'
+alias  poweroff='\sudo  /sbin/poweroff'
+alias  grep='\grep  --color'                                            # Note that bash-windows does not support --color
+alias  eject='\eject  -i 0 ; \eject'                                    # Force-eject
+
 
 # This won't work on cygwin, and I'm not even sure what it was for..
 #alias  cd='\cd  --no-dereference'
@@ -17,37 +30,21 @@ alias  more='less  --quit-at-eof  --quit-if-one-screen'
 alias  ..='  \cd  ..'
 alias  cd..='\cd  ..'
 
-alias  less='\less  --RAW-CONTROL-CHARS'
-#alias  less='\less  --force  --RAW-CONTROL-CHARS  --quit-if-one-screen  $@'
-alias  nano='\nano  --mouse'
 
-alias  df='_df_sorted 5'    # sorted by mountpoint
-alias  df='_df_sorted 1'    # sorted by filesystem
+alias  current=" \cd  ~/l/path/current/"
+alias  previous="\cd  ~/l/path/previous/"
 
-# --
-# -- Linux console Applications
-# --
 
 # 2016-03-26 - Lubuntu (version not recorded)
 # 2016-03-28 - Slackware 14.1
 #alias  su="\sudo  $( \basename  $( \readlink  /proc/$$/exe ) )"
 #alias  sul='\sudo  \su  --login'
 
-# Go fuck yourself, drive.
-alias  eject='\eject  -i 0 ; \eject'
-# bash-windows does not support --color
-alias  grep='\grep --color'
 
 # TODO - What was I trying to achieve with this?
 # TODO - Improve
 #alias  killjobs='\kill  -9  $( \jobs -p )'
 
+
 # TODO? - What does this do?
 # alias  screen='TERM=screen screen'  # http://ubuntuforums.org/showthread.php?t=90910
-
-alias  pm-suspend='\sudo  /usr/sbin/pm-suspend'
-alias  reboot="    /bin/su  -c  '/sbin/shutdown  -r  -t now  now  rebooting'"
-alias  poweroff='  \sudo  /sbin/poweroff'
-
-alias  current=" \cd  ~/l/path/current/"
-alias  previous="\cd  ~/l/path/previous/"
