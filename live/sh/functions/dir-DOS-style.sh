baf99732c9e066db040da66d9246afb810c357d6
spiralofhope <soh@spiralofhope.com>
2019-11-19 10:26:16 -0800

    dash compatibility fix

--- live/sh/functions/dir-DOS-style.sh
+++ live/sh/functions/dir-DOS-style.sh
@@ -4,25 +4,26 @@
 
 
 
-#:<<'}'   
 # TODO? - Allow the user to pass a wildcard to restrict the listing.
-{
-  dir_tools(){
-    #  Works well, though it has various dependencies.  So what.
-    #  -Q  will add double quotes around everything.
-    \tree  $@  -C  -i  -L 1  --dirsfirst  |\
-      \tail --lines='+2'  |\
-      \head --lines='-2'  |\
-      \less  --RAW-CONTROL-CHARS  --quit-if-one-screen  --QUIT-AT-EOF
-  }
-  dir() {
-    if   [ "$1" == "/ad" ] || [ "$1" == "/AD" ]; then  dir_tools  -ad  -pugDF
-    elif [ "$1" == "/a"  ] || [ "$1" == "/A"  ]; then  dir_tools  -ah  -pugDF
-    elif [ "$1" == "/d"  ] || [ "$1" == "/D"  ]; then  dir_tools  -d
-    else                                               dir_tools
-    fi
-  }
+dir_tools(){
+  #  Works well, though it has various dependencies.  So what.
+  #  -Q  will add double quotes around everything.
+  \tree  $@  -C  -i  -L 1  --dirsfirst  |\
+    \tail --lines='+2'  |\
+    \head --lines='-2'  |\
+    \less  --RAW-CONTROL-CHARS  --quit-if-one-screen  --QUIT-AT-EOF
 }
+
+
+dir() {
+  if   [ "$1" = "/ad" ] || [ "$1" = "/AD" ]; then  dir_tools  -ad  -pugDF
+  elif [ "$1" = "/a"  ] || [ "$1" = "/A"  ]; then  dir_tools  -ah  -pugDF
+  elif [ "$1" = "/d"  ] || [ "$1" = "/D"  ]; then  dir_tools  -d
+  else                                             dir_tools
+  fi
+}
+
+
 alias DIR='dir $@'
 alias dir/a='dir /a'
 alias dir/ad='dir /ad'
