41000fd12e7b14ef2ff236a381dcd45fad8413bb
spiralofhope <soh@spiralofhope.com>
2019-11-19 17:49:57 -0800

    - reworked to somewhat simplify it - implemented a manual summary - implemented wildcards - introduced wildcard-related bugs

--- live/sh/functions/dir-DOS-style.sh
+++ live/sh/functions/dir-DOS-style.sh
@@ -4,27 +4,47 @@
 
 
 
-# TODO? - Allow the user to pass a wildcard to restrict the listing.
-dir_tools(){
-  #  Works well, though it has various dependencies.  So what.
-  #  -Q  will add double quotes around everything.
-  \tree  $@  -C  -i  -L 1  --dirsfirst  |\
-    \tail --lines='+2'  |\
-    \head --lines='-2'  |\
-    \less  --RAW-CONTROL-CHARS  --quit-if-one-screen  --QUIT-AT-EOF
-}
-
-
+# This only partially supports wildcards because `tree` appears to be broken.
+# You are better off doing things like dir/a|grep foo
+# wildcards also screw up summaries
+# wildcards also make it inappropriate to use tail, so detection is needed to work around that.
+# wildcards have --dirsfirst ignored
+# . basically wildcards suck.
 dir() {
-  if   [ "$1" = "/ad" ] || [ "$1" = "/AD" ]; then  dir_tools  -ad  -pugDF
-  elif [ "$1" = "/a"  ] || [ "$1" = "/A"  ]; then  dir_tools  -ah  -pugDF
-  elif [ "$1" = "/d"  ] || [ "$1" = "/D"  ]; then  dir_tools  -d
-  else                                             dir_tools
+  #echo parameters:  _${*}_
+  local  my_parameters=''
+
+  if   [ "$1" = '/ad' ] || [ "$1" = '/AD' ]; then  shift; my_parameters="-d   -pugDF  $*"  #; echo _1
+  elif [ "$1" = '/a'  ] || [ "$1" = '/A'  ]; then  shift; my_parameters="-ahs  -pugDF  --timefmt '%Y-%m-%d'  $*"  #; echo _2
+  elif [ "$1" = '/d'  ] || [ "$1" = '/D'  ]; then  shift; my_parameters="-d            $*"  #; echo _3
+  else                                                    my_parameters="              $*"  #; echo _4
   fi
+
+  # FIXME - I want --du but it can't exist alongside with a wildcard for unknown reasons.
+  # I can remove the `tail` and things almost appear.  It's odd.
+  #\echo "tree  -C  -i  -L 1  --dirsfirst  --timefmt '%Y-%m-%d'  --  $my_parameters"
+  \tree  --noreport  -C  -i  -L 1  --dirsfirst  $my_parameters |\
+    ` # --no-init is mandatory for Windows Subsystem for Linux. ` \
+    \less  --no-init  --RAW-CONTROL-CHARS  --quit-if-one-screen  --QUIT-AT-EOF
+
+    #\tail  --lines='+2'  |\
+
+
+  # Manually create a summary
+  # TODO - only display it with /a (that would be annoying)
+  # du doesn't seem to work.
+  #\du  --all  --human-readable  --max-depth=0  $*
+  #\echo  "$( \ls -ARgo "$@" | awk '{q += $3} END {print q}' ) bytes"
+  local __="$( \ls -ARgo "$@" | awk '{q += $3} END {print q}' )"
+  # add commas
+  \echo  -n  $__  |\
+    \sed  --expression=':a'  --expression='s/\(.*[0-9]\)\([0-9]\{3\}\)/\1,\2/;ta'
+  \echo ' bytes total'
+
 }
 
 
-alias DIR='dir $@'
-alias dir/a='dir /a'
-alias dir/ad='dir /ad'
-alias dir/d='dir /d'
+#alias  DIR="   dir ''"
+alias  dir/a=' dir /a'
+alias  dir/ad='dir /ad'
+alias  dir/d=' dir /d'
