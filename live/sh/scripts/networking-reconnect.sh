e925638e93b939532177df42cc51e22b11a5fc83
spiralofhope <soh@spiralofhope.com>
2019-12-02 07:48:22 -0800

    I'm lazy

---
+++ live/sh/scripts/networking-reconnect.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env  sh
+# Reconnect to networking
+# Because the UI doesn't appear to have anything, and I can't be bothered to remember this command.
+# Virtualbox/debian-10.1.0-amd64-xfce-CD-1.iso
+
+
+
+\sudo  \dhclient  enp0s3
