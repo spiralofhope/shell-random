ef2d85db792abc46b858e9c004986aa68ace0629
spiralofhope <soh@spiralofhope.com>
2019-12-21 13:22:21 -0800

    .

--- live/sh/scripts/networking-reconnect.sh
+++ live/sh/scripts/networking-reconnect.sh
@@ -4,5 +4,15 @@
 # Virtualbox/debian-10.1.0-amd64-xfce-CD-1.iso
 
 
+# You can also get more information with:
+# \ip  link  show
+# \ip  a
+# \cat  /proc/net/dev
+
 
 \sudo  \dhclient  enp0s3
+
+\echo  ''
+\echo  ' * Note:  If on VirtualBox, you may have better luck disconnecting and reconnecting:'
+\echo  '   <host>-home > Devices > Network > (Connect Network Adapter)'
+\echo  ''
