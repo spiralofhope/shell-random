b9d883478c127aab0315d5beaa7f8ecdf68226f2
spiralofhope <soh@spiralofhope.com>
2020-01-02 16:38:05 -0800

    fixed yet again

--- live/sh/scripts/drop_caches.sh
+++ live/sh/scripts/drop_caches.sh
@@ -12,7 +12,7 @@
 # 3 to free pagecache, dentries and inodes
 __=3
 
-\sudo  \sh  -c "/usr/bin/echo $__ > /proc/sys/vm/drop_caches"
+\sudo  \sh  -c "/bin/echo $__ > /proc/sys/vm/drop_caches"
 \sudo  /sbin/sysctl  --write vm.drop_caches=$__
 
 \sudo  \sync
