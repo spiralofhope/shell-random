d8de13bce680f6c3c6399f96f31a7ac775380b4f
spiralofhope <soh@spiralofhope.com>
2020-01-04 01:08:32 -0800

    I don't even know how to name this..

---
+++ live/sh/scripts/examples/environment-variables-unique-to-script.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env  sh
+# Run a script, passing environment variables unique to it.
+
+
+# For example, $SHELL is almost always set.
+# But perhaps you want it to become something else.
+# BUT only for the script and not anything else in your shell.
+#
+# Normally you would do something like the following on the commandline:
+#
+#   shell_old=$SHELL
+#   SHELL=text
+#   ./script.sh
+#   SHELL=$shell_old
+#   shell_old=
+#
+# While this works for some cases, what if you have many environment variables you wish to do this with?
+#
+# This is simplified using  `env`
+
+
+
+\echo  "\$SHELL is currently:  $SHELL"
+\echo  "\$HOME  is currently:  $HOME"
+
+
+if [ -z "$1" ]; then
+  \env  SHELL='replacement $SHELL'  HOME='replacement $HOME'   "$0"  'parameter 1'  'parameter 2'
+  
+  :<<'  }'   #  It can be more neatly written like:
+  {
+  \env  \
+    SHELL='replacement $SHELL'  \
+    HOME='replacement $HOME'  \
+    "$0"  \
+      'parameter 1'  \
+      'parameter 2'  \
+    `#`
+  }
+
+fi
