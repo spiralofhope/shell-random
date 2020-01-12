333f990f1d165faab494245633fb384d12f75a96
spiralofhope <soh@spiralofhope.com>
2020-01-11 16:08:47 -0800

    .

---
+++ live/bash/scripts/examples/launch bash, run command, stay in that shell.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env  bash
+# - Launch bash
+# - Run commands
+# - Stay in that bash shell
+
+# https://stackoverflow.com/a/7193037
+
+
+
+\bash  --rcfile  <(  \
+  \echo '.  ~/.bashrc  ;\
+    \ls  --color=never  ;\
+    \ls  --color=auto   ;\
+  '  \
+)
