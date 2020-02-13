47aa9966dec20ad6e669ee69cb6718bb7ef5b2b0
spiralofhope <soh@spiralofhope.com>
2020-02-13 00:19:18 -0800

    updated, found sources

--- live/git/rewrite-editors.sh
+++ live/git/rewrite-editors.sh
@@ -1,24 +1,31 @@
 #!/usr/bin/env  sh
 # Search-replace an editor's name and email address.
-  #  Note that you may have to nuke and re-create your remote repository.  I don't know.
-# At least as old as 2017-10-20
+# TODO - this can be made more fancy by accepting user input so it doesn't have to be edited directly.
+# https://help.github.com/en/github/using-git/changing-author-info
 
+
+
+# \git  clone  --bare https://github.com/user/repo.git
+# cd  repo.git
 
 
 \git  filter-branch  --env-filter '
-WRONG_EMAIL="wrong@example.com"
-NEW_NAME="Some Name"
-NEW_EMAIL="author@example.com"
+email_search="search@example.com"
+email_replace="replace@example.com"
+name_replace="user"
 
-if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
-then
-    export GIT_COMMITTER_NAME="$NEW_NAME"
-    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
+if [ "$GIT_COMMITTER_EMAIL" = "$email_search" ]; then
+    export  GIT_COMMITTER_NAME="$name_replace"
+    export  GIT_COMMITTER_EMAIL="$email_replace"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
-then
-    export GIT_AUTHOR_NAME="$NEW_NAME"
-    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+if [ "$GIT_AUTHOR_EMAIL" = "$email_search" ]; then
+    export  GIT_AUTHOR_NAME="$name_replace"
+    export  GIT_AUTHOR_EMAIL="$email_replace"
 fi
 # -f is force the overwrite of any backup
-' -f --tag-name-filter cat -- --branches --tags
+'  --force  --tag-name-filter  cat  --  --branches  --tags
+
+
+# \git  push  --force  --tags origin 'refs/heads/*'
+# \cd ..
+# \rm  --recursive  --force  ./repo.git
