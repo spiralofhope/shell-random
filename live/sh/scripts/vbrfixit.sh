42de5480abf039116bc4d845a35ea9c4895050d8
spiralofhope <soh@spiralofhope.com>
2020-03-10 00:06:33 -0700

    code cleanup

--- live/sh/scripts/vbrfixit.sh
+++ live/sh/scripts/vbrfixit.sh
@@ -2,18 +2,18 @@
 
 
 
-# TODO - deduplicate this
+_go() {
+  \echo  " * Processing:    $1"
+  \vbrfix  -always  "$1"  out
+  \mv  --force  out  "$1"
+}
+
+
+
 if [ -z $1 ]; then
-  for i in *; do
-    \echo ----- "$i"
-    if [ -f "$i" ]; then
-      \vbrfix  -always  "$i"  out
-      \mv  --force  out  "$i"
-    fi
+  for i in *.mp3; do
+    _go  "$i"
   done
 else
-  if [ -f "$i" ]; then
-    \vbrfix  -always  "$i"  out
-    \mv  --force  out  "$i"
-  fi
+  _go  "$1"
 fi
