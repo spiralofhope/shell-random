654fe18afdcf517fcf88d9479dd7c8eba570fc64
spiralofhope <soh@spiralofhope.com>
2020-03-10 00:58:29 -0700

    - use a proper temp file - general update

--- live/sh/scripts/vbrfixit.sh
+++ live/sh/scripts/vbrfixit.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env  sh
+# A helper for the program `vbrfix`
+#   .. because it's stupid.
 
 
 
 _go() {
-  \echo  " * Processing:    $1"
-  \vbrfix  -always  "$1"  out
-  \mv  --force  out  "$1"
+  file_source="$1"
+  \echo  " * VBR fixing:  $file_source"
+  local  tempfile=$( \mktemp  --suffix=".transcode-audio-into-mp3.$$" )
+  \vbrfix  -always  -makevbr  "$file_source"  $tempfile
+  \mv  --force  $tempfile  "$file_source"
+  \rm  --force  $verbose  vbrfix.log  vbrfix.tmp  $tempfile
 }
 
 
