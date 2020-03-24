35039a6c54acf61a33a60afc5657ae413a8405c3
spiralofhope <soh@spiralofhope.com>
2020-03-23 20:50:40 -0700

    shellchecked

--- live/sh/scripts/ziprepair.sh
+++ live/sh/scripts/ziprepair.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env  sh
-
-# Repairs and unzips a broken .zip file
+# Repairs then unzips a broken .zip file
 
 
 
 file="ziprepair.$$.zip"
 dir="$1".ziprepair.$$
 
-\echo  y  |  \zip  --fixfix "$1"  --out "$file"
+\echo  'y'  |  \zip  --fixfix "$1"  --out "$file"
 \mkdir  "$dir"
-\cd     "$dir"
+\cd     "$dir"  ||  exit  1
 \unzip  -o  ../"$file"
-\cd  -
+\cd  -  ||  exit  1
 \rm  --force  "$file"
 
-#  \unzip  -tqq  $file  >> /dev/null 2>&1
+#  \unzip  -tqq  "$file"  >> /dev/null 2>&1
