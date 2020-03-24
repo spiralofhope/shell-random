bc3668ea379709030e0a6c08067b45777eff0b88
spiralofhope <soh@spiralofhope.com>
2020-03-23 21:21:22 -0700

    shellcheck

--- live/sh/scripts/examples/redirection_basics.sh
+++ live/sh/scripts/examples/redirection_basics.sh
@@ -5,49 +5,60 @@
 
 
 
-# If it outputs something, let it.
-\expr 10 + 10
-\echo  $?
+#:<<'}'   #  If it outputs something, display it (no redirection).
+{
+\echo  $(( 10 + 10 ))
 # =>
 # 20
-# 0
+}
 
 
-# Suppress regular output
-\expr 10 + 10  > /dev/null
-\echo  $?
+#:<<'}'   #  If it fails to execute and outputs error text, display it (no redirection).
+{
+\sh  --nonexistant_flag
 # =>
-# 0
+# sh: 0: Illegal option --
+# 
+# Note that if the command generates an error then the whole script will halt and exit with that error code at this point.  See  `trapping-signals.sh`  for more.
+}
 
 
-# If it outputs something, let it.
-\expr 10 + a
-\echo  $?
+#:<<'}'   #  Suppress regular (but not error) output
+{
+\echo  $(( 10 + 10 ))  > /dev/null
 # =>
-# expr: non-integer argument
-# 2
-
-
-# If it outputs an error, stop it.
-\expr 10 + a  2> /dev/null
-\echo  $?
+# 
+# An example of an error:
+\sh  --nonexistant_flag   > /dev/null
 # =>
-# 2
+# sh: 0: Illegal option --
+}
 
 
-# If it outputs an error, or regular output, stop it.
-\expr 10 + a  > /dev/null 2> /dev/null
-\echo  $?
-\expr 10 + 10 > /dev/null 2> /dev/null
-\echo  $?
+#:<<'}'   #  Suppress error (but not regular) output.
+{
+\sh  --nonexistant_flag   2> /dev/null
 # =>
-# 2
-# 0
+# 
+}
 
 
-# Force commandline application non-interactivity
-# Programs like FFmpeg can act non-interactively from a script, but if they're at a prompt they go interactive.  This screws up the copy-and-paste I like doing.
-\expr 10 + 10  >/dev/null 2>/dev/null </dev/null
+#:<<'}'   #  Suppress regular and error output; display nothing.
+{
+\sh  --nonexistant_flag   > /dev/null  2> /dev/null
+# =>
+# 
+}
 
+
+#:<<'}'   #  Force commandline application non-interactivity
+         # Programs like FFmpeg can act non-interactively from a script, but if they're at a prompt they go interactive.  This screws up the copy-and-paste I like doing.
+{
+\some_command  >/dev/null 2>/dev/null </dev/null
+# =>
+# 
 # You can also send it to the background:
-\expr 10 + 10  >/dev/null 2>/dev/null </dev/null &
+\some_command  >/dev/null 2>/dev/null </dev/null &
+# =>
+# 
+}
