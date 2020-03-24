05e86112c9bdb8a7a75ba91da8a4b81cae16cad8
spiralofhope <soh@spiralofhope.com>
2020-03-23 23:04:08 -0700

    This stopped working!

--- live/sh/scripts/examples/case-insensitivity.sh
+++ live/sh/scripts/examples/case-insensitivity.sh
@@ -3,38 +3,42 @@
 # Demonstrate case-sensitive and -insensitive settings.
 # .. both for programs and for a "for loop".
 
+# This no longer works as of
+# 2020-03-23 - Debian 10.1.0-amd64-xfce-CD-1
+
+
+working_directory=$( \mktemp  --directory )
 
 
 command() {
-  # ls  -1  $tmp/
-  for i in $tmp/*; do
-    \echo  $i
+  #ls  -1  "$working_directory"/
+  for i in "$working_directory"/*; do
+    \echo  "$i"
   done
 }
 
 
 setup() {
-  tmp=/tmp/conditionals.$$
-  \mkdir  $tmp/
-  \mkdir  $tmp/aaa
-  \mkdir  $tmp/AAA
-  \mkdir  $tmp/bbb
-  \mkdir  $tmp/BBB
-  \mkdir  $tmp/ccc
-  \mkdir  $tmp/CCC
+  \mkdir  "$working_directory"/aaa
+  \mkdir  "$working_directory"/AAA
+  \mkdir  "$working_directory"/bbb
+  \mkdir  "$working_directory"/BBB
+  \mkdir  "$working_directory"/ccc
+  \mkdir  "$working_directory"/CCC
 }
 
 
 go() {
-
-
-  \echo
+  \echo  ''
   \echo  '-- before'
   command
 
-  \echo
+  \echo  ''
   \echo  '-- case-insensitive'
+  # This used to work
   LC_COLLATE=en_US ; export LC_COLLATE
+  # This does not work either:
+  #LC_COLLATE=en_US.utf8 ; export LC_COLLATE
   command
   # =>
   #aaa
@@ -44,7 +48,7 @@ go() {
   #ccc
   #CCC
 
-  \echo
+  \echo  ''
   \echo  '-- case-sensitive'
   LC_COLLATE=C ; export LC_COLLATE
   command
@@ -59,8 +63,8 @@ go() {
 
 
 teardown() {
-  \rmdir  $tmp/*
-  \rmdir  $tmp/
+  \rmdir  "$working_directory"/*
+  \rmdir  "$working_directory"
 }
 
 
