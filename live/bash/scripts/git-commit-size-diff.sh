cd80c6c178f3aff88aafbfa4576718c57c36d8d5
spiralofhope <soh@spiralofhope.com>
2020-03-25 04:23:50 -0700

    - shellcheck - converted to sh

--- live/bash/scripts/git-commit-size-diff.sh
+++ live/bash/scripts/git-commit-size-diff.sh
@@ -1,34 +1,45 @@
-#!/usr/bin/env  bash
+#!/usr/bin/env  sh
 # Learn the difference in filesize between repository commits
-# Copied from https://stackoverflow.com/a/10847242
-# FIXME - Make this a dash-comparible script:  remove bashisms and use `\bc`
+# Modified from https://stackoverflow.com/a/10847242
 
 
 
 USAGE='[--cached] [<rev-list-options>...]
 
-Show file size changes between two commits or the index and a commit.'
+Show file size changes between two commits or the index and a commit.
 
-. "$(git --exec-path)/git-sh-setup"
-args=$(git rev-parse --sq "$@")
+example of use:
+'"$( \basename  "$0" )"'  HEAD~1  HEAD~2
+'"$( \basename  "$0" )"'  --cached master
+'
+
+
+
+.  "$( \git  --exec-path )/git-sh-setup"
+args=$( \git  rev-parse  --sq "$@" )
 [ -n "$args" ] || usage
-cmd="diff-tree -r"
-[[ $args =~ "--cached" ]] && cmd="diff-index"
-eval "git $cmd $args" | {
+
+
+
+if [ "$1" = '--cached' ]; then
+  cmd="diff-index"
+else
+  cmd="diff-tree -r"
+fi
+eval "\git $cmd $args" | {
   total=0
-  while read A B C D M P
-  do
+  while  \read  -r  A B C D M P; do
     case $M in
-      M) bytes=$(( $(git cat-file -s $D) - $(git cat-file -s $C) )) ;;
-      A) bytes=$(git cat-file -s $D) ;;
-      D) bytes=-$(git cat-file -s $C) ;;
+      M)  bytes="$(( $( \git  cat-file  -s "$D" ) - $( \git  cat-file  -s "$C" ) ))"  ;;
+      A)  bytes="$(  \git cat-file -s "$D" )"  ;;
+      D)  bytes="-$( \git cat-file -s "$C" )"  ;;
       *)
-        echo >&2 warning: unhandled mode $M in \"$A $B $C $D $M $P\"
+        \echo  >&2  "warning: unhandled mode $M in \"$A $B $C $D $M $P\""
         continue
-        ;;
+      ;;
     esac
-    total=$(( $total + $bytes ))
-    printf '%d\t%s\n' $bytes "$P"
+    total=$(( total + bytes ))
+    \printf  '%d\t%s\n'  "$bytes"  "$P"
   done
-  echo total $total
+  \echo  "total $total"
 }
