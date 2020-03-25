184d6626a16baec17f75cfe096475c5413161874
spiralofhope <soh@spiralofhope.com>
2020-03-25 08:06:52 -0700

    moved notes from the zsh example, since it all works under sh

--- live/sh/scripts/examples/command-in-a-variable.sh
+++ live/sh/scripts/examples/command-in-a-variable.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env  sh
+#
+# See https://blog.spiralofhope.com/?p=845
 
 
+\echo ' * 1 --------'
+
+a="ls -a -l"
+eval "$a"
+
+\echo ' * 2 --------'
+
+a="ls"
+b="-a -l"
+eval "$a" "$b"
+
+\echo ' * 3 --------'
 
 a='ls'
 b='-a -l'
