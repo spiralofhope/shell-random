a8996d8c2bb2d3c0715363ff91a5832ffe338ecf
spiralofhope <soh@spiralofhope.com>
2020-03-27 23:37:22 -0700

    elaboration

--- live/sh/scripts/examples/variables.sh
+++ live/sh/scripts/examples/variables.sh
@@ -2,23 +2,43 @@
 
 
 
-#variable=1
-#echo $variable
+:<<'}'   #  Simple
+{
+  variable=example text one
+  \echo  "$variable"
+  # =>
+  # example text one
+  
+}
 
 
+:<<'}'   #  Set if it isn't already set
+{
+  unset  variable
+  : "${variable:=example text two}"
+  \echo  "$variable"
+  # =>
+  # example text two
+}
 
-unset  variable
-: "${variable:=example text one}"
-\echo  "$variable"
+:<<'}'   #  Set if it isn't already set
+{
+  variable='example text three'
+  : "${variable:=replacement text}"
+  \echo  "$variable"
+  # =>
+  # example text three
+
+  # So you can do things like this (which will do nothing if $PATH is already set.)
+  #: "${PATH:=$PATH:example text two}"
+  #\echo  "$PATH"
+}
 
 
-variable='example'
-#: "${variable:=}"
-# Set the variable if it isn't already set
-: "${variable:=example text two}"
-\echo  "$variable"
+#:<<'}'   #  If the variable is set, act on it
+{
+  : "${__:=$PATH}"
+  # Appending to the path:
+  echo "testing;$__"
+}
 
-# So you can do things like:
-
-: "${PATH:=$PATH:example text two}"
-\echo  "$PATH"
