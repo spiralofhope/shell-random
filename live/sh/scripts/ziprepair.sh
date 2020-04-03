9f7436ec808b0af8185c9e58c84493e76c36ad7b
spiralofhope <soh@spiralofhope.com>
2020-04-03 02:14:44 -0700

    no, shellcheck, tempfile is better.

--- live/sh/scripts/ziprepair.sh
+++ live/sh/scripts/ziprepair.sh
@@ -1,27 +1,38 @@
 #!/usr/bin/env  sh
 # Repairs then unzips a broken .zip file
+# shellcheck disable=2186
+# mktemp does not support:
+#   -  using the current directory
+#   -  a prefix
+
 
 # Note that because this is a script and not a procedure the user will end up in the same directory they began in.  This does not act like unc()
 
 
 
-if [ -z "$*" ]; then exit 1; fi
+if  [   -z "$*" ] || \
+    [ ! -f "$1" ]
+then
+  exit  1
+fi
 
 
-# test
+
+# testing:
 # touch some_file ; zip some_zip.zip some_file ; rm -f some_file
 
 
 
-# setup
+# setup:
 temporary_file=$(      \tempfile  --directory ./  --prefix 'file_'  --suffix '.ziprepair.zip'  ||  exit $? )
 temporary_directory=$( \tempfile  --directory ./  --prefix 'dir_'   --suffix '.ziprepair'      ||  exit $? )
+# `tempfile` does not support creating a temporary directory:
 \rm  --force  "$temporary_directory"  ||  exit  $?
 \mkdir        "$temporary_directory"  ||  exit  $?
 
 
 
-# go
+# go:
 \echo  'y'  |  \zip  --fixfix "$1"  --out "$temporary_file"
 \cd     "$temporary_directory"  ||  exit  $?
 \unzip  -o  ../"$temporary_file"
@@ -29,5 +40,5 @@ temporary_directory=$( \tempfile  --directory ./  --prefix 'dir_'   --suffix '.z
 
 
 
-# teardown
+# teardown:
 \rm  --force  ../"$temporary_file"  ||  exit $?
