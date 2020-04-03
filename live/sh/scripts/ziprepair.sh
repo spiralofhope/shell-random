2faea2d07b5a3d3d2d421bf801d562434e0e46a5
spiralofhope <soh@spiralofhope.com>
2020-04-03 02:07:18 -0700

    - decrufted - using a proper temporary file and directory.

--- live/sh/scripts/ziprepair.sh
+++ live/sh/scripts/ziprepair.sh
@@ -1,16 +1,33 @@
 #!/usr/bin/env  sh
 # Repairs then unzips a broken .zip file
 
+# Note that because this is a script and not a procedure the user will end up in the same directory they began in.  This does not act like unc()
 
 
-file="ziprepair.$$.zip"
-dir="$1".ziprepair.$$
 
-\echo  'y'  |  \zip  --fixfix "$1"  --out "$file"
-\mkdir  "$dir"
-\cd     "$dir"  ||  exit  1
-\unzip  -o  ../"$file"
-\cd  -  ||  exit  1
-\rm  --force  "$file"
+if [ -z "$*" ]; then exit 1; fi
 
-#  \unzip  -tqq  "$file"  >> /dev/null 2>&1
+
+# test
+# touch some_file ; zip some_zip.zip some_file ; rm -f some_file
+
+
+
+# setup
+temporary_file=$(      \tempfile  --directory ./  --prefix 'file_'  --suffix '.ziprepair.zip'  ||  exit $? )
+temporary_directory=$( \tempfile  --directory ./  --prefix 'dir_'   --suffix '.ziprepair'      ||  exit $? )
+\rm  --force  "$temporary_directory"  ||  exit  $?
+\mkdir        "$temporary_directory"  ||  exit  $?
+
+
+
+# go
+\echo  'y'  |  \zip  --fixfix "$1"  --out "$temporary_file"
+\cd     "$temporary_directory"  ||  exit  $?
+\unzip  -o  ../"$temporary_file"
+#  \unzip  -tqq  "$temporary_file"  >> /dev/null 2>&1
+
+
+
+# teardown
+\rm  --force  ../"$temporary_file"  ||  exit $?
