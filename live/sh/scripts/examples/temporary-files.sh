7747fb15e735a7cc1c15662ddb988082fe880ae2
spiralofhope <soh@spiralofhope.com>
2020-04-10 18:21:25 -0700

    Cleaned up a little.

--- live/sh/scripts/examples/temporary-files.sh
+++ live/sh/scripts/examples/temporary-files.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env  sh
+# The program `tempfile` usually also exists, but it is deprecated.  Note, however, that only `tempfile` has  --prefix
 
 
 
-#:<<'}'   #  Referencing the current process' PID ($$).
+:<<'}'   #  Referencing the current process' PID ($$).
 {
   # While $$ is easy to remember, this is not 100% guaranteed to be a unique filename.
   # It's also not appropriate when your script needs to refer to use several unique tempfiles.
   unique_file="/tmp/my_temporary_file.$$"
-  \touch        "$unique_file"
-  \ls           "$unique_file"
-  \rm  --force  "$unique_file"
+  # Make the file
+  :>                       "$unique_file"
+  \rm  --force  --verbose  "$unique_file"
 }
 
 
@@ -18,8 +19,7 @@
   # GNU mktemp will create a file which is guaranteed to be unique.
   # By default it will use `/tmp` but you can specify another directory with the --tmpdir switch.
   unique_file=$( \mktemp )
-  \ls           "$unique_file"
-  \rm  --force  "$unique_file"
+  \rm  --force  --verbose  "$unique_file"
 }
 
 
@@ -31,12 +31,11 @@
   # You can create a unique string to help identify/organize your tempfiles.
   unique_file=$( \mktemp  --suffix=".my_temporary_file.$$" )
   # example file:  /tmp/tmp.CjgG1vqFFC.my_temporary_file.17065
-  \ls           "$unique_file"
-  \rm  --force  "$unique_file"
+  \rm  --force  --verbose  "$unique_file"
 }
 
 
-:<<'}'   #  Using  `mktemp`  to note a reasonably-unique temporary filename.
+#:<<'}'   #  Using  `mktemp`  to note a reasonably-unique temporary filename.
          #  This cannot be guaranteed to be unique since something else could theoretically come along and happen to make a file with that exact name.
 {
   # Consider using "local" if this is in a function.
