729d7b7d21bb81d85f83630a62e83c91f5a875be
spiralofhope <soh@spiralofhope.com>
2020-04-28 20:58:59 -0700

    nocorrect killall

--- live/zsh/aliases.sh
+++ live/zsh/aliases.sh
@@ -6,16 +6,17 @@
 # autoload -U zsh-mime-setup
 # zsh-mime-setup
 
-alias  apt='  nocorrect  \apt'
-alias  cp='   nocorrect  \cp  --interactive  --preserve=all'
-alias  git='  nocorrect  \git'
-alias  ls='   nocorrect  \ls  -1  --all  --classify  --color=always  --group-directories-first  --show-control-chars'
-alias  man='  nocorrect  \man'
-alias  mkdir='nocorrect  \mkdir'
-alias  mv='   nocorrect  \mv  --interactive'
-alias  ping=' nocorrect  \ping'
-alias  rm='   nocorrect  \rm  --interactive  --one-file-system'
-alias  geany='nocorrect  geany'
+alias  apt='    nocorrect  \apt'
+alias  cp='     nocorrect  \cp  --interactive  --preserve=all'
+alias  geany='  nocorrect  geany'
+alias  git='    nocorrect  \git'
+alias  killall='nocorrect  \killall'
+alias  ls='     nocorrect  \ls  -1  --all  --classify  --color=always  --group-directories-first  --show-control-chars'
+alias  man='    nocorrect  \man'
+alias  mv='     nocorrect  \mv  --interactive'
+alias  mkdir='  nocorrect  \mkdir'
+alias  ping='   nocorrect  \ping'
+alias  rm='     nocorrect  \rm  --interactive  --one-file-system'
 
 
 # --  Suffixes
