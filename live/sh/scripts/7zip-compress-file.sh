3ae6f344c07028aca4f2c8b8fb77f1939cf9e068
spiralofhope <soh@spiralofhope.com>
2020-06-06 01:28:35 -0700

    .

--- live/sh/scripts/7zip-compress-file.sh
+++ live/sh/scripts/7zip-compress-file.sh
@@ -6,10 +6,9 @@
 # -- 7zip compression helper.
 # --   Because it's really this difficult to remember how in the fuck to use it.
 # --
-
-
-
+# TODO - borrow from  7zip-compress-directory.sh
 # TODO? - If -z $1, then prompt to doing all files in the current directory.
+
 
 \7z  a  -mx=9  "$1".7z  "$1" && \
   \rm  --force  --verbose  "$1"
