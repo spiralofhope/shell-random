f7fd5b980c25e62a6360131b22b9c601b27e57d8
spiralofhope <soh@spiralofhope.com>
2020-06-06 01:28:40 -0700

    significantly expanded-upon

--- live/sh/scripts/7zip-compress-directory.sh
+++ live/sh/scripts/7zip-compress-directory.sh
@@ -6,12 +6,44 @@
 # -- 7zip compression helper.
 # --   Because it's really this difficult to remember how in the fuck to use it.
 # --
-# summon with -m0 to simply store and not compress at all.
+
+
+if   [ -z "$1" ]  \
+|| ! [ -d "$1" ]; then
+  \echo  'Usage:'
+  \echo  "  $( \basename $0 )  \"source_directory\"  \"target_directory_for_file\""
+  \echo  ''
+  \echo  'For no compression (store):'
+  \echo  "  $( \basename $0 )  \"source_directory\"  -m0"
+  \echo  ''
+  return  1
+fi
+# TODO - if the file already exists, prompt to insert into that existing archive
 
 
 source="$1"
-target=$( \basename "$source" )
-target="./${target}.tar.7z"
 shift
-\tar  cf  -  "$source" |\
-  \7za a -si "$*"  "$target"
+if [ -z "$1" ]; then 
+  target=$( \basename "$source" )
+  target="./${target}.tar.7z"
+else
+echo here
+  target="$( \realpath $1 )"
+  target="${target}/${source}.tar.7z"
+  shift
+fi
+\echo  "compressing:     $source"
+\echo  "compressing to:  $target"
+\echo  "additional flags:  $*"
+
+
+# tar
+#   --create   create archive (.7z)
+#   f             unknown, and the man page doesn't say.
+#   -             unknown, and the man page doesn't say.
+# 7za
+#   a          create archive (.tar)
+#   -si        standard input
+#   $*         any other inputs the user passes, like -m0 for no compression (store)
+\tar  --create  -f  -  "$source" |\
+  \7za  a   -si "$target"
