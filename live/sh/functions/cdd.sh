6d91bd716fe0d19fb74be7c1b480fe93d6c5e3b4
spiralofhope <soh@spiralofhope.com>
2020-06-07 01:58:42 -0800

    .

--- live/sh/functions/cdd.sh
+++ live/sh/functions/cdd.sh
@@ -6,9 +6,9 @@
 # I'd love to hijack 'cd' directly using cd(), but that doesn't work.
 cdd() {
   _target="$( \realpath "$*" )"
-  if   [ -L "$*" ] && [ -d "$_target" ] ; then  \cd  "$_target"                   ||  exit
-  elif                [ -f "$_target" ] ; then  \cd  "$( \dirname  "$_target" )"  ||  exit
-  else                                          \cd  "$*"                         ||  exit
+  if   [ -L "$*" ] && [ -d "$_target" ] ; then  \cd  "$_target"                   ||  return  $?
+  elif                [ -f "$_target" ] ; then  \cd  "$( \dirname  "$_target" )"  ||  return  $?
+  else                                          \cd  "$*"                         ||  return  $?
   fi
   unset  _target
 }
