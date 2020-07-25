fcb199e9b1635f36c93d43dfaffef96a333544bf
spiralofhope <soh@spiralofhope.com>
2020-07-25 15:27:38 -0700

    A simple iteration.
    To be used in more complex code..

---
+++ live/sh/scripts/examples/iterate-over-characters-in-a-string.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env  sh
+
+# Thanks to:
+#   https://stackoverflow.com/a/51052644/1190568
+
+
+
+if [ -z "$*" ]; then
+  # Pass example parameters to this very script:
+  "$0"  'string'
+  return
+fi
+
+
+string="$1"
+
+
+__="$string"
+while [ -n "$__" ]; do
+  # All but the first character of the string
+  rest="${__#?}"
+  #  Remove $rest, and you're left with the first character
+  first="${__%"$rest"}"
+  echo "$first"
+  __="$rest"
+done
