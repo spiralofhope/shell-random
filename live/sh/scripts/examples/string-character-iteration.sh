f25967261d312ab0a2203656cbdf522f5bc8c787
spiralofhope <soh@spiralofhope.com>
2020-07-27 18:41:13 -0700

    style

---
+++ live/sh/scripts/examples/string-character-iteration.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env  sh
+
+# Thanks to:
+#   https://stackoverflow.com/a/51052644/1190568
+
+
+
+if [ -z "$*" ]; then
+  # Pass example parameters to this very script:
+  "$0"  'string'
+  return
+fi
+
+
+string="$1"
+
+
+__="$string"
+while [ -n "$__" ]; do
+  string_without_first_character="${__#?}"
+  first_character="${__%"$string_without_first_character"}"
+  \echo  "$first_character"
+  __="$string_without_first_character"
+done
