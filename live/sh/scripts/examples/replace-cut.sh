0872b5348b7a65dfe6b5c2bb696c494e584110cb
spiralofhope <soh@spiralofhope.com>
2020-07-30 02:52:31 -0700

    tidied up so it's reusable

--- live/sh/scripts/examples/replace-cut.sh
+++ live/sh/scripts/examples/replace-cut.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env  sh
 
-# Split a string on a character and output one of the items.
+# Split a string on a single character and output one selected field.
 
 # Thanks to:
 #   https://github.com/dylanaraps/pure-sh-bible/#split-a-string-on-a-delimiter
+
+# TODO - Be able to select a range of fields.
 
 
 
@@ -11,14 +13,8 @@ if [ -z "$*" ]; then
   # Pass example parameters to this very script:
   "$0"  ','  2  apples,oranges,pears,grapes
   # => oranges
-  #"$0"  ','  4  apples,oranges,pears,grapes
-  # => grapes
   return
 fi
-
-
-#\echo  "$*"
-
 
 
 # TODO - proper help text.
@@ -29,56 +25,46 @@ fi
 
 
 
-# aka "field separator"
-character_to_split_on="$1"
-which_match_number_to_output="$2"
-shift
-shift
-text="$*"
-
-
-
-# Disable globbing.
-# This ensures that the word-splitting is safe.
-set  -f
-
-# Store the current value of 'IFS' so we
-# can restore it later.
-old_ifs=$IFS
-
-# Change the field separator to what we're
-# splitting on.
-IFS=$character_to_split_on
-
-# Create an argument list splitting at each
-# occurance of '$character_to_split_on'.
-#
-# This is safe to disable as it just warns against
-# word-splitting which is the behavior we expect.
-# shellcheck disable=2086
-set  --  "$text"
-
-
-output_part_number(){
-  number_to_output=$1
+replace_cut() {
+  # replace_cut  ','  2  apples,oranges,pears,grapes
+  # => oranges
+  #
+  field_separator="$1"
+  field="$2"
   shift
-  while [ $# -ne 0 ]; do
-    i="$(( i + 1 ))"
-    if [ "$i" -eq "$number_to_output" ]; then echo "$1"; break; fi
-    #if [ "$i" -eq "$number_to_output" ]; then echo "$i - $1"; break; fi
+  shift
+  text="$*"
+  #
+  # Disable globbing:
+  #   (this ensures that the word-splitting is safe)
+  set  -f
+  old_ifs=$IFS
+  # Change the field separator to what we're splitting on:
+  IFS=$field_separator
+  # Create an argument list splitting at each occurance of '$field_separator':
+  # shellcheck disable=2086
+  #   This is safe to disable as it just warns against word-splitting which is the behavior we expect.
+  set  --  "$text"
+  #
+  output_part_number(){
+    number_to_output=$1
     shift
-  done
+    while [ $# -ne 0 ]; do
+      i="$(( i + 1 ))"
+      if [ "$i" -eq "$number_to_output" ]; then echo "$1"; break; fi
+      #if [ "$i" -eq "$number_to_output" ]; then echo "$i - $1"; break; fi
+      shift
+    done
+  }
+  #
+  # shellcheck disable=2086
+  #   I want word splitting:
+  output_part_number  "$field"  $text
+  IFS=$old_ifs
+  # Re-enable globbing:
+  set  +f
 }
 
 
-# I want word splitting:
-#   shellcheck disable=2086
-output_part_number  "$which_match_number_to_output"  $text
 
-
-
-# Restore the value of 'IFS'.
-IFS=$old_ifs
-
-# Re-enable globbing.
-set  +f
+replace_cut  "$@"
