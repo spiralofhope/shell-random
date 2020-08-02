7352cf6feeab10637f487100507541f31f6f7748
spiralofhope <soh@spiralofhope.com>
2020-08-01 18:19:23 -0700

    I forgot what my coding style was.

--- live/sh/scripts/is_string_a_number？.sh
+++ live/sh/scripts/is_string_a_number？.sh
@@ -6,31 +6,31 @@
 
 case $* in
   '')
-    \echo  1
+    \return  1
   ;;
   [0-9])
-    \echo  0
+    \return  0
   ;;
   [0-9][0-9])
-    \echo  0
+    \return  0
   ;;
   [-+][0-9]*[.][0-9]*)
-    \echo  0
+    \return  0
   ;;
   [0-9]*[.][0-9]*)
-    \echo  0
+    \return  0
   ;;
   [-+][0-9])
-    \echo  0
+    \return  0
   ;;
   ## shellcheck disable=2039
   ## shellcheck disable=2102
   [0-9]*[^^0-9])
     # FIXME? - In POSIX sh, ^ in place of ! in glob bracket expressions is undefined.
     # https://github.com/koalaman/shellcheck/wiki/SC2039
-    \echo  0
+    \return  0
   ;;
   *)
-    \echo  1
+    \return  1
   ;;
 esac
