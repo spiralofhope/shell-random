7cdcf16890541acfe1ed24a21bdd39ca5b03b23a
spiralofhope <soh@spiralofhope.com>
2020-08-01 18:06:04 -0700

    Making it echo instead of return, so it's cleaner for my coding style.

--- live/sh/scripts/is_string_a_number？.sh
+++ live/sh/scripts/is_string_a_number？.sh
@@ -1,34 +1,36 @@
 #!/usr/bin/env  sh
 
+# NOTE - POSIX shism
+
 
 
 case $* in
   '')
-    return 1
+    \echo  1
   ;;
   [0-9])
-    return 0
+    \echo  0
   ;;
   [0-9][0-9])
-    return 0
+    \echo  0
   ;;
   [-+][0-9]*[.][0-9]*)
-    return 0
+    \echo  0
   ;;
   [0-9]*[.][0-9]*)
-    return 0
+    \echo  0
   ;;
   [-+][0-9])
-    return 0
+    \echo  0
   ;;
   ## shellcheck disable=2039
   ## shellcheck disable=2102
   [0-9]*[^^0-9])
     # FIXME? - In POSIX sh, ^ in place of ! in glob bracket expressions is undefined.
     # https://github.com/koalaman/shellcheck/wiki/SC2039
-    return 0
+    \echo  0
   ;;
   *)
-    return 1
+    \echo  1
   ;;
 esac
