1834cdb5638c4a1663a7da0ccf8bb6970719ff34
spiralofhope <soh@spiralofhope.com>
2020-08-13 13:00:15 -0700

    I know it's simple/stupid, but use a script for the date.

--- live/sh/scripts/7zip-file-with-date-and-time.sh
+++ live/sh/scripts/7zip-file-with-date-and-time.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env  sh
-
+# shellcheck disable=1001
+  # I like my backslashes.
 
 # Compress a file and append the date and time to its filename.
 # Note that 7-zip does not store the owner/group of the file.
 # TODO - just leverage 7zip-compress-file.sh
 
 
-date_and_time=$( \date  --utc  +%Y-%m-%d\ %H։%M  )
-filename="$1"__"$date_and_time".7z
+date_hours_minutes=$( \date.sh  'minutes'  )
+filename="$1"__"$date_hours_minutes".7z
 
 \7z  a  \
-  ` # maximum compression ` \
+  "` # maximum compression `" \
   -mx9 \
   "$filename" \
   "$1"
