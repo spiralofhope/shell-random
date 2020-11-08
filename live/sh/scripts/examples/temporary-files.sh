6fe534432b809ad1dab853c5707588d2504b7554
spiralofhope <soh@spiralofhope.com>
2020-11-07 17:12:14 -0800

    uncomment everything

--- live/sh/scripts/examples/temporary-files.sh
+++ live/sh/scripts/examples/temporary-files.sh
@@ -6,7 +6,7 @@
 
 
 
-:<<'}'   #  Referencing the current process' PID ($$).
+#:<<'}'   #  Referencing the current process' PID ($$).
 {
   # While $$ is easy to remember, this is not 100% guaranteed to be a unique filename.
   # It's also not appropriate when your script needs to refer to use several unique tempfiles.
@@ -17,7 +17,7 @@
 }
 
 
-:<<'}'   #  Using  `mktemp`
+#:<<'}'   #  Using  `mktemp`
 {
   # GNU mktemp will create a file which is guaranteed to be unique.
   # By default it will use `/tmp` but you can specify another directory with the --tmpdir switch.
@@ -26,7 +26,7 @@
 }
 
 
-:<<'}'   #  Using  `mktemp`  in a more fancy way.
+#:<<'}'   #  Using  `mktemp`  in a more fancy way.
 {
   # This method is especially useful for debugging multiple executions of the same script.
   # $$  is the process ID of the current process.
