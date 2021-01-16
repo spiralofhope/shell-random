c2d1f8b47602789fcab5cf17842b1723b6d1c3af
spiralofhope <soh@spiralofhope.com>
2021-01-15 19:10:57 -0800

    - implement a shorthand for `cdd` wielding `findfile_raw` - replace dirname

--- live/sh/functions/cdd.sh
+++ live/sh/functions/cdd.sh
@@ -1,14 +1,40 @@
 #!/usr/bin/env  sh
+# shellcheck  disable=1001  disable=1012
+#   (I like backslashes)
 # Change directory into a directory-symbolic-link's real location, or a file's directory.
+# Requires `realpath`
 
 
 
 # I'd love to hijack 'cd' directly using cd(), but that doesn't work.
 cdd() {
-  _target="$( \realpath "$*" )"
-  if   [ -L "$*" ] && [ -d "$_target" ] ; then  \cd  "$_target"                   ||  return  $?
-  elif                [ -f "$_target" ] ; then  \cd  "$( \dirname  "$_target" )"  ||  return  $?
-  else                                          \cd  "$*"                         ||  return  $?
+  # Only the first line, in case the user does something like
+  #   cdd `findfile_raw filename`
+  for line in "$@"; do
+    __="$line"
+    break
+  done
+  #
+  __="$( \realpath  "$__" )"
+  #
+  # taken from  `replace-dirname.sh`
+  _dirname() {
+    dir=${1:-.}
+    dir=${dir%%"${dir##*[!/]}"}
+    [ "${dir##*/*}" ] && dir=.
+    dir=${dir%/*}
+    dir=${dir%%"${dir##*[!/]}"}
+    printf '%s\n' "${dir:-/}"
+  }
+  #
+  if   [ -L "$__" ] && [ -d "$__" ] ; then  \cd  "$__"                   ||  return  $?
+  elif                 [ -f "$__" ] ; then  \cd  "$( _dirname  "$__" )"  ||  return  $?
+  else                                      \cd  "$@"                    ||  return  $?
   fi
-  unset  _target
+  unset  __
 }
+
+
+
+cddf() { cdd  "$( findfile_raw  "$@" )" ; }
+cddd() { cdd  "$( finddir_raw   "$@" )" ; }
