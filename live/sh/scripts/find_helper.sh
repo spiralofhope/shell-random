e930adffe83cf10ba5af7f2b27c875dd7b3c624e
spiralofhope <soh@spiralofhope.com>
2021-01-15 19:01:10 -0800

    - style - example - implement quotes on/off for use with something like:   cdd `findfile_raw filename`

--- live/sh/scripts/find_helper.sh
+++ live/sh/scripts/find_helper.sh
@@ -7,24 +7,37 @@
 find_helper() {
   type="$1"
   color="$2"
-  shift; shift   #  $3*
-  \find  \
-    ./  \
-    -type  "$type"  \
-    -iname  \*"$*"\*  |\
-      \sed  's/^/"/'  |\
-        \sed  's/$/"/'  |\
-          \grep  \
-            --colour="$color"  \
-            --fixed-strings  \
-            --ignore-case  \
-            --  \
-            "$*"
+  quotes="$3"
+  shift; shift; shift   #  $4*
+  if [ "$quotes" = 'quotes_on' ]; then
+    \find  \
+      ./  \
+      -type  "$type"  \
+      -iname  \*"$*"\*  \
+    | \sed  's/^/"/'  \
+    | \sed  's/$/"/'  \
+    | \grep  \
+        --colour="$color"  \
+        --fixed-strings  \
+        --ignore-case  \
+        --  \
+        "$*"
+  else
+      \find  \
+      ./  \
+      -type  "$type"  \
+      -iname  \*"$*"\*  \
+    | \grep  \
+        --colour="$color"  \
+        --fixed-strings  \
+        --ignore-case  \
+        --  \
+        "$*"
+  fi
 }
 
 
 
-find_helper  "$@"
-
-
-
+find_helper  "$*"
+# e.g.
+# find_helper  'f'  'always'  'quotes_on'  "$*"
