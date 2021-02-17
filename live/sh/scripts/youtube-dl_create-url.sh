686aab28935780cc3da9a9fef0ff1286dc82d186
spiralofhope <soh@spiralofhope.com>
2021-02-17 15:57:12 -0800

    .

--- live/sh/scripts/youtube-dl_create-url.sh
+++ live/sh/scripts/youtube-dl_create-url.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env  sh
 
+# Create a Windows-compatible .lnk (web page link) 
 
 
-#:<<'}'   #  Create a Windows-compatible .lnk (web page link) to the source
-{
+
 filename='v.url'
-url=$( search-json.sh  'webpage_url'  v.info.json )
+
+if [ ! -z "$#" ]; then
+  url="$1"
+else
+  url=$( search-json.sh  'webpage_url'  v.info.json )
+fi
 \echo  '[{000214A0-0000-0000-C000-000000000046}]
 Prop3=19,2
 [InternetShortcut]
 IDList='               >   "$filename"
 \echo  "URL=${url}"  >>  "$filename"
-}
