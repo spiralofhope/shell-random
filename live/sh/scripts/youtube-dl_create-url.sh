8cd0cbb5a28e03363e21127de9cf329a259676db
spiralofhope <soh@spiralofhope.com>
2021-03-16 17:55:24 -0700

    .

--- live/sh/scripts/youtube-dl_create-url.sh
+++ live/sh/scripts/youtube-dl_create-url.sh
@@ -7,13 +7,15 @@
 filename='v.url'
 
 
+\echo  'Creating a a Windows-compatible .lnk (web page link)...'
 if [ $# -eq 0 ]; then
-  \echo  " * Searching for the URL using 'v.info.json'..."
+  \echo  ' * Searching for the URL using:  v.info.json'
+  \echo  '   (this will take a moment)'
   url=$( search-json.sh  'webpage_url'  v.info.json )
 else
   url="$1"
 fi
-\echo  "   Using the URL $1"
+\echo  "   Using the URL:  $url"
 \echo  '[{000214A0-0000-0000-C000-000000000046}]
 Prop3=19,2
 [InternetShortcut]
