f4791d82e59c5bfdae61931d85fb4cc9c8b72cb7
spiralofhope <soh@spiralofhope.com>
2021-03-16 17:49:39 -0700

    Corrected.. so that it *actually* works.

--- live/sh/scripts/youtube-dl_create-url.sh
+++ live/sh/scripts/youtube-dl_create-url.sh
@@ -6,13 +6,16 @@
 
 filename='v.url'
 
-if [ ! -z "$#" ]; then
-  url="$1"
-else
+
+if [ $# -eq 0 ]; then
+  \echo  " * Searching for the URL using 'v.info.json'..."
   url=$( search-json.sh  'webpage_url'  v.info.json )
+else
+  url="$1"
 fi
+\echo  "   Using the URL $1"
 \echo  '[{000214A0-0000-0000-C000-000000000046}]
 Prop3=19,2
 [InternetShortcut]
 IDList='               >   "$filename"
-\echo  "URL=${url}"  >>  "$filename"
+\echo    "URL=${url}"  >>  "$filename"
