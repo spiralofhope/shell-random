d655c50d8f8f2ea2aede620795059d42ebf4cabe
spiralofhope <spiralofhope@spiralofhope.com>
2022-04-09 17:12:05 -0700

    oh god, the spaces before nocorrect were screwing up entries into the history!

--- live/zsh/aliases.sh
+++ live/zsh/aliases.sh
@@ -6,18 +6,18 @@
 # autoload -U zsh-mime-setup
 # zsh-mime-setup
 
-alias  apt='    nocorrect  \apt'
-alias  cp='     nocorrect  \cp  --interactive  --preserve=all'
-alias  geany='  nocorrect  geany'
-alias  git='    nocorrect  \git'
+alias  apt='nocorrect      \apt'
+alias  cp='nocorrect       \cp  --interactive  --preserve=all'
+alias  geany='nocorrect     geany'
+alias  git='nocorrect      \git'
 alias  killall='nocorrect  \killall'
-alias  ls='     nocorrect  \ls  -1  --all  --classify  --color=always  --group-directories-first  --show-control-chars'
-alias  man='    nocorrect  \man'
-alias  mcd='    nocorrect  mcd'
-alias  mv='     nocorrect  \mv  --interactive'
-alias  mkdir='  nocorrect  \mkdir'
-alias  ping='   nocorrect  \ping'
-alias  rm='     nocorrect  \rm  --interactive  --one-file-system'
+alias  ls='nocorrect       \ls  -1  --all  --classify  --color=always  --group-directories-first  --show-control-chars'
+alias  man='nocorrect      \man'
+alias  mcd='nocorrect       mcd'
+alias  mv='nocorrect       \mv  --interactive'
+alias  mkdir='nocorrect    \mkdir'
+alias  ping='nocorrect     \ping'
+alias  rm='nocorrect       \rm  --interactive  --one-file-system'
 
 
 # --  Suffixes
