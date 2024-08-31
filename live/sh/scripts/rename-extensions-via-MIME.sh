7c3937e5d3c8f5f9e0cbea640ff948a30871119a
spiralofhope <soh@spiralofhope.com>
2024-08-31 13:18:02 -0700

    Finished more-complex script

--- live/sh/scripts/rename-extensions-via-MIME.sh
+++ live/sh/scripts/rename-extensions-via-MIME.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# 
 
 
 
@@ -16,8 +17,9 @@ _basename() {
 
 
 
+# In case you don't have system-specified MIME files, or you want to override content in them:
 fallback_temporary_mime_file="/tmp/$( _basename  "$0" )-mime_types.$$.txt"
-: <<EOF > "$fallback_temporary_mime_file"
+\cat > "$fallback_temporary_mime_file" << HERE_DOCUMENT
 application/pdf pdf
 application/vnd.rn-realmedia rm
 audio/mpeg mp3
@@ -30,47 +32,74 @@ text/x-shellscript sh
 video/mp4 mp4
 video/ogg ogg
 video/webm webm
-EOF
+HERE_DOCUMENT
+# Automatically clean up the temporary file on exit, error, control-c, etc.
+# See  `trapping-signals.sh`
+_teardown()  {
+  \rm  --force  "$fallback_temporary_mime_file"
+  exit
+  for  signal  in  INT QUIT HUP TERM EXIT; do
+    trap  -  "$signal"
+  done
+}
+trap  _teardown  INT QUIT HUP TERM EXIT
 
-# Preferred are at the bottom
-_="$fallback_temporary_mime_file" ; if [ -f "$_" ]; then  MIME_FILE="$_";  fi
-_='/usr/share/mime/types'         ; if [ -f "$_" ]; then  MIME_FILE="$_";  fi
-_='/etc/mime.types'               ; if [ -f "$_" ]; then  MIME_FILE="$_";  fi
 
 
-
-
-for FILE in *; do
-  if [ ! -f "$FILE" ]; then  continue;  fi
-
-  MIME_TYPE=$( \file  --brief  --mime-type  "$FILE" )
-
-  \echo  -n "$MIME_TYPE \t $FILE\n"
-
-  EXT="$( \awk -v mime="$MIME_TYPE" '
+_find_extension() {
+  filename_to_check="$1"
+  mime_types_file="$2"
+  #
+  if [ ! -f "$mime_types_file"   ]; then  return  1; fi
+  if [ ! -f "$filename_to_check" ]; then  return  1; fi
+  mime_type=$( \file  --brief  --mime-type  "$filename_to_check" )
+  # \echo  -n "$mime_type \t $file\n"
+  extension_detected="$( \awk -v mime="$mime_type" '
     $0 ~ /^[[:space:]]*#/ { next }  # Skip lines starting with #
     $0 ~ /^[[:space:]]*$/ { next }  # Skip blank lines
-    $1 == mime { print $2 }' "$MIME_FILE" )"
+    $1 == mime { print $2 }' "$mime_types_file" )"
+  if [ -z "$extension_detected" ]; then
+#    \echo  " * ERROR:  MIME file \"$mime_types_file\" has no type for:  \"$filename_to_check\""  >&2
+   return  1
+ fi
+ # \echo  "Detected extension: $extension_detected"
+}
 
-  # Check if an extension was found
-  if [ -n "$EXT" ]; then
-    BASE_NAME="$( _basename "$FILE" )"
-    CURRENT_EXT="${BASE_NAME##*.}"
-    BASE_NAME="${BASE_NAME%.*}"
-    NEW_FILE="./$BASE_NAME.$EXT"
-    if [ "$CURRENT_EXT" != "$EXT" ]; then
-      if [ "$DRY_MODE" = 'true' ]; then
-        \echo  "\mv  --verbose  \"$FILE\" \"$NEW_FILE\"  (DRY RUN)"
-      else
-        \mv  --verbose  "$FILE" "$NEW_FILE"
-      fi
-    else
-      \echo  "Skipping correct extension for:  $FILE"
-    fi
-  else
-    \echo  " * ERROR:  Unknown MIME type or no matching extension found for:  $FILE"
+
+
+# TODO - Allow the user to specify a filename.  If not specified, then iterate through *
+for file in *; do
+  if [ ! -f "$file" ]; then
+    \echo  "Skipping non-directory \"$file\""  >&2
+    continue
   fi
+  #
+  # Bottom takes priority:
+  _find_extension  "$file"  '/etc/mime.types'
+  # Has no extensions:
+  #_find_extension  "$file"  '/usr/share/mime/types'
+  _find_extension  "$file"  "$fallback_temporary_mime_file"
+  if [ -z "$extension_detected" ]; then
+    \echo  " * ERROR:  No extension found:"  >&2
+    \echo  "   $file"  >&2
+    \echo  "   $mime_type"  >&2
+    continue
+  fi
+  #
+  file_basename="$( _basename "$file" )"
+  extension_current="${file_basename##*.}"
+  file_basename="${file_basename%.*}"
+  file_new_name="./$file_basename.$extension_detected"
+  #
+  if [ "$extension_current" = "$extension_detected" ]; then
+    \echo  "Skipping correct extension for:  $file"  >&2
+    continue
+  fi
+  #
+  if [ "$DRY_MODE" = 'true' ]; then
+    \echo  "\mv  --verbose  \"$file\" \"$file_new_name\"  (DRY RUN)"  >&2
+    continue
+  fi
+  # The stars have aligned.
+  \mv  --verbose  "$file"  "$file_new_name"
 done
-
-
-\rm  --force  --verbose  "$fallback_temporary_mime_file"
