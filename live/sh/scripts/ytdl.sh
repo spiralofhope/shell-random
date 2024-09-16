d9f275025b07e306d6e602412d91c39f74c7ff2e
spiralofhope <soh@spiralofhope.com>
2024-09-16 04:20:22 -0700

    apparently this is in the repository now

--- live/sh/scripts/ytdl.sh
+++ live/sh/scripts/ytdl.sh
@@ -2,8 +2,8 @@
 
 
 
-# https://github.com/yt-dlp/yt-dlp
-# https://github.com/yt-dlp/yt-dlp#output-template-examples
+# https://github.com/\yt-dlp/\yt-dlp
+# https://github.com/\yt-dlp/\yt-dlp#output-template-examples
 
 # Note that --write-info-json might still contain personal information if special settings are used.
 
@@ -30,61 +30,61 @@ https://sponsor.ajay.app/
 
 
 update(){
-yt-dlp_linux  --update
+  \yt-dlp  --update
 }
 
 
 get_comments() {
-yt-dlp_linux  \
-  --skip-download  \
-  --write-comments  \
-  --output '%(uploader)s/%(upload_date>%Y)s-%(upload_date>%m)s-%(upload_date>%d)s - %(title)s/comments--'"$( \date  --utc  +%Y-%m-%d_%H։%M )"'.%(ext)s'  \
-$*
+  \yt-dlp  \
+    --skip-download  \
+    --write-comments  \
+    --output '%(uploader)s/%(upload_date>%Y)s-%(upload_date>%m)s-%(upload_date>%d)s - %(title)s/comments--'"$( \date  --utc  +%Y-%m-%d_%H։%M )"'.%(ext)s'  \
+  $*
 }
 
 
 
 get_subtitles() {
-yt-dlp_linux  \
-  --no-abort-on-error  \
-  --output '%(uploader)s/%(upload_date>%Y)s-%(upload_date>%m)s-%(upload_date>%d)s - %(title)s/subs/v.%(ext)s'  \
-  --skip-download  \
-  --write-subs  \
-    --all-subs  \
-    --convert-subs srt  \
-    --write-auto-subs  \
-$*
+  \yt-dlp  \
+    --no-abort-on-error  \
+    --output '%(uploader)s/%(upload_date>%Y)s-%(upload_date>%m)s-%(upload_date>%d)s - %(title)s/subs/v.%(ext)s'  \
+    --skip-download  \
+    --write-subs  \
+      --all-subs  \
+      --convert-subs srt  \
+      --write-auto-subs  \
+  $*
 }
 
 
 
 get_video_etc(){
-yt-dlp_linux  \
-  --concurrent-fragments  3  \
-    --no-keep-fragments  \
-  --continue  \
-  --embed-chapters  \
-  --embed-metadata  \
-    --xattrs  \
-  --no-abort-on-error  \
-  --no-write-subs  \
-    --all-subs  \
-    --embed-subs  \
-    --write-auto-subs  \
-  --output '%(uploader)s/%(upload_date>%Y)s-%(upload_date>%m)s-%(upload_date>%d)s - %(title)s/v.%(ext)s'  \
-  --rm-cache-dir  \
-  --windows-filenames  \
-  --write-description  \
-  --write-info-json  \
-    --clean-info-json  \
-    --embed-info-json  \
-  --write-link  \
-    --write-desktop-link  \
-    --write-url-link  \
-    --write-webloc-link  \
-  --write-thumbnail  \
-    --embed-thumbnail  \
-$*
+  \yt-dlp  \
+    --concurrent-fragments  3  \
+      --no-keep-fragments  \
+    --continue  \
+    --embed-chapters  \
+    --embed-metadata  \
+      --xattrs  \
+    --no-abort-on-error  \
+    --no-write-subs  \
+      --all-subs  \
+      --embed-subs  \
+      --write-auto-subs  \
+    --output '%(uploader)s/%(upload_date>%Y)s-%(upload_date>%m)s-%(upload_date>%d)s - %(title)s/v.%(ext)s'  \
+    --rm-cache-dir  \
+    --windows-filenames  \
+    --write-description  \
+    --write-info-json  \
+      --clean-info-json  \
+      --embed-info-json  \
+    --write-link  \
+      --write-desktop-link  \
+      --write-url-link  \
+      --write-webloc-link  \
+    --write-thumbnail  \
+      --embed-thumbnail  \
+  $*
 }
 
 
