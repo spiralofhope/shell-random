61946e78b2f5c89c6a00540f18fbfd1d0d168eb8
spiralofhope <spiralofhope@spiralofhope.com>
2024-11-13 21:49:39 +0000

    correct using geany with no parameters

--- live/sh/functions/geany.sh
+++ live/sh/functions/geany.sh
@@ -7,6 +7,10 @@
 geany() {
   # I don't understand why \geany won't work.
   #/usr/bin/geany  &
+  if [ -z "$@" ]; then
+    /usr/bin/geany &
+    return
+  fi
   for i in "$@"; do
     if [ -f "$i" ]; then
       /usr/bin/geany  "$( \realpath  "$i" )" &
