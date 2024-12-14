431b7630f1251ef728c742238c6b35077ccf2c63
spiralofhope <soh@spiralofhope.com>
2024-12-14 11:00:29 -0800

    - Integrated old notes - Re-implemented a missing smarter-update - A general looking-over - Note:  Subtitle downloading is still unsolved - Note:  Cookie functionality has not been tested in a long time; there's a username/password feature now anyway

--- live/sh/scripts/ytdl.sh
+++ live/sh/scripts/ytdl.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env  sh
 
 
+youtube_username='spiralofhope'
+
 
 # https://github.com/\yt-dlp/\yt-dlp
 # https://github.com/\yt-dlp/\yt-dlp#output-template-examples
@@ -11,10 +13,6 @@
 
 :<<'}'
 {
-FIXME - The comments need a date.  Therefore give this two passes.
-
-
-
 SponsorBlock Options are interesting.
 https://sponsor.ajay.app/
 
@@ -25,40 +23,77 @@ https://sponsor.ajay.app/
   --output '%(uploader)s/%(upload_date)s - %(title)s/v.%(ext)s'
   --split-chapters
     --force-keyframes-at-cuts
+
+
+Cookies:
+DEBUG='true'
+# https://blog.spiralofhope.com/?p=79681#using-cookies
+#USE_COOKIES='forced'
+#USE_COOKIES='automatic'
+COOKIES_FILE="$HOME/cookies.txt"
+PROFILE_DIRECTORY="/mnt/c/Users/user/AppData/Local/BraveSoftware/Brave-Browser/User Data/Profile\ 1/Network/Cookies"
+
+
+COOKIES_DIRECTORY=${COOKIES_DIRECTORY='brave:profile'}
+# Defaults
+USE_COOKIES=${USE_COOKIES='false'}
+COOKIES_FILE=${COOKIES_FILE="$HOME/cookies.txt"}
+DEBUG=${DEBUG='false'}
+
+--cookies-from-browser brave:"$PROFILE_DIRECTORY"  \
 }
 
 
 
-update(){
-  \yt-dlp  --update
+_update(){
+  # For testing:
+  #\touch  --date="yesterday"  "$0"
+  date_script=$( \date -r "$0" +"%Y-%m-%d" )
+  date_today=$(  \date         +"%Y-%m-%d" )
+  #
+  if [ "$date_script" != "$date_today" ]; then
+    \yt-dlp  --update  ||  exit  1
+    \touch  "$0"
+  else
+    \echo 'No update needed.'
+  fi
 }
 
 
-get_comments() {
+_get_comments() {
   \yt-dlp  \
     --skip-download  \
     --write-comments  \
     --output '%(uploader)s/%(upload_date>%Y)s-%(upload_date>%m)s-%(upload_date>%d)s - %(title)s/comments--'"$( \date  --utc  +%Y-%m-%d_%H։%M )"'.%(ext)s'  \
-  $*
+  $*  ||  exit  1
 }
 
 
 
-get_subtitles() {
+_get_subtitles() {
+# This doesn't work any more.  Too many downloads occur and it kills the connection.
+# ERROR: Unable to download video subtitles for 'xx': HTTP Error 429: Too Many Requests
+#   https://github.com/blackjack4494/yt-dlc/issues/158
+# This doesn't work:
+#      --sleep-subtitles 4  ` # Most videos are autotranslated into dozens of languages.`  \
+#    --username  "$youtube_username"  ` # For some people, this will bypass the 429 error.  It prompts for a password.`  \
+#
+#
   \yt-dlp  \
     --no-abort-on-error  \
-    --output '%(uploader)s/%(upload_date>%Y)s-%(upload_date>%m)s-%(upload_date>%d)s - %(title)s/subs/v.%(ext)s'  \
+    --output  '%(uploader)s/%(upload_date>%Y)s-%(upload_date>%m)s-%(upload_date>%d)s - %(title)s/subs/v.%(ext)s'  \
     --skip-download  \
     --write-subs  \
-      --all-subs  \
-      --convert-subs srt  \
+      --convert-subs  srt  \
+      --sub-format  srt/best  \
+      --sub-langs  all  \
       --write-auto-subs  \
-  $*
+  $*  ||  exit  1
 }
 
 
 
-get_video_etc(){
+_get_video_etc(){
   \yt-dlp  \
     --concurrent-fragments  3  \
       --no-keep-fragments  \
@@ -68,7 +103,7 @@ get_video_etc(){
       --xattrs  \
     --no-abort-on-error  \
     --no-write-subs  \
-      --all-subs  \
+      --sub-langs  en  \
       --embed-subs  \
       --write-auto-subs  \
     --output '%(uploader)s/%(upload_date>%Y)s-%(upload_date>%m)s-%(upload_date>%d)s - %(title)s/v.%(ext)s'  \
@@ -84,18 +119,16 @@ get_video_etc(){
       --write-webloc-link  \
     --write-thumbnail  \
       --embed-thumbnail  \
-  $*
+  $*  ||  exit  1
 }
 
 
 
-#update
-get_comments   $*  && \
-get_subtitles  $*  && \
-get_video_etc  $*
+_update
+_get_subtitles  $*
+_get_comments   $*
+_get_video_etc  $*
 
-
-#get_subtitles  $*  && \
 
 
 :<<'}'
