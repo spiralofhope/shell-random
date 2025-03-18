a03f62423615cc40cf2504654e90dc6e59d272b4
spiralofhope <soh@spiralofhope.com>
2025-03-18 01:43:58 -0700

    I have no idea why there's a ytdl()

--- live/sh/aliases.sh
+++ live/sh/aliases.sh
@@ -46,11 +46,11 @@ ddir() {
 alias  dir='dir-4DOS-style.sh  .  |  myless'
 
 
-ytdl(){
-  printf  '\033]0;...\007'
-  youtube-download.sh  "$@"
-  printf  '\n\n'
-}
+#ytdl(){
+#  printf  '\033]0;...\007'
+#  youtube-download.sh  "$@"
+#  printf  '\n\n'
+#}
 
 
 # This won't work on cygwin, and I'm not even sure what it was for..
