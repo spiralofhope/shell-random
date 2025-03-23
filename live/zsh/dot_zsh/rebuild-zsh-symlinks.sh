a53d005260990b80395cef6c27da7d795aaa589f
spiralofhope <soh@spiralofhope.com>
2025-03-22 22:19:27 -0700

    From a while back, likely still useful.

---
+++ live/zsh/dot_zsh/rebuild-zsh-symlinks.sh
@@ -0,0 +1,5 @@
+\ln  --symbolic  --force  ~/.zsh/4-login.sh        ~/.zlogin
+\ln  --symbolic  --force  ~/.zsh/shutdown.sh       ~/.zlogout
+\ln  --symbolic  --force  ~/.zsh/2-login.sh        ~/.zprofile
+\ln  --symbolic  --force  ~/.zsh/1-every.sh        ~/.zshenv
+\ln  --symbolic  --force  ~/.zsh/3-interactive.sh  ~/.zshrc
