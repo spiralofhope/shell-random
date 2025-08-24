61867bba7ad7081bb7ffa42f3e6a42fefc06beb8
spiralofhope <spiralofhope@spiralofhope.com>
2025-08-24 10:06:45 -0700

    - Decruft with the new `find` parameter. - Add a random new .db file I found.

--- live/sh/scripts/nuke-bullshit.sh
+++ live/sh/scripts/nuke-bullshit.sh
@@ -1,29 +1,30 @@
 #!/usr/bin/env  sh
 # Delete a list of files
 
-# TODO - Build the list as an array.
-# TODO - Build the list as a text file.
+# TODO - Build the list as a HEREDOC.
 
 
 
-#:<<'}'  #  Create a list of files
+# For testing purposes, create a list of files:
+:<<'}'
 {
-  
-  for file in \
-    .DS_Store  \
-    .BridgeCache  \
+  for file in      \
+    .DS_Store      \
+    .BridgeCache   \
     .BridgeCacheT  \
-    Thumbs.db  \
-    desktop.ini  \
+    desktop.ini    ` # Windows ` \
+    photothumb.db  \
+    Thumbs.db      \
   ; do
     :>  "$file"
   done
-  \echo  '--'
-  \ls  -A1
-  \echo  '--'
+#  \echo  '--'
+#  \ls  -A1
+#  \echo  '--'
 }
 
 
+# Method 1
 #:<<'}'   #   Delete using `find`, using multiple options
 {
   \find  \
@@ -31,26 +32,23 @@
         -type f  -name '.DS_Store'      -delete  \
     -o  -type f  -name '.BridgeCache'   -delete  \
     -o  -type f  -name '.BridgeCacheT'  -delete  \
+    -o  -type f  -name 'desktop.ini'    -delete  ` # Windows ` \
+    -o  -type f  -name 'photothumb.db'  -delete  \
     -o  -type f  -name 'Thumbs.db'      -delete  \
-    ` # Windows `  \
-    -o  -type f  -name 'desktop.ini'    -delete  \
      \
   ` # `
 }
 
 
 
-#:<<'}'   #   Delete using `find`, using a single long regex
+# Method 2
+:<<'}'   #   Delete using `find`, using a single long regex
 {
   \find  \
     ./  \
     -type f  \
-    -regextype  posix  \
-    -regex  '.*\/\(\.DS_Store\|\.BridgeCache\|\.BridgeCacheT\|Thumbs\.db\|desktop\.ini\)'  \
+    -regextype  posix-basic  \
+    -regex  '.*\/\(\.DS_Store\|\.BridgeCache\|\.BridgeCacheT\|desktop\.ini\|photothumb\.db\|Thumbs\.db\)'  \
     -delete  \
   ` # `
 }
-
-
-
-\ls  -A1
